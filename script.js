29ae4fe374ba46f3066734561e52193e5b8d8aef
Stefano <stefanooliveira1@gmail.com>
2021-05-06 15:11:21 -0300

    save

---
+++ script.js
@@ -0,0 +1,9 @@
+function buttonClick () {
+    let getButton = document.querySelector('#criar-tarefa');
+    let getUL = document.querySelector('#lista-tarefas');
+    let newListItem = document.createElement('li');
+
+    getButton.addEventListener('click', function() {
+        
+    })
+}
