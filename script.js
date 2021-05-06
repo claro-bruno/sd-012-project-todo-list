cb79446fcc36fc9c3a90fa88533bee05fff1b2a9
Anderson B <andbmarques@gmail.com>
2021-05-06 15:34:29 -0300

    Requisito 7 adicionado

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 window.onload = function(){
-    let button = document.getElementById('criar-tarefa')
-    let list = document.getElementById('lista-tarefas') 
-    let taskName = document.getElementById('texto-tarefa')
+    createTask();
+    selectTask();
+}
 
+function createTask(){
+    let list = document.getElementById('lista-tarefas'); 
+    let taskName = document.getElementById('texto-tarefa');
+    let button = document.getElementById('criar-tarefa');
     button.addEventListener('click', () => {
-        let newTask = document.createElement('li')
-        newTask.innerText = taskName.value
-        list.appendChild(newTask)
-        taskName.value = ''
+        let newTask = document.createElement('li');
+        newTask.innerText = taskName.value;
+        newTask.className = 'task';
+        list.appendChild(newTask);
+        taskName.value = '';
     })
+}
 
+function selectTask(){
+    addEventListener('click', (event) => {
+        if(event.target.className === 'task'){
+            event.target.style.background = 'rgb(128, 128, 128)';
+        }
+    })
 }
