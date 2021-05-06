1af17e1855dd5df25f9c78e4f93fce7820eaffdb
Sarah <sarahgsb@hotmail.com>
2021-05-06 15:51:55 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let addButton = document.getElementById("criar-tarefa");
 let taskList = document.getElementById("lista-tarefas");
 let inputText = document.getElementById("texto-tarefa");
+
+addButton.addEventListener("click", function () {
+    let createList = document.createElement("li");
+    createList.classList.add("create-list");
+    createList.innerText = inputText.value;
+    inputText.value = "";
+    taskList.appendChild(createList);
+    }); 
