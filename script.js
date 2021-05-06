3d44ad3377cbe79f1ab41ad84f30b5045dd1d0dc
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-05-06 16:14:29 -0300

    Feito requisito 2

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const content = document.getElementById('content');
 const title = document.createElement('h1');
-const header = document.createElement('header');
 title.innerHTML = 'Minha Lista de Tarefas';
+
+const header = document.createElement('header');
+
 header.appendChild(title);
 content.appendChild(header);
+
+const paragraph = document.createElement('p');
+paragraph.id = 'funcionamento';
+paragraph.innerHTML =
+  'Clique duas vezes em um item para marcá-lo como completo';
+
+content.appendChild(paragraph);
