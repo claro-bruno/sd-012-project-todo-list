b1eb1708a9e99eda84419727249ddb7b7b40f385
Henrique Alarcon <henrique.alarcon@hotmail.com>
2021-05-06 18:20:52 -0300

    requisito 5,6 e 7

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-let botaoTarefa = document.getElementById("criar-tarefa")
-function gerarItem(){
-    document.target.innerHTML = ""
-}
+let input = document.getElementById("texto-tarefa");
+let listaTarefa = document.getElementById("lista-tarefas");
+let button = document.getElementById("criar-tarefa");
+
+function alteraFundoLista(event){
+    let fundoLista = document.getElementById("selected")
+
+    if(fundoLista){
+        fundoLista.classList.remove("selected");
+        fundoLista.style.backgroundColor("")
+    };
+    event.target.classList.add("selected");
+    event.target.style.backgroundColor = "rgb(128, 128, 128)"
+};
+function criarElementosLista(){
+    let novaTarefa = document.createElement("li");
+    novaTarefa.classList.add("tarefa-lista");
+    novaTarefa.innerHTML = input.value;
+    document.querySelector("#lista-tarefas").appendChild(novaTarefa);
+    novaTarefa.addEventListener("click", alteraFundoLista);
+
+};
+
+function limpaInput(){
+    input.value = "";
+};
+
+function funcaoBotao(){
+    criarElementosLista();
+    limpaInput();
+};
+
+button.addEventListener("click", funcaoBotao);
