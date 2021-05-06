42bfbcd2bb72f52cd00380d50af403bdc2d048e0
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-05-06 15:03:52 -0300

    conclusao do Desafio_02, onde crio um paragrafo com id funcionamento.

---
+++ script.js
@@ -0,0 +1,5 @@
+const bodyPagina = document.body;
+const pIdFuncionamento = document.createElement('p');
+pIdFuncionamento.id = 'funcionamento';
+pIdFuncionamento.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+bodyPagina.appendChild(pIdFuncionamento);
