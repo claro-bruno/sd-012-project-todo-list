9c210112378f6a595c9fc3854517e79f5e197c4b
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-05-06 17:31:17 -0300

    Requisitos 07 e 08

--- script.js
+++ script.js
@@ -5,13 +5,33 @@ const adicionaTarefa = () => {
   const capturaOl = document.querySelector('#lista-tarefas');
   const criaLi = document.createElement('li');
   criaLi.innerText = capturaInput.value;
+  criaLi.className = 'tarefa';
   capturaOl.appendChild(criaLi);
   capturaInput.value = '';
 };
 
+// Seleciona a tarefa
+
+const selecionaTarefa = (event) => {
+  const capturaLi = document.querySelectorAll('.tarefa');
+  for (let index = 0; index < capturaLi.length; index += 1) {
+    capturaLi[index].classList.remove('selecionada');
+  }
+  event.target.classList.add('selecionada');
+};
+
 // Adiciona Eventos
 document.addEventListener('click', (event) => {
-  if(event.target.id === 'criar-tarefa') {
+  if (event.target.id === 'criar-tarefa') {
     adicionaTarefa();
   }
-})
+  if (event.target.className === ('tarefa')) {
+    selecionaTarefa(event);
+  }
+});
+
+capturaInput.addEventListener('keypress', (event) => {
+  if (event.keyCode === 13) {
+    adicionaTarefa();
+  }
+});
