d2976113c5c249a9a88f74ed0cb24cd136099809
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-05-06 15:19:36 -0300

    Apaga script e testa

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function createOrderList() {
-  const orderList = document.createElement('ol');
-  orderList.id = 'lista-tarefas';
-  document.body.appendChild(orderList);
-}
-createOrderList();
+// function createOrderList() {
+//   const orderList = document.createElement('ol');
+//   orderList.id = 'lista-tarefas';
+//   document.body.appendChild(orderList);
+// }
+// createOrderList();
