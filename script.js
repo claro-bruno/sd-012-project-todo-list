98b2395cfcefeae3faff524f9069a7bf3c415411
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-05-06 15:07:19 -0300

    Ajusta lint do script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function createOrderList(){
-  let orderList = document.createElement('ol');
-  orderList.id = 'lista-tarefas'
+function createOrderList() {
+  const orderList = document.createElement('ol');
+  orderList.id = 'lista-tarefas';
   document.body.appendChild(orderList);
 }
 createOrderList();
