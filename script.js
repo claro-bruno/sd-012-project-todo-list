b44a07ab386d635f81830740e7c5f72c0da66e4b
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-06 19:08:53 -0300

    adding requirement 12

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const buttonNovaTarefa = document.querySelector('#criar-tarefa');
 const listaTarefas = document.querySelector('#lista-tarefas');
 const buttonApagaLista = document.querySelector('#apaga-tudo');
 const buttonApagaFinalizados = document.querySelector('#remover-finalizados');
+const buttonSalvaLista = document.querySelector('#salvar-tarefas');
 
 function riscaItemDaLista(element) {
   element.addEventListener('dblclick', (event) => {
@@ -11,7 +12,7 @@ function riscaItemDaLista(element) {
 }
 
 function mudaCorDeFundo(elemento) {
-  elemento.addEventListener('click', (event) => { 
+  elemento.addEventListener('click', (event) => {
     const itensDalista = document.querySelectorAll('.item-lista');
     for (let index = 0; index < itensDalista.length; index += 1) {
       if (event.target !== itensDalista[index]) itensDalista[index].classList.remove('selected');
@@ -27,7 +28,6 @@ function criandoNovaTarefa() {
     novoItem.innerText = inputTarefas.value;
     mudaCorDeFundo(novoItem);
     riscaItemDaLista(novoItem);
-    apagaItensDaLista(novoItem);
     listaTarefas.appendChild(novoItem);
     inputTarefas.value = '';
   });
@@ -48,9 +48,38 @@ apagaFinalizados();
 
 function apagaItensDaLista() {
   buttonApagaLista.addEventListener('click', () => {
-    const itensDalista = document.querySelectorAll('.item-lista');
+    const itensDalista = document.querySelectorAll('ol li');
     for (let index = 0; index < itensDalista.length; index += 1) {
       listaTarefas.removeChild(itensDalista[index]);
     }
   });
 }
+
+apagaItensDaLista();
+
+function salvarTarefas() {
+  buttonSalvaLista.addEventListener('click', () => {
+    localStorage.clear();
+    sessionStorage.clear();
+    const itensDalista = document.querySelectorAll('.item-lista');
+    for (let index = 0; index < itensDalista.length; index += 1) {
+      localStorage.setItem(`item${index}`, itensDalista[index].innerText);
+      sessionStorage.setItem(`class${index}`,itensDalista[index].className);
+    }
+  });
+}
+
+salvarTarefas();
+
+window.onload = () => {
+  for (let index = 0; index < localStorage.length; index += 1) {
+    const novoItem = document.createElement('li');
+    novoItem.className = sessionStorage.getItem(`class${index}`);
+    novoItem.innerText = localStorage.getItem(`item${index}`);
+    listaTarefas.appendChild(novoItem);
+    mudaCorDeFundo(novoItem);
+    riscaItemDaLista(novoItem);
+   }
+};
+
+
