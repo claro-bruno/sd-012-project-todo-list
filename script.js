81cde6a62af864167f4e0eea9149216e6e15684d
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-05-06 16:23:24 -0300

    Requisito 9, 10 e 11

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
-function unmarkTask(){
-    let selectedTask = document.querySelector('#selected');
-    selectedTask.id = '';
+function unmarkTask() {
+  let selectedTask = document.querySelector('#selected');
+  selectedTask.id = '';
 }
 
-function changeTaskColor(){
-    let taskList = document.querySelector('#lista-tarefas');
-    taskList.addEventListener('click', (e)=>{
-        if(e.target.className === 'tarefa'){
-            if(document.querySelector('#selected') !== null){
-                unmarkTask();
-                console.log('   hceugie')
-            }
-            e.target.id = 'selected';
-        };
-    });
+function changeTaskColor() {
+  const taskList = document.querySelector('#lista-tarefas');
+  taskList.addEventListener('click', (e) => {
+    if (e.target.className === 'tarefa' || e.target.className === 'tarefa completed') {
+      if (document.querySelector('#selected') !== null) {
+        unmarkTask();
+      }
+      e.target.id = 'selected';
+    };
+  });
+}
+
+function deleteCompletedTasks() {
+  const btn = document.querySelector('#remover-finalizados');
+  btn.addEventListener('click', () => {
+    const completedTasks = document.querySelectorAll('.tarefa.completed');
+    console.log(completedTasks)
+    for (let taskIndex = 0; taskIndex < completedTasks.length; taskIndex += 1) {
+      completedTasks[taskIndex].remove();
+    };
+  });
+}
+
+function completeTask() {
+  const taskList = document.querySelector('#lista-tarefas');
+  taskList.addEventListener('dblclick', (e) => {
+    if (e.target.className === 'tarefa') {
+      e.target.className += ' completed';
+    } else if (e.target.className === 'tarefa completed') {
+      e.target.className = 'tarefa';
+    };
+    deleteCompletedTasks();
+  });
+}
+
+function deleteAllList() {
+  const btn = document.querySelector('#apaga-tudo');
+  btn.addEventListener('click', () => {
+    const tasks = document.querySelectorAll('.tarefa');
+    for (let taskIndex = 0; taskIndex < tasks.length; taskIndex += 1) {
+      tasks[taskIndex].remove();
+    };
+  });
 }
 
 function createTask() {
@@ -26,11 +58,14 @@ function createTask() {
     input.value = '';
     let list = document.querySelector('#lista-tarefas');
     list.appendChild(task);
-    
+    deleteAllList();
   });
 }
 
+
+
 window.onload = () => {
-    createTask();
-    changeTaskColor();
+  createTask();
+  changeTaskColor();
+  completeTask();
 };
