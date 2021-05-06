60aef018aeab3c4a1b13a15a005de8b4f80eda3d
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-05-06 18:09:59 -0300

    Resolvendo requisitos do 7 ao 9

--- script.js
+++ script.js
@@ -1,36 +1,68 @@
 
-  const header = document.createElement('header');
-  const title = document.createElement('h1');
-  title.innerHTML = 'Minha Lista de Tarefas';
-  const main = document.querySelector('main');
-  const paragraph = document.createElement('p');
-  paragraph.id = 'funcionamento';
-  paragraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
-  main.appendChild(header);
-  document.querySelector('header').appendChild(title);
-  main.appendChild(paragraph);
+const header = document.createElement('header');
+const title = document.createElement('h1');
+title.innerHTML = 'Minha Lista de Tarefas';
+const main = document.querySelector('main');
+const paragraph = document.createElement('p');
+paragraph.id = 'funcionamento';
+paragraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
+main.appendChild(header);
+document.querySelector('header').appendChild(title);
+main.appendChild(paragraph);
 
-  const input = document.createElement('input');
-  input.id = 'texto-tarefa';
-  input.value = '';
-  input.type = 'text';
-  main.appendChild(input);
+const input = document.createElement('input');
+input.id = 'texto-tarefa';
+input.value = '';
+input.type = 'text';
+main.appendChild(input);
 
-  const btn1 = document.createElement('button');
-  btn1.id = 'criar-tarefa';
-  btn1.innerHTML = 'Adicionar';
-  main.appendChild(btn1);
+const btn1 = document.createElement('button');
+btn1.id = 'criar-tarefa';
+btn1.innerHTML = 'Adicionar';
+main.appendChild(btn1);
 
-  const lista = document.createElement('ol');
-  lista.id = 'lista-tarefas';
-  main.appendChild(lista);
+const lista = document.createElement('ol');
+lista.id = 'lista-tarefas';
+main.appendChild(lista);
 
-  const buttonClick = document.querySelector('#criar-tarefa');
-  buttonClick.addEventListener('click', function () {
-    let valor = document.getElementById('texto-tarefa').value;
+const buttonClick = document.querySelector('#criar-tarefa');
+buttonClick.addEventListener('click', function () {
+let valor = document.getElementById('texto-tarefa').value;
+  if(valor === '' || valor === ' ') {
+    alert('Nenhum texto foi digitado');
+  }
+  else {
     let item = document.createElement('li');
+    item.className = 'item_list';
     item.innerText = valor;
     let list = document.querySelector('#lista-tarefas');
     list.appendChild(item);
     document.getElementById('texto-tarefa').value = '';
-  })
+    color();
+  }
+})
+
+  function color () {
+    let item_list = document.querySelectorAll('.item_list');
+    for(let index = 0; index < item_list.length; index += 1) {
+      item_list[index].addEventListener('click', background);
+      item_list[index].addEventListener('dblclick', completed);
+    }
+  }
+
+  function background (e) {
+    let rem = document.querySelectorAll('.item_list');
+    for(let index = 0; index < rem.length; index += 1) {
+      rem[index].classList.remove('selected');
+    }
+    e.target.classList.add('selected');
+  }
+
+  function completed (e) {
+    if (e.target.classList.contains('completed')) {
+      e.target.classList.remove('completed');
+    }
+    else {
+      e.target.classList.add('completed');
+    }
+  }
