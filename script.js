eedb540e89cfead5ebd80138b5cb0d378c8be8d6
Camila Damásio <camiladamasioef@hotmail.com>
2021-05-06 19:26:51 -0300

    Corrige erros de indentaçao

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 window.onload = function () {
   const addTarefa = document.getElementById('criar-tarefa');
   addTarefa.addEventListener('click', function () {
-  const input = document.getElementById('texto-tarefa');
-  console.log(input.value);
-  let li = document.createElement('li');
-  li.classList.add('itemList');
-  let ol = document.getElementById('lista-tarefas');
-  li.innerHTML = input.value;
-  ol.appendChild(li);
-  input.value = '';
-  })
-}
+    const input = document.getElementById('texto-tarefa');
+    console.log(input.value);
+    const li = document.createElement('li');
+    li.classList.add('itemList');
+    const ol = document.getElementById('lista-tarefas');
+    li.innerHTML = input.value;
+    ol.appendChild(li);
+    input.value = '';
+  });
+};
