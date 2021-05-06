b63b7561f28ef65c852c682aa4cee337e7deca5d
camila-mp <ca.malvp@gmail.com>
2021-05-06 16:54:16 -0300

    resolve requisitos 5 e 6

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 //REQUISITO 5
 
-let botaoCriarTarefa = document.createElement('button');
-document.body.appendChild(botaoCriarTarefa);
-botaoCriarTarefa.id='criar-tarefa';
+let criaBotao = document.createElement('button');
+document.body.appendChild(criaBotao);
+criaBotao.id='criar-tarefa';
+
+let listaTarefas = document.getElementById('lista-tarefas');
+let botaoCriarTarefa = document.getElementById('criar-tarefa');
+let elementoInput = document.getElementById('texto-tarefa');
+
+//criando uma div para guardar texto do botão
+let divBotao = document.createElement('div');
+botaoCriarTarefa.appendChild(divBotao);
+divBotao.id = 'texto-botao'
+divBotao.innerHTML = 'Adicionar'
+
+//função que cria event listener no botaoCriarTarefa
+
+function criaEventoBotao() {
+    botaoCriarTarefa.addEventListener('click', function() {
+        let novoLi = document.createElement('li');
+        listaTarefas.appendChild(novoLi);
+        novoLi.innerHTML = elementoInput.value;
+        elementoInput.value = '';
+    });
+};
+
+criaEventoBotao();
