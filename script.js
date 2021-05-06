42b680ad076ff5fb83b26a1f0472348d6da6815f
Nuwanda <morato_dev@protonmail.com>
2021-05-06 16:09:53 -0300

    adiçao da estrutura html base. css base e botao de adicionar

---
+++ script.js
@@ -0,0 +1,10 @@
+const addToListButton = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefas');
+const typedTextInput = document.getElementById('texto-tarefa');
+
+addToListButton.addEventListener('click', () => {
+  const textAsItemList = document.createElement('li');
+  textAsItemList.innerHTML = typedTextInput.value;
+  taskList.appendChild(textAsItemList);
+});
+npx
