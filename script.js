1494c8d66cfda2e762cab6facac38facbb95bb3c
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-06 17:27:59 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ const criarButton = document.querySelector('#criar-tarefa');
 const text = document.querySelector('#texto-tarefa');
 const listMom = document.querySelector('#lista-tarefas');
 
+function completeItem(allItems) {
+  const last = allItems.length - 1;
+  allItems[last].addEventListener('dblclick', () => {
+    if (allItems[last].classList.contains('completed')) {
+      allItems[last].classList.remove('completed');
+    } else {
+      allItems[last].classList.add('completed');
+    }
+  });
+}
+
 function checkIfSelected() {
   const alreadySelected = document.querySelector('.selected');
   if (alreadySelected !== null) {
@@ -9,14 +20,12 @@ function checkIfSelected() {
   }
 }
 
-function SelectItem() {
-  const allItems = document.querySelectorAll('li');
-  for (let i = 0; i < allItems.length; i += 1) {
-    allItems[i].addEventListener('click', () => {
-      checkIfSelected();
-      allItems[i].classList.add('selected');
-    });
-  }
+function SelectItem(allItems) {
+  const last = allItems.length - 1;
+  allItems[last].addEventListener('click', () => {
+    checkIfSelected();
+    allItems[last].classList.add('selected');
+  });
 }
 
 function addElement() {
@@ -25,7 +34,9 @@ function addElement() {
     listItem.innerHTML = text.value;
     text.value = '';
     listMom.appendChild(listItem);
-    SelectItem();
+    const allItems = document.querySelectorAll('li');
+    SelectItem(allItems);
+    completeItem(allItems);
   });
 }
 
