bf010e6f010e6f879e1e4528c57c990461f4ec06
paulomichael <paulo.michael@gmail.com>
2021-05-06 15:35:21 -0300

    requisito 3 - adiciona caixa de input com id "texto-tarefa" pra entrada das tarefas

--- script.js
+++ script.js
@@ -25,3 +25,16 @@ function adicionaParagrafoFuncionamento () {
     paragrafo.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
 }
 adicionaParagrafoFuncionamento();
+
+
+/* 3 - Adicione um input com o id="texto-tarefa" onde a pessoa usuária poderá digitar o nome do item que deseja adicionar à lista
+O que será verificado:
+
+Será verificada a existência de um elemento do tipo input com o id texto-tarefa. */
+
+function inputTextoTarefa () {
+    let input = document.createElement('input');
+    input.id = 'texto-tarefa';
+    document.body.appendChild(input);
+}
+inputTextoTarefa();
