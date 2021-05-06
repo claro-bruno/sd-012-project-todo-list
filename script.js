3407a443413e4aecfa47807311ca2ad5dc879eef
Patrick Dack <patrickdack89@gmail.com>
2021-05-06 17:11:33 -0300

    Requisito 7 - Foi preciso chamar a funçao dentro do evento Adicionar, pois os itens da lista ainda nao estao criados quando a pagina se inicia.

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const input = document.getElementById('texto-tarefa');
 const btnAdd = document.getElementById('criar-tarefa');
-// Captura o texto do input e adiciona a lista
-function catchInput () {  
+// Captura o texto do input e adiciona à lista
+function catchInput() {
   btnAdd.addEventListener('click', () => {
-    let taskList = document.getElementById('lista-tarefas');
-    let createLi = document.createElement('li');
+    const taskList = document.getElementById('lista-tarefas');
+    const createLi = document.createElement('li');
+    createLi.className = 'item-list';
     createLi.innerHTML = input.value;
     taskList.appendChild(createLi);
     input.value = '';
-  });      
+    selectItem();
+  });
 }
 catchInput();
+// Altera a cor de fundo do item selecinado
+function selectItem() {
+  const itemList = document.querySelectorAll('.item-list');
+  for (let item of itemList) {
+    item.addEventListener('click', () => {
+      item.style.backgroundColor = 'rgb(128, 128, 128)';
+    })
+  }
+}
