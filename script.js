600a5f00a770b5c9c64801511859edb10d2a1f39
paulomichael <paulo.michael@gmail.com>
2021-05-06 15:31:20 -0300

    requisito 2 - adiciona paragrafo com id funcionamento e texto "Clique duas vezes em um item para marcá-lo como completo"

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ function adicionaTitulo () {
     header.appendChild(h1);
 }
 adicionaTitulo();
+
+
+/* 2 - Adicione abaixo do título um pequeno e discreto parágrafo com id="funcionamento" e com o texto "Clique duas vezes em um item para marcá-lo como completo"
+O que será verificado:
+
+Será verificado que existe na sua página um elemento com o id funcionamento com o conteúdo Clique duas vezes em um item para marcá-lo como completo */
+
+function adicionaParagrafoFuncionamento () {
+    let paragrafo = document.createElement('p');
+    document.body.appendChild(paragrafo);
+    paragrafo.id = 'funcionamento';
+    paragrafo.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
+}
+adicionaParagrafoFuncionamento();
