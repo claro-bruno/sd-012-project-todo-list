d0338cff70d9043924421f118469cc63a6d2d741
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-06 19:00:09 -0300

    requisito 5 com funçao preventDefault

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const inputButton = document.getElementById("texto-tarefa");
 const taskList = document.getElementById("lista-tarefas");
+const input = document.getElementById("criar-tarefa");
 
 function createTask() {
     let taskBoard = document.createElement("li");
     taskBoard.className = "task";
     taskBoard.innerHTML = inputButton.value;
     taskList.appendChild(taskBoard);
+    inputButton.innerHTML = '';
 }   
 
-function clearBox() {
-    inputButton.innerHTML = '';
-};
+input.addEventListener('click', (event)=> {
+    event.preventDefault();
+    createTask();
+});
+
+// Em pesquisa foi localizada a função preventDefault(), evitando que o envio da tarefa resete o formulário, gravando seu conteúdo; acessado em (https://www.w3schools.com/jsref/event_preventdefault.asp)
+
