584fa90267fc07c9aef1920079aa9436d0621396
tiagoornelas <advtiagoornelas@gmail.com>
2021-05-06 17:23:53 -0300

    Resolve requisitos 5 e 6

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+
+    const addButton = document.querySelector("#add-button");
+    const inputBox = document.querySelector("#input-box");
+    const createButton = document.querySelector("#criar-tarefa");
+    const input = document.querySelector('input');
+    const list = document.querySelector('ol');
+
+    // Dynamic Input Field
+    addButton.addEventListener('click', function () {
+        inputBox.style.display = 'block'; 
+    });
+
+    // Task Add by Input
+    createButton.addEventListener('click', function () {
+        let inputText = input.value;
+        let task = document.createElement('li');
+        task.innerHTML = inputText;
+        list.appendChild(task);
+        inputBox.style.display = 'none';
+    })
+}
