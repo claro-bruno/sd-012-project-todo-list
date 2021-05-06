4ba7af7fa062341b1c5090f7400158fcb5b21a7d
Gabriel Bueno <gabrielsbueno@hotmail.com>
2021-05-06 17:50:49 -0300

    Requisitos 09

--- script.js
+++ script.js
@@ -11,13 +11,21 @@ const adicionaTarefa = () => {
 };
 
 // Seleciona a tarefa
-
 const selecionaTarefa = (event) => {
   const capturaLi = document.querySelectorAll('.tarefa');
   for (let index = 0; index < capturaLi.length; index += 1) {
     capturaLi[index].classList.remove('selecionada');
   }
   event.target.classList.add('selecionada');
+};
+
+// Risca tarefas completas
+const completaTarefa = (event) => {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
 };
 
 // Adiciona Eventos
@@ -27,6 +35,12 @@ document.addEventListener('click', (event) => {
   }
   if (event.target.className === ('tarefa')) {
     selecionaTarefa(event);
+  }
+});
+
+document.addEventListener('dblclick', (event) => {
+  if (event.target.classList.contains('tarefa')) {
+    completaTarefa(event);
   }
 });
 
