561ac6a0b2f9ffb872922eaeae67a4945dc575f7
Renan_Miyashita <renanmiyashita@gmail.com>
2021-05-06 19:12:27 -0300

    pequenas alteraçoes porem nenhum outro requisito feito

--- script.js
+++ script.js
@@ -17,14 +17,31 @@ button.addEventListener('click', () => {
         if (input.value != '') {
             input.value = '';
         };
-        createList.addEventListener('click', changeBColor)
+
+        createList.addEventListener('click', changeBackColor)   
+
+        
 });
 
-function changeBColor(event) {
+    function changeBackColor(event) {
+
+    
     let target = event.target;
     target.style.backgroundColor = 'rgb(128,128,128)';
+    let targetColor = target.style.backgroundColor = 'rgb(128,128,128)';
     
-};
+    };
 
 
+
+/*
+    EX 8 - tentativa de so deixar 1 elemento selecionado
+if (targetColor != 'white') {
+    for (let item of target.parentNode.children) {
+        console.log ('1')
+    targetColor = target.style.removeProperty('backgroundColor')
+    }
+    targetColor;
+}
+*/
 
