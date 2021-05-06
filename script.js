263c25b1f36033f763c98e153cbc0d0425477842
Bruno Yamamoto <bes.yamamoto@gmail.com>
2021-05-06 16:31:49 -0300

    Feito o requisito 7, 6 completou junto do 5

--- script.js
+++ script.js
@@ -2,12 +2,30 @@ const criarButton = document.querySelector('#criar-tarefa');
 const text = document.querySelector('#texto-tarefa');
 const listMom = document.querySelector('#lista-tarefas');
 
+function checkIfSelected() {
+  const alreadySelected = document.querySelector('.selected');
+  if (alreadySelected !== null) {
+    alreadySelected.classList.remove('selected');
+  }
+}
+
+function SelectItem() {
+  const allItems = document.querySelectorAll('li');
+  for (let i = 0; i < allItems.length; i += 1) {
+    allItems[i].addEventListener('click', () => {
+      checkIfSelected();
+      allItems[i].classList.add('selected');
+    });
+  }
+}
+
 function addElement() {
   criarButton.addEventListener('click', () => {
     const listItem = document.createElement('li');
     listItem.innerHTML = text.value;
     text.value = '';
     listMom.appendChild(listItem);
+    SelectItem();
   });
 }
 
