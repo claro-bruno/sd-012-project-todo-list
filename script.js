20b8cae2c9fed55cbac472d189d0138d146896af
Adriana Biberg <adribiberg@hotmail.com>
2021-05-06 18:01:03 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -5,17 +5,25 @@ function createList() {
   const inputToDo = document.getElementById('texto-tarefa');
   const newLi = document.createElement('li');
   newLi.classList.add('tarefa');
+  newLi.classList.add('selected');
   orderList.appendChild(newLi);
   newLi.textContent = inputToDo.value;
   inputToDo.value = '';
 
+  // Parte 7 resolvida com auxílio do colega Rodrigo Facury:
   newLi.addEventListener('click', (event) => {
     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-  })
+  // 
+    const selected = document.querySelectorAll('.selected');
+    for (let index = 0; index < selected.length; index += 1) {
+      selected[index].classList.remove('selected');
 
-  newLi.addEventListener('dblclick', (event) => {
-    event.target.style.backgroundColor = 'white';
-  })
+      if (selected[index] !== 'selected') {
+        selected[index].style.backgroundColor = 'white';
+      }
+    }
+    event.target.classList.add('selected');
+  });
 }
 
 buttonToDo.addEventListener('click', createList);
