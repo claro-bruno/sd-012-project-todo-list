9489393384440914f525c01fd81f14a57372cb9b
Ramon Paschoal <rpaschoal90@gmail.com>
2021-05-06 19:39:24 -0300

    Exercicio 8 e 9

--- script.js
+++ script.js
@@ -11,13 +11,26 @@ let selectOl = document.getElementById('lista-tarefas');
 
 function capturar () {
     botao.addEventListener('click', function (event) {
-        let criarLista = document.createElement('li');
-        criarLista.addEventListener('click', function() {
-            criarLista.style.backgroundColor = 'rgb(128, 128, 128)'
-        })
+        let createList = document.createElement('li');
+        createList.addEventListener('click', function(event) { // Exercício 7 e 8
+            let teste = document.querySelector('.selected');
+            if (teste) {
+                teste.classList.remove('selected');
+                teste.style.backgroundColor = '';
+            } 
+            event.target.classList.add('selected');
+            event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+
+            // createList.classList.add('selected');
+            // event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+            // if(createList.classList.contains('selected')) {
+            //     event.target.classList.remove('selected');
+            // } 
+        });
+
         let valorCapturado = inputSelect.value;
-        criarLista.innerHTML = valorCapturado;
-        selectOl.appendChild(criarLista);
+        createList.innerHTML = valorCapturado;
+        selectOl.appendChild(createList);
 
 
         function clearInput () { // Exercício 6
@@ -31,8 +44,3 @@ function capturar () {
 };
 
 capturar ();
-
-//================================================================================
-
-//Exercício 7
-
