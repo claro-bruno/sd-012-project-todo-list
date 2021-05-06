ca41e32cd82a0559e4e167e4bb8acd8b1ed0693a
Renan_Miyashita <renanmiyashita@gmail.com>
2021-05-06 15:42:02 -0300

    requisitos 3 e 4

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+let input = document.querySelector('#texto-tarefa');
+let button = document.querySelector('#criar-tarefa');
+let divList = document.querySelector('.listHolder');
+
+button.addEventListener('click', () => {
+    let ol = divList.querySelector('ol');
+    let li = document.createElement('li');
+    ol.appendChild(li);
+});
+    
+
 
 
