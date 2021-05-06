b72f5c2fda7c9080fca3cbb5b382ae14c0594ab8
Rodrigo <rodrigo.facury14@gmail.com>
2021-05-06 20:21:17 -0300

    requisito 8 agora vai mesmo!!

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const input = document.querySelector('#texto-tarefa');
 const button = document.querySelector('#criar-tarefa');
 const list = document.querySelector('#lista-tarefas');
+const selected = document.querySelector('.selected');
+
+function changeBgColor(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  event.target.classList.add('selected');
+}
+
+function changeSelected() {
+  for (index = 0; index < list.children.length; index += 1) {
+    if (list.children[index].className = 'selected') {
+      list.children[index].classList.remove('selected');
+      list.children[index].style.backgroundColor = '';
+    }
+  }
+  
+}
 
 function createItem() {
   const newItem = document.createElement('li');
@@ -9,17 +25,6 @@ function createItem() {
   newItem.innerHTML = input.value;
   list.appendChild(newItem);
   input.value = '';
-  
-  function changeBgColor(event) {
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-    event.target.classList.add('selected');
-  }
-  
-  function changeSelected() {
-    const selected = document.querySelector('.selected');
-    selected.style.backgroundColor = '';
-    selected.classList.remove('selected');
-  }
 }
 
 button.addEventListener('click', createItem);
