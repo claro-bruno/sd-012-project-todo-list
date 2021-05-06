796004bd0cb414f99b8eb6069c1cf965292e0101
Rodrigo <rodrigo.facury14@gmail.com>
2021-05-06 17:24:44 -0300

    requisito 7

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ const list = document.querySelector('#lista-tarefas');
 
 function createItem() {
   const newItem = document.createElement('li');
+  newItem.addEventListener('click', changeItemBgColor);
   newItem.innerHTML = input.value;
   list.appendChild(newItem);
   input.value = '';
 }
 
 button.addEventListener('click', createItem);
+
+function changeItemBgColor(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
