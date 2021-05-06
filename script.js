8ad7e252ac3d1770dc8990675b877bc881b5070a
camila-mp <ca.malvp@gmail.com>
2021-05-06 19:36:24 -0300

    tenta resolver requisito 9

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 
-//armazenando elementos uteis em variaveis
+//armazenando elementos uteis em variaveis//
 let listaTarefas = document.getElementById('lista-tarefas');
 let botaoCriarTarefa = document.getElementById('criar-tarefa');
 let elementoInput = document.getElementById('texto-tarefa');
 
-//criando uma div para guardar texto do botao
+//criando uma div para guardar texto do botao//
 let divBotao = document.createElement('div');
 botaoCriarTarefa.appendChild(divBotao);
 divBotao.id = 'texto-botao'
 divBotao.innerHTML = 'Adicionar'
 
-//REQUISITO 5 e 6
-//função que cria event listener no botao, adiciona value na lista e limpa input
+//REQUISITO 5 e 6//
+//função que cria event listener no botao, adiciona value na lista e limpa input//
 
 function criaEventoBotao() {
     botaoCriarTarefa.addEventListener('click', function() {
@@ -25,7 +25,7 @@ function criaEventoBotao() {
 
 criaEventoBotao();
 
-//REQUISITO 7
+//REQUISITO 7 e 8//
 
 function mudaCorItem() {
     let listaTarefas = document.getElementById('lista-tarefas');
@@ -39,3 +39,21 @@ function mudaCorItem() {
 };
 
 mudaCorItem();
+
+//REQUISITO 9//
+
+//função para riscar elemento com double-click e des-riscar com double-click//
+
+function riscaItem() {
+    let listaTarefas = document.getElementById('lista-tarefas');
+    let listaLis = document.getElementsByClassName('item-lista');
+    listaTarefas.addEventListener('dblclick', function(event) {
+        if (event.target.className === 'item-lista completed'){
+            event.target.className = 'item-lista';
+        }else{
+            event.target.className = 'item-lista completed'
+        };
+    });
+};
+
+riscaItem();
