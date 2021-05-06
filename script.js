d4ce5d04abe1aea38328f052da41115abbd15a18
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-06 15:57:34 -0300

    BONUS | Requisito 12

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ const inputTask = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const removeFinishedTasksButton = document.getElementById('remover-finalizados');
 const removeAllButton = document.querySelector("#apaga-tudo");
+const saveTasksButton = document.querySelector('#salvar-tarefas');
 
-newTaskButton.addEventListener('click', () => {
+function loadtasks() {
+  let tasksArray = JSON.parse(localStorage.getItem('tasksArray'));
+  for (let index = 0; index < tasksArray.length; index += 1) {
+    createTask(tasksArray[index]);
+  }
+};
+
+loadtasks();
+
+function deselectOtherLis() {
+  const li = document.querySelectorAll('li');
+  for (let index = 0; index < li.length; index += 1) { // refatorar para retirar o loop e retirar a seleção apenas do elemento com classe selected
+    li[index].style.backgroundColor = 'white';
+    li[index].classList.remove('selected');
+  }
+}
+
+function createTask(taskString) {
   const li = document.createElement('li');
-  li.innerHTML = inputTask.value;
-  inputTask.value = '';
+  li.innerHTML = taskString;
   li.addEventListener('click', (event) => {
     deselectOtherLis();
     const targ = event.target;
@@ -24,6 +41,11 @@ newTaskButton.addEventListener('click', () => {
     targ.classList.toggle('completed');
   });
   taskList.appendChild(li);
+};
+
+newTaskButton.addEventListener('click', () => {
+  createTask(inputTask.value);
+  inputTask.value = '';
 });
 
 removeFinishedTasksButton.addEventListener('click', () => {
@@ -42,12 +64,13 @@ removeAllButton.addEventListener('click', () => {
   }
 });
 
-function deselectOtherLis() {
+saveTasksButton.addEventListener('click', () => {
   const li = document.querySelectorAll('li');
-  for (let index = 0; index < li.length; index += 1) { // refatorar para retirar o loop e retirar a seleção apenas do elemento com classe selected
-    li[index].style.backgroundColor = 'white';
-    li[index].classList.remove('selected');
+  let tasksArray = [];
+  for (let index = 0; index < li.length; index += 1) {
+    tasksArray.push(li[index].innerHTML);
   }
-}
+  localStorage.setItem('tasksArray', JSON.stringify(tasksArray));
+});
 
 
