24f7bf2266c0f4c141bcc570a0992e1e701c44f0
David Azevedo <zevid.dev@outlook.com>
2021-05-06 15:50:03 -0300

    Done requirement 5,6

---
+++ script.js
@@ -0,0 +1,23 @@
+function createButton() {
+  let createButtonSubmit = document.createElement('button');
+  createButtonSubmit.id = 'criar-tarefa';
+  createButtonSubmit.innerHTML = 'Criar Tarefa';
+  document.querySelector('#container-submit').appendChild(createButtonSubmit);
+}
+createButton();
+
+function createListItem() {
+  let createItemList = document.createElement('li');
+  let contentInput = document.querySelector('#texto-tarefa').value;
+  createItemList.innerHTML = contentInput;
+  document.querySelector('#lista-tarefas').appendChild(createItemList);
+}
+
+function clearInput() {
+  let contentInput = document.querySelector('#texto-tarefa');
+  contentInput.value = '';
+}
+
+let buttonSubmit = document.querySelector('#criar-tarefa');
+buttonSubmit.addEventListener('click', createListItem);
+buttonSubmit.addEventListener('click', clearInput);
