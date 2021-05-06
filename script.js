16d77e783d8a7d256717865e3e47051d81e83de2
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-06 17:39:39 -0300

    Requirement 09-> Add line in task when double-clicked

--- script.js
+++ script.js
@@ -1,26 +1,40 @@
-window.onload = function page() {
-  const btnTaskCreate = document.querySelector('#criar-tarefa');
-  const textInput = document.querySelector('#texto-tarefa');
-  const taskList = document.querySelector('#lista-tarefas');
+const btnTaskCreate = document.querySelector('#criar-tarefa');
+const textInput = document.querySelector('#texto-tarefa');
+const taskList = document.querySelector('#lista-tarefas');
 
-  function taskCreator() {
-    const taskItem = document.createElement('li');
-    taskItem.className = 'tarefa';
-    taskItem.innerText = textInput.value;
-    textInput.value = '';
-    taskList.appendChild(taskItem);
-  }
+function taskCreator() {
+  const taskItem = document.createElement('li');
+  taskItem.className = 'tarefa';
+  taskItem.innerText = textInput.value;
+  textInput.value = '';
+  taskList.appendChild(taskItem);
+}
 
-  btnTaskCreate.addEventListener('click', taskCreator);
-
-  document.addEventListener('click', function (event) {
-    if (event.target.classList.contains('tarefa')) {
-      const currentTaskSelected = document.querySelector('.selected');
-      console.log(currentTaskSelected);
-      if (currentTaskSelected !== null) {
-        currentTaskSelected.classList.remove('selected');
-      }
-      event.target.classList.add("selected")
+function selectTask(event) {
+  const targetedEvent = event.target;
+  if (targetedEvent.classList.contains('tarefa')) {
+    const currentTaskSelected = document.querySelector('.selected');
+    if (currentTaskSelected !== null) {
+      currentTaskSelected.classList.remove('selected');
     }
-  })
+    targetedEvent.classList.add('selected');
+  }
+}
+
+function completeTask(event) {
+  const targetedEvent = event.target;
+  if (targetedEvent.classList.contains('tarefa')) {
+    console.log('Pra quem fica minha...');
+    if (targetedEvent.classList.contains('completed')) {
+      targetedEvent.classList.remove('completed');
+    } else {
+      targetedEvent.className += ' completed';
+    }
+  }
+}
+
+window.onload = function page() {
+  btnTaskCreate.addEventListener('click', taskCreator);
+  document.addEventListener('click', selectTask);
+  document.addEventListener('dblclick', completeTask);
 };
