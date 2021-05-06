c0ebba3050a6c10b3b91e49fc64273cb20b0d5be
André Moreno <deco616@hotmail.com>
2021-05-06 17:13:03 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let buttonAdicionar = document.getElementById('criar-tarefa');
 let listaTarefas = document.getElementById('lista-tarefas');
 let inputTarefas = document.getElementById('texto-tarefa');
+let elementosDaLista = listaTarefas.childNodes;
 
 buttonAdicionar.addEventListener('click', () => {
   let criarLi = document.createElement('li');
@@ -8,4 +9,17 @@ buttonAdicionar.addEventListener('click', () => {
   listaTarefas.appendChild(criarLi);
   //listaTarefas.insertBefore(criarLi, listaTarefas.childNodes[0]);
   inputTarefas.value = '';
+  marcarTarefa();
 })
+
+function marcarTarefa() {
+  for (let n = 0; n < elementosDaLista.length; n += 1) {
+    elementosDaLista[n].addEventListener('click', (event) => {
+      let element = document.querySelector('.selected');
+      if (element != null) {
+        element.classList.remove('selected');
+      }
+      event.target.classList.add('selected');
+    })
+  }
+}
