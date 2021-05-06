27d420217aa3737fd79c26c6b6da95c8cf82ebc0
vinicius-patriota <vinicius.patriota@outlook.com.br>
2021-05-06 18:16:01 -0300

    Completos requisitos ate 9

--- script.js
+++ script.js
@@ -5,8 +5,28 @@ let lista = document.getElementById("lista-tarefas");
 document.addEventListener("click", function (event) {
     if (event.target == btn) {
         let item = document.createElement("li");
+        item.className = "item";
         item.innerText = input.value;
         input.value = "";
         lista.appendChild(item);
+    }
+
+    if (event.target.classList.contains("item")) {
+        let items = document.getElementsByClassName("item");
+        for (let index = 0; index < items.length; index += 1) {
+            items[index].style.backgroundColor = "white";
+        }
+        event.target.style.backgroundColor = "rgb(128,128,128)";
+    }
+})
+
+document.addEventListener("dblclick", function (event) {
+    if (event.target.classList.contains("item") && event.target.classList.contains("completed")) {
+        event.target.style.textDecoration = "none";
+        event.target.classList.remove("completed");
+    }
+    else {
+        event.target.style.textDecoration = "line-through";
+        event.target.classList.add("completed")
     }
 })
