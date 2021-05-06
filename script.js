6b585ea452753f98b806ea0c6340e8b91bcb1b16
Fabio Ryudi <ryu_fabio@hotmail.com>
2021-05-06 18:53:08 -0300

    adiciona requisito 8 para teste

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const buttonAdd = document.getElementById('criar-tarefa')
 const textInput = document.getElementById('texto-tarefa')
 const list  = document.getElementById('lista-tarefas')
 const inputValue = textInput.value 
+const listFirstItem = document.getElementsByTagName('ol')[0];
+const liListItems = document.getElementsByTagName('li');
 
 
 function createList () {
@@ -11,6 +13,37 @@ function createList () {
     const inputValue = textInput.value 
     addTextItem.innerText = inputValue
     textInput.value = "";
+    addTextItem.classList.add('list-item')
   });
 }
 createList()
+
+// Requisito 7  Minha nossa, nem eu acredito que esse trem funcionou 
+
+function grayClick () {
+  listFirstItem.addEventListener('click', (event) => {
+    const clickItem = event.target
+      for (let index= 0; index < liListItems.length; index += 1){
+        liListItems[index].style.backgroundColor = document.body.style.backgroundColor;
+          if (clickItem.localName = 'li') { 
+            clickItem.style.backgroundColor = 'rgb(128,128,128)'
+        }
+      };
+  });
+}
+grayClick()
+
+// requisito 8
+
+function lineThrough () {
+  list.addEventListener('dblclick', (event) => {
+    const dblclickItem = event.target;
+    if (dblclickItem.classList.contains('completed')) { 
+        dblclickItem.classList.remove('completed');
+      } else {
+        dblclickItem.classList.add('completed');
+      };
+    });
+  
+}
+lineThrough ()
