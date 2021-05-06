9dfbc01a065b9593dfad71a5a57b83172ebbf2c3
Fabio Ryudi <ryu_fabio@hotmail.com>
2021-05-06 16:32:54 -0300

    adiciona requisito 6 para teste

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const buttonAdd = document.getElementById('criar-tarefa')
 const textInput = document.getElementById('texto-tarefa')
 const list  = document.getElementById('lista-tarefas')
+const inputValue = textInput.value 
+
 
 function createList () {
   buttonAdd.addEventListener('click', (event) => {
     const addTextItem = document.createElement('li')
     list.appendChild(addTextItem)
     const inputValue = textInput.value 
-    addTextItem.innerHTML = inputValue
-  })
+    addTextItem.innerText = inputValue
+    textInput.value = "";
+  });
 }
 createList()
