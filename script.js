95bbc86440b83ceaff4ddf07e38b5cbff2a9a57e
Adriana Biberg <adribiberg@hotmail.com>
2021-05-06 17:31:19 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const buttonToDo = document.querySelector('#criar-tarefa');
-const inputToDo = document.querySelector('#texto-tarefa');
 const orderList = document.querySelector('#lista-tarefas');
-
 
 function createList() {
   const inputToDo = document.getElementById('texto-tarefa');
   const newLi = document.createElement('li');
+  newLi.classList.add('tarefa');
   orderList.appendChild(newLi);
   newLi.textContent = inputToDo.value;
   inputToDo.value = '';
+
+  newLi.addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  })
+
+  newLi.addEventListener('dblclick', (event) => {
+    event.target.style.backgroundColor = 'white';
+  })
 }
 
 buttonToDo.addEventListener('click', createList);
