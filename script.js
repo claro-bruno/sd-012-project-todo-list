f252d412892fdb3785222512c7c39c3716530ccf
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-06 18:26:22 -0300

    adding requirement 11, refactoring requirement 10

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const inputTarefas = document.querySelector('#texto-tarefa');
 const buttonNovaTarefa = document.querySelector('#criar-tarefa');
 const listaTarefas = document.querySelector('#lista-tarefas');
+const buttonApagaLista = document.querySelector('#apaga-tudo');
+const buttonApagaFinalizados = document.querySelector('#remover-finalizados');
+
+function riscaItemDaLista(element) {
+  element.addEventListener('dblclick', (event) => {
+    event.target.classList.toggle('completed');
+  });
+}
+
+function mudaCorDeFundo(elemento) {
+  elemento.addEventListener('click', (event) => { 
+    const itensDalista = document.querySelectorAll('.item-lista');
+    for (let index = 0; index < itensDalista.length; index += 1) {
+      if (event.target !== itensDalista[index]) itensDalista[index].classList.remove('selected');
+    }
+    event.target.classList.toggle('selected');
+  });
+}
 
 function criandoNovaTarefa() {
   buttonNovaTarefa.addEventListener('click', () => {
@@ -17,28 +35,22 @@ function criandoNovaTarefa() {
 
 criandoNovaTarefa();
 
-function mudaCorDeFundo(elemento) {
-  elemento.addEventListener('click', (event) => { 
+function apagaFinalizados() {
+  buttonApagaFinalizados.addEventListener('click', () => {
+    const tarefasFinalizadas = document.querySelectorAll('.completed');
+    for (let index = 0; index < tarefasFinalizadas.length; index += 1) {
+      listaTarefas.removeChild(tarefasFinalizadas[index]);
+    }
+  });
+}
+
+apagaFinalizados();
+
+function apagaItensDaLista() {
+  buttonApagaLista.addEventListener('click', () => {
     const itensDalista = document.querySelectorAll('.item-lista');
     for (let index = 0; index < itensDalista.length; index += 1) {
-      if(event.target !== itensDalista[index]) itensDalista[index].classList.remove('selected');  
+      listaTarefas.removeChild(itensDalista[index]);
     }
-    event.target.classList.toggle('selected');
   });
 }
-
-function riscaItemDaLista(element) {
-  element.addEventListener('dblclick', (event) => {
-    event.target.classList.toggle('completed');
-  });
-}
-
-const buttonApagaLista= document.querySelector('#apaga-tudo');
-
-function apagaItensDaLista(element) {
-  buttonApagaLista.addEventListener('click', () => {
-      listaTarefas.removeChild(element);
-    });
-}
-
-
