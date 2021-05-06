e0596d9543f5edd3fb19de68eca9618ab16471ce
GMODias <gmd.delta@gmail.com>
2021-05-06 17:12:51 -0300

    [PT-BR] Atendendo aos requisito 5,6 e 7 [EN] Solved 5th, 6th and 7th requirements.

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ const createBtn = document.getElementById('criar-tarefa');
 const InputTask = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 
-createBtn.addEventListener('click', createTask)
+createBtn.addEventListener('click', createTask);
 
 function createTask() {
   let task = document.createElement("LI");
   task.innerText = InputTask.value;
   InputTask.value = '';
   taskList.appendChild(task);
+}
+
+taskList.addEventListener('click', selectTask);
+
+function selectTask(event) {
+  let tasks = document.getElementsByTagName('LI')
+  for (index = 0; index < tasks.length; index +=1) {
+    tasks[index].style.backgroundColor = 'rgb(255,255,255)';
+    console.log(tasks[index].style);
+  }
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
