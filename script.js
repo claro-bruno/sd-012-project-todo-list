fa9e7a6bb5b5ce100b575ee9501c785f725d01a5
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-05-06 15:15:03 -0300

    conclusao do Desafio_03 e Desafio_04, onde um input com id texto-tarefa e um ol com id lista-tarefas.

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const bodyPagina = document.body;
+
 const pIdFuncionamento = document.createElement('p');
 pIdFuncionamento.id = 'funcionamento';
 pIdFuncionamento.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 bodyPagina.appendChild(pIdFuncionamento);
+
+const inputIdTextoTarefa = document.createElement('input');
+inputIdTextoTarefa.id = 'texto-tarefa';
+bodyPagina.appendChild(inputIdTextoTarefa);
+
+const olIdListaTarefa = document.createElement('ol');
+olIdListaTarefa.id = 'lista-tarefas';
+bodyPagina.appendChild(olIdListaTarefa);
