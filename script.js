8ace458c2c9b42f4505a7d187097e5bb61bd63ba
Rodrigo <rodrigo.facury14@gmail.com>
2021-05-06 18:45:30 -0300

    requisito 8 quarta tentativa

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-window.onload = function () {
+const input = document.querySelector('#texto-tarefa');
+const button = document.querySelector('#criar-tarefa');
+const list = document.querySelector('#lista-tarefas');
 
-  const input = document.querySelector('#texto-tarefa');
-  const button = document.querySelector('#criar-tarefa');
-  const list = document.querySelector('#lista-tarefas');
+window.onload = function () {
 
   function changeBgColor(event) {
     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
@@ -14,16 +14,15 @@ window.onload = function () {
     selected.style.backgroundColor = '';
     selected.classList.remove('selected');
   }
+}  
 
-  function createItem() {
-    const newItem = document.createElement('li');
-    newItem.addEventListener('click', changeSelected);
-    newItem.addEventListener('click', changeBgColor);
-    newItem.innerHTML = input.value;
-    list.appendChild(newItem);
-    input.value = '';
-  }
-
-  button.addEventListener('click', createItem);
-
+function createItem() {
+  const newItem = document.createElement('li');
+  newItem.addEventListener('click', changeSelected);
+  newItem.addEventListener('click', changeBgColor);
+  newItem.innerHTML = input.value;
+  list.appendChild(newItem);
+  input.value = '';
 }
+
+button.addEventListener('click', createItem);
