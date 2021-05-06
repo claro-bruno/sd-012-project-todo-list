a520a8a04d95636861f3d4f4251f1c534ffffa5d
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-06 15:44:13 -0300

    corrige algumas coisas do linter

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let tarefaNova = document.querySelector('#texto-tarefa');
-let listaDeTarefas = document.querySelector('#lista-tarefas');
-let botaoAddTarefa = document.querySelector('#criar-tarefa');
-function addTarefa () {
+const tarefaNova = document.querySelector('#texto-tarefa');
+const listaDeTarefas = document.querySelector('#lista-tarefas');
+const botaoAddTarefa = document.querySelector('#criar-tarefa');
+
+function addTarefa() {
     if (tarefaNova.value.length > 0) {
-        let tarefaAdicionada = document.createElement('li');
+        const tarefaAdicionada = document.createElement('li');
         tarefaAdicionada.innerText = tarefaNova.value;
         listaDeTarefas.appendChild(tarefaAdicionada);
         tarefaNova.value = '';
     }
-}; botaoAddTarefa.addEventListener('click', addTarefa);
+} botaoAddTarefa.addEventListener('click', addTarefa);
