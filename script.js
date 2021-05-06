7e1653312d2c5fdfc961923145f103999c59e816
Gabriel Takaki <gabitj@icloud.com>
2021-05-06 16:23:34 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -5,10 +5,21 @@ const olEl = document.getElementById('lista-tarefas');
 function addTask() {
     addTaskBtn.addEventListener('click', () => {
         if(input.value !== '') {
+
             const taskLi = document.createElement('li');
             taskLi.innerText = input.value
+            taskLi.className = 'taskLi';
             olEl.appendChild(taskLi);
             input.value = '';
+
+            // Desafio 7
+            const tasks = document.querySelectorAll('.taskLi');
+            for(let index = 0; index < tasks.length; index += 1) {
+                tasks[index].addEventListener('click', () => {
+                    tasks[index].style.backgroundColor = 'rgb(128, 128, 128)';
+                })
+            }
+
         } else {
             alert('Preencha com alguma tarefa');
         }
