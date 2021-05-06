0923b18a3b92d2b9a762371c823953a129f15ab4
Rodrigo <rodrigo.facury14@gmail.com>
2021-05-06 19:13:01 -0300

    requisito 8 quinta tentativa

--- script.js
+++ script.js
@@ -2,20 +2,6 @@ const input = document.querySelector('#texto-tarefa');
 const button = document.querySelector('#criar-tarefa');
 const list = document.querySelector('#lista-tarefas');
 
-window.onload = function () {
-
-  function changeBgColor(event) {
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-    event.target.classList.add('selected');
-  }
-
-  function changeSelected() {
-    const selected = document.querySelector('.selected');
-    selected.style.backgroundColor = '';
-    selected.classList.remove('selected');
-  }
-}  
-
 function createItem() {
   const newItem = document.createElement('li');
   newItem.addEventListener('click', changeSelected);
@@ -26,3 +12,14 @@ function createItem() {
 }
 
 button.addEventListener('click', createItem);
+
+function changeBgColor(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  event.target.classList.add('selected');
+}
+
+function changeSelected() {
+  const selected = document.querySelector('.selected');
+  selected.style.backgroundColor = '';
+  selected.classList.remove('selected');
+}
