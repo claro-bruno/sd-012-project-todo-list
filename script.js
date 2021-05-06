889b566bd881b9e51db31d90923a5fdb1c0eb4ca
Wellington <junior.wel02@exemplo.br>
2021-05-06 16:59:57 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 let botao = document.querySelector('#criar-tarefa');
-let li = document.querySelector('ol');
+let li = document.querySelectorAll('ol');
 function adicionarTarefa() {
   let caixaTexto = document.querySelector('#texto-tarefa');
   let lista = document.getElementById('lista-tarefas');
   let criarLi = document.createElement('li');
   criarLi.textContent = caixaTexto.value;
+  criarLi.className = 'tasks';
   lista.appendChild(criarLi);
   caixaTexto.value = '';
 }
 
 botao.addEventListener('click', adicionarTarefa);
 
-li.addEventListener('click', function (event) {
-  event.target.id = 'corSelecionada';
-});
+for (let i = 0; i < li.length; i += 1) {
+  li[i].addEventListener('click', function (event) {
+    if (event.target.className == 'tasks') {
+      if (document.querySelector('#corSelecionada') !== null) {
+        document.querySelector('#corSelecionada').removeAttribute('id');
+      }
+      event.target.id = 'corSelecionada';
+    }
+  });
+}
+
