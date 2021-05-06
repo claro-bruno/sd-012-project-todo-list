38055d417ad72b30a28c1db331adc6f7e4e4827e
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-06 10:42:37 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -7,16 +7,25 @@ newTaskButton.addEventListener('click', () => {
   li.innerHTML = inputTask.value;
   inputTask.value = '';
   taskList.appendChild(li);
-  colorLi();
+  selectLi();
 });
 
-function colorLi() {
+function selectLi() {
   const li = document.querySelectorAll('li');
   for (let index = 0; index < li.length; index += 1) {
     li[index].addEventListener('click', (event) => {
+      deselectOtherLis();
       const targ = event.target;
-      console.log(targ);
+      targ.className = 'selected';
       targ.style.backgroundColor = 'rgb(128, 128, 128)';
     });
   }
 };
+
+function deselectOtherLis() {
+  const li = document.querySelectorAll('li');
+  for (let index = 0; index < li.length; index += 1) {
+    li[index].style.backgroundColor = 'white';
+    li[index].classList.remove('selected');
+  }
+}
