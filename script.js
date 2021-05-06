4215d5326a7d5e9bfd8fe80dbac5d0cab38a1c43
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-06 16:57:16 -0300

    adding few changes, no requirement finished

--- script.js
+++ script.js
@@ -17,22 +17,32 @@ criandoNovaTarefa();
 function gerandoListadeItens() {
   buttonNovaTarefa.addEventListener('click', () => {
     const itensDaLista = document.querySelectorAll('.item-lista');
+    console.log(itensDaLista);
     mudaCorDeFundo(itensDaLista);
+    riscaItemDaLista(itensDaLista);
   });
 }
 
 gerandoListadeItens();
 
 function mudaCorDeFundo(elements) {
-  console.log(elements.length);
   for (let index = 0; index < elements.length; index += 1) {
     elements[index].addEventListener('click', (event) => { 
+      console.log(event.target);
       for (let itemKey = 0; itemKey < elements.length; itemKey += 1) {
         elements[itemKey].classList.remove('selected');
       }
       event.target.classList.add('selected');
     });
   }
+}
+
+function riscaItemDaLista(elements) {
+  elements.forEach(element => {
+    element.addEventListener('dblclick', (event) => {
+      event.target.classList.toggle('completed');
+    })
+  });
 }
 
 
