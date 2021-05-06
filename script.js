b726536f4edcfafbbba5cf04f9e705977a447343
José Carlos <kalarruda@hotmail.com>
2021-05-06 18:45:02 -0300

    adiciona o 8

--- script.js
+++ script.js
@@ -2,21 +2,115 @@ const btnCriaTarefa = document.getElementById('criar-tarefa');
 btnCriaTarefa.innerHTML = 'Adiciona Tarefa';
 const inputField = document.getElementById('texto-tarefa');
 const ordList = document.getElementById('lista-tarefas');
+let lista2 = document.createElement('li');
 
-function criaTarefa() {
-  btnCriaTarefa.addEventListener('click', function () {
-    const lista = document.createElement('li');
-    lista.innerHTML = inputField.value;
-    ordList.appendChild(lista);
-    inputField.value = '';
-    lista.addEventListener('click', changeColor);
-  });
+
+btnCriaTarefa.addEventListener('click', function () {
+  let lista = document.createElement('li');
+  lista.innerHTML = inputField.value;
+  ordList.appendChild(lista);
+  inputField.value = '';
+  changeColor();
+  // lista.addEventListener('click', changeColor);
+  lista.addEventListener('dblclick', taskEnd);
+  // lista.addEventListener('dblclick', taskNotEnd);
+});
+
+function changeColor() {
+  let lista = document.querySelectorAll('li');
+  for (let index = 0; index < lista.length; index += 1) {
+    lista[index].addEventListener('click', function (event) {
+      const greyBackground = document.querySelector('.color');
+      if (event.target.className !== 'color') {
+        event.target.classList.add('color');
+        if (greyBackground) { // verifica se ele existe
+          greyBackground.classList.remove('color');
+        }        
+      }
+    });
+  }
 }
-criaTarefa()
 
-
-
-function changeColor(event) { 
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+function taskEnd(event) {
+  if (event.target.className !== 'completed') {
+    event.target.classList.add('completed');
+  }
+  else {
+    event.target.classList.remove('completed');
+  }
 }
 
+
+
+
+// function changeColor() {
+//   let lista = document.querySelectorAll('li');
+//   for (let index = 0; index < lista.length; index += 1) {
+//     lista[index].addEventListener('click', function (event) {
+//       const greyBackground = document.querySelector('.color');
+//       if (event.target.className !== 'color') {
+//         event.target.classList.add('color');
+//         greyBackground.classList.remove('color');
+//       }
+//     });
+//   }
+// }
+
+// function changeColor(event) {
+//   if (event.target.style.backgroundColor !== 'rgb(128, 128, 128)') {
+//     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+//   }
+//   else {
+//     event.target.style.backgroundColor ='';
+//   }
+// }
+
+
+// function changeColor(event) {
+//   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+//   let lista = document.getElementsByTagName('li');
+//   for (let index = 0; index < lista.length; index += 1) {
+//     if (lista)
+//     // lista.className = 'completed';
+//   }    
+// }
+
+// function taskEnd(event) {
+//   event.target.className = 'completed';
+//   let lista = document.getElementsByTagName('li');
+//   console.log(lista.length);
+//   for (let index = 0; index < lista.length; index += 1) {
+//     lista[index].addEventListener('click', function (event2) {
+//       if (event2.target.className === 'completed') {
+//         event2.target.classList.remove('completed');
+//       }
+//     });
+//   }
+// }
+
+
+// function changeColor(event) { 
+//   let lista = document.getElementsByTagName('li');
+//     lista.style.backgroundColor = 'rgb(128, 128, 128)';
+//     lista.className = 'completed';
+// }
+
+
+// function taskNotEnd(event) {
+//   if (event.target.className = 'completed') {
+//     event.target.className = '';
+//   }
+// }
+
+// function criaTarefa() {
+//   btnCriaTarefa.addEventListener('click', function () {
+//     let lista = document.createElement('li');
+//     lista.innerHTML = inputField.value;
+//     ordList.appendChild(lista);
+//     inputField.value = '';
+//     lista.addEventListener('click', changeColor);
+//     lista.addEventListener('dblclick', taskEnd);
+//     // lista.addEventListener('dblclick', taskNotEnd);
+//   });
+// }
+// criaTarefa()
