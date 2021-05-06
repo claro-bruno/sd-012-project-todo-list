2caffcbdce9e91067035b5dfacab32879c6ce6f5
Caio César <caiovg32@gmail.com>
2021-05-06 18:10:16 -0300

    123

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
-
-
 function createTask() {
-    const task = document.getElementById('texto-tarefa');
-    const lista = document.getElementById('lista-tarefas');
-    const addButton = document.getElementById('criar-tarefa');
-    const mensagemListaVazia = document.getElementById('lista-vazia');
-    task.focus();
-    addButton.addEventListener('click', function () {
-      const item = document.createElement('li');
-      item.addEventListener('click', changeBack);
-      item.innerHTML = task.value;
-      item.className = 'item';
-      lista.appendChild(item);
-      task.value = '';
-    });
-  }
-  
+  const task = document.getElementById('texto-tarefa');
+  const lista = document.getElementById('lista-tarefas');
+  const addButton = document.getElementById('criar-tarefa');
+  task.focus();
+  addButton.addEventListener('click', function () {
+    const item = document.createElement('li');
+    item.innerHTML = task.value;
+    item.className = 'item';
+    lista.appendChild(item);
+    task.value = '';
+  });
+}
 
-  
-  window.onload = function toDoList() {
-    createTask();
-  } 
 
-  item.addEventListener('click', changeBack);
 
-  function changeBack(itemDaLista) {
-      itemDaLista.target.style.backgroundColor = 'rgb(128, 128, 128';
+window.onload = function toDoList() {
+  createTask();
+}
 
-  }
 
