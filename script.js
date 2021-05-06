22aa0b0ecab01512fcf4c35bb051fcdbe7c2b397
Abner <gedeaoabner@gmail.com>
2021-05-06 15:23:37 -0300

    atualiza função para capturar o valor do input, armazenar na lista e apagar o valor do input - desafio 5

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ function adicionar() {
   document.addEventListener("click", function(btnAdicionar) {
     if (btnAdicionar.target.id === "criar-tarefa") {
         let lista = document.getElementById("lista-tarefas");
-        console.log(lista);
         let novaTarefa = document.createElement("li");
-        novaTarefa.innerText = "olá";
+        let textoTarefa = document.getElementById("texto-tarefa");
+        let tarefaValor = textoTarefa.value;
+        novaTarefa.innerText = tarefaValor;
         lista.appendChild(novaTarefa);
+        textoTarefa.value = "";
     }
   });
 }
