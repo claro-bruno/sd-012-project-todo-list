001ed787e2c9221ba7e629e1b4012c3d1938cd4b
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-05-06 16:32:22 -0300

    Realiza requisito 7

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 window.onload = function(){
     const input = document.getElementById('texto-tarefa');
     const button = document.getElementById('criar-tarefa');
+    const listParent = document.getElementById('lista-tarefas');
 
-    button.addEventListener("click", addTask);
-
-    function addTask() {
-        const listParent = document.getElementById('lista-tarefas');
+    button.addEventListener("click", function() {
         const addItem = document.createElement('li');
         addItem.innerText = input.value;
         listParent.appendChild(addItem);
         input.value = '';
-    }
+    });
+
+    
+
+    function chooseTaks(event) {
+        const taskSelected = document.querySelector('.selected');
+        if (taskSelected !== null) {
+            taskSelected.classList.remove('selected');
+        };
+        event.target.classList.add('selected');
+    };
+    listParent.addEventListener('click', chooseTaks);
+
 };
