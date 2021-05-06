c8e1e65daa545d5bef27a177f76ebc9546f8c734
Álvaro Palma <palma.alvaro@outlook.com>
2021-05-06 15:43:15 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
+let textEntrance;
+
 function createInput() {
-  let textEntrance = document.createElement('input');
+  textEntrance = document.createElement('input');
   textEntrance.id = 'texto-tarefa';
   document.body.appendChild(textEntrance);
 }
 
 createInput();
 
+let taskList;
+
 function createOrdenedList() {
-  let taskList = document.createElement('ol');
+  taskList = document.createElement('ol');
   taskList.id = 'lista-tarefas';
   document.body.appendChild(taskList);
 }
 
 createOrdenedList();
+
+let insertTask;
+
+function createButton() {
+  insertTask = document.createElement('button');
+  insertTask.id = 'criar-tarefa';
+  document.body.appendChild(insertTask);
+  insertTask.innerHTML = 'Adicionar tarefa';
+}
+
+createButton();
+
+insertTask.addEventListener('click', newTask);
+
+let task;
+
+function newTask() {
+  task = document.createElement('li');
+  if (textEntrance.value != '') {
+    task.innerText = textEntrance.value;
+    taskList.appendChild(task);
+    textEntrance.value = '';
+  }
+}
