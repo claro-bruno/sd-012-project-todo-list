463fead4a8c6feeb787c723d09bc01440bf33cf9
Teika <shimworks.tk@gmail.com>
2021-05-06 20:08:35 -0300

    resolve requisito 10 e 11

--- script.js
+++ script.js
@@ -5,28 +5,36 @@
 const criaTarefaButton = document.getElementById('criar-tarefa');
 const oderList = document.getElementById('lista-tarefas');
 
-
-
 document.addEventListener('click', (event) => {
-  if (event.target.id === 'criar-tarefa') {
+  const alvo = event.target;
+  if (alvo.id === 'criar-tarefa') {
     const inputText = document.getElementById('texto-tarefa')
     let lista = document.createElement('li');
     lista.className = 'item-list';
     lista.innerText = inputText.value
     oderList.appendChild(lista);
-    inputText.value = ''
+    inputText.value = '';
   }
-  if (event.target.className === 'item-list') {
+  if (alvo.className === 'item-list') {
     const selected = document.querySelector('.selected');
     if (selected !== null) {
       selected.classList.remove('selected');
-      selected.style.backgroundColor = 'white'
+      selected.style.backgroundColor = 'white';
     }
-    event.target.classList.add ('selected');
+    alvo.classList.add ('selected');
+  }
+  if (alvo.id === 'apaga-tudo') {
+    oderList.innerHTML = '';
+  }
+  if (alvo.id === 'remover-finalizados') {
+    let selecionados = document.querySelectorAll('.completed')
+    for (let index = 0; index < selecionados.length; index += 1) {
+        selecionados[index].remove()
+    }
   }
 })
 
-document.addEventListener('dblclick', (evento) => {
+oderList.addEventListener('dblclick', (evento) => {
   let alvo = evento.target
   if (alvo.classList.contains('completed')) {
     alvo.classList.remove('completed')
