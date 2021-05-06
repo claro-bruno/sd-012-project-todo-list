cee656581ae28a15711b8bc18ec6c816ff17f6d7
Isaac Cruz Leite Machado <isaac.clm1@gmail.com>
2021-05-06 15:41:21 -0300

    Adiciona requisitos 6, 7 e 8 - Ordena tasks e seleciona task

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ function createTask() {
     let inputValue = document.querySelector("#texto-tarefa").value;
     let element = document.createElement("li");
     element.innerHTML = inputValue;
+    element.addEventListener("click", highlightTask);
     taskList.appendChild(element);
     document.querySelector("#texto-tarefa").value = "";
 };
 
 let newTaskButton = document.querySelector("#criar-tarefa");
 newTaskButton.addEventListener("click", createTask);
+
+// Destacar item da lista em cinza
+
+function highlightTask (event){
+    let listOfTasks = document.querySelectorAll("#lista-tarefas li");
+    
+    for(let index = 0; index < listOfTasks.length; index += 1){
+        listOfTasks[index].style.backgroundColor = "rgb(255, 255, 255)"
+    };
+
+    event.target.style.backgroundColor = "rgb(128, 128, 128)";
+};
