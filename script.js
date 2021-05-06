31b1d4bdd41a98091aa9e3111d8d92e54a5a57c2
Bruno Augusto <brunaugusto@gmail.com>
2021-05-06 15:47:31 -0300

    Primeiros Requisitos

---
+++ script.js
@@ -0,0 +1,85 @@
+let arrTasks = localStorage.getItem('tasks');
+console.log(arrTasks);
+function loadTasksList(arrTasks){
+    if(arrTasks !== null) {
+        for(let index = 0; index < arrTasks.length; index += 1) {
+            addTask(arrTasks[index],'');
+        }
+    }
+}
+
+loadTasksList(arrTasks);
+
+let btnAddTask = document.querySelector('#criar-tarefa');
+btnAddTask.addEventListener('click',  (evt) => addTask(document.querySelector('#texto_tarefa').value,evt));
+
+let btnCleanList = document.querySelector('#apaga-tudo');
+btnCleanList.addEventListener('click', cleanTask);
+
+let btnCleanCompletedTasks = document.querySelector('#remover-finalizados');
+btnCleanCompletedTasks.addEventListener('click', cleanCompletedTasks);
+
+let btnSaveTasks = document.querySelector('#salvar-tarefas');
+btnSaveTasks.addEventListener('click', saveTasks);
+
+function addTask(myTask,evt) {
+
+    let ol_task = document.querySelector('#lista-tarefas');
+    //let txtTask = document.querySelector('#texto_tarefa').value;
+
+    if(myTask.length > 0) {
+        let li_task = document.createElement('li');
+        li_task.innerHTML = myTask;
+        li_task.className = 'task';
+        li_task.addEventListener('click', changeBackgroundTask)
+        li_task.addEventListener('dblclick', setCompletedTask)
+        ol_task.appendChild(li_task);
+        document.querySelector('#texto_tarefa').value = '';
+    } 
+}
+
+function changeBackgroundTask(event) {
+    const removeSelected = document.querySelector('.selected');
+    if(removeSelected !== null){
+        removeSelected.classList.remove('selected');
+        event.target.classList.add('selected');
+    } 
+    else {
+        event.target.classList.add('selected');
+    }
+}
+
+function setCompletedTask(event) {
+
+    if(event.target.classList.value.includes('completed')) {
+        event.target.className = 'task';
+    }
+    else {
+        event.target.className = 'task completed';
+    }
+}
+
+function cleanTask(event) {
+    let ol_task = document.querySelector('#lista-tarefas');
+    ol_task.innerHTML = '';
+}
+
+function cleanCompletedTasks(event) {
+    let li_tasks = document.querySelectorAll('.task');
+    let ol_tasks = document.querySelector('#lista-tarefas');
+    for(let index = 0; index < li_tasks.length; index += 1) {
+        if(li_tasks[index].classList.value.includes('completed')) {
+            ol_tasks.removeChild(li_tasks[index]);
+        }
+    }
+}
+
+function saveTasks (event) {
+    let li_tasks = document.querySelectorAll('.task');
+    
+    for(let index = 0; index < li_tasks.length; index += 1) {
+        console.log(li_tasks[index].innerText);
+        localStorage.setItem(`task${index+1}`, arrTasks[index].innerText);
+    }
+    
+}
