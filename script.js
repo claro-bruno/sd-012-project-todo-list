5fc17e5e19e6a655bf98dfe4fc4a719f2173d37c
RyanLaiber <ryanlaiber@gmail.com>
2021-05-06 16:51:01 -0300

    adiciona requisito 7 e 8, ao clicar em tarefa muda background color e seleciona

--- script.js
+++ script.js
@@ -3,18 +3,33 @@ let caminhoBtn = document.getElementById("criar-tarefa");
 let caminhoImput = document.getElementById("texto-tarefa");
 let caminhoLista = document.getElementById("lista-tarefas");
 
-//  função que adiciona tarefa e limpa imput
+// função que muda o backgroundColor de tarefa e seleciona
+function mudaFundoSeleciona(event){
+    let velhoClick = document.querySelector(".selected");
+
+    if (velhoClick){
+        velhoClick.style.backgroundColor = "";
+        velhoClick.classList.remove("selected");
+    };
+
+    event.target.style.backgroundColor = "rgb(128,128,128)";
+    event.target.classList.add("selected");
+};
+
+//  funções de adiciona tarefa e limpa imput
 function addTarefa (){
     let caixaTarefa = document.createElement("li");
     caixaTarefa.className = "tarefa";
     caixaTarefa.innerText = caminhoImput.value;
     caminhoLista.appendChild(caixaTarefa);
+    caixaTarefa.addEventListener("click", mudaFundoSeleciona); //  adiciona eventlistener na tarefa 
 };
 
 function limpaImput(){
     caminhoImput.value = "";
 };
 
+//  função que adiciona tarefa e limpa imput
 function clicaBtn(){
     addTarefa();
     limpaImput();
