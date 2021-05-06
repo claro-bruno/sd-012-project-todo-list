45838e1ce32ffa0f3184f85bb344ffd7dbc2660b
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-05-06 15:12:06 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+function changeTaskColor(){
+    let tasks = document.querySelectorAll('.tarefa');
+    for(let indexTask = 0;indexTask < tasks.length; indexTask += 1){
+        let task = tasks[indexTask];
+        task.addEventListener('click', ()=>{
+            task.style.backgroundColor = 'rgb(128,128,128)';
+        });
+    };
+};
+
 function createTask() {
   let btn = document.querySelector('#criar-tarefa');
   btn.addEventListener('click', () => {
     let input = document.querySelector('#texto-tarefa');
     let task = document.createElement('li');
+    task.className = 'tarefa';
     task.innerHTML = input.value;
     input.value = '';
     let list = document.querySelector('#lista-tarefas');
     list.appendChild(task);
+    changeTaskColor();
   });
 };
 
