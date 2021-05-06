fbbfcec375acce2c1cd68bbc5a65cbbcfb13ef1d
Thiago Prado <thiagopc_dev@hotmail.com>
2021-05-06 17:29:10 -0300

    Desafio 7

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ window.onload = () => {
         const getList = getById('lista-tarefas');
         const newTask = document.createElement('li');
         newTask.innerHTML = getTask();
+        newTask.className = "tasks";
         getList.appendChild(newTask);
+        paintLi();
     }
     const add = getById('criar-tarefa');
     const getInput = getById('texto-tarefa');
@@ -22,9 +24,23 @@ window.onload = () => {
     add.addEventListener('click', () => {
         if (getInput.value.length > 0) addToList();
     });
+    // Listener para adicionar com a tecla Enter \\
     getInput.addEventListener('keyup', (e) => {
         if((e.key ==='Enter') && (getInput.value.length > 0))  addToList();
     });
     
+    function paintLi(){
+        const getTasksArray = document.getElementsByClassName('tasks');
+        if(getTasksArray.length > 0){
+            for(let i = 0; i < getTasksArray.length; i += 1){
+                getTasksArray[i].addEventListener('click', (e) => {
+                        e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+                })
+            }
+        }
+    }
+       
+    
+   
 
 }
