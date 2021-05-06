c3225f1671071e546ce5dccffb6fc9ff54d8bcec
Adryan Rosa <adryan.lmrosa@gmail.com>
2021-05-06 16:40:50 -0300

    Satisfaz requisito 7

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const button = document.getElementById('criar-tarefa');
-button.addEventListener('click', adicionaTarefa);
+function select(e) {
+  const ultimaTarefa = document.querySelector('.selected');
+  if (ultimaTarefa != null) {
+    ultimaTarefa.classList.remove('selected');
+  }
+
+  const tarefaAtual = e.target;
+  tarefaAtual.classList.add('selected');
+}
 
 function adicionaTarefa() {
   const textoTarefa = document.getElementById('texto-tarefa').value;
   const lista = document.getElementById('lista-tarefas');
   const novaTarefa = document.createElement('li');
   novaTarefa.innerText = textoTarefa;
+  novaTarefa.addEventListener('click', select);
   lista.appendChild(novaTarefa);
-  document.getElementById("form").reset();
+  document.getElementById('form').reset();
+  // capturaTarefas();
 }
+
+const button = document.getElementById('criar-tarefa');
+button.addEventListener('click', adicionaTarefa);
