b2ecb6e332cdf90ec7288fc405761b31b6aa6a0c
Eric Faria <ericdeofaria@gmail.com>
2021-05-06 15:38:00 -0300

    Requisito 07

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const taskList = document.getElementById('lista-tarefas');
 const addBtn = document.getElementById('criar-tarefa');
 const taskInput = document.getElementById('texto-tarefa');
+const title = document.getElementById('title');
+
+function upgradeList() {
+  const list = document.querySelectorAll('li');
+  for (let index = 0; index < list.length; index += 1) {
+    list[index].addEventListener('click', select);
+  };
+};
 
 function addTask() {
   const task = document.createElement('li');
   task.innerText = taskInput.value;
   taskList.appendChild(task);
   taskInput.value = null;
+  upgradeList();
+}
+
+function select(event) {
+    event.target.classList.add('selected');
 }
 
 window.onload = function loadPage() {
-  addBtn.addEventListener('click', addTask)
+  addBtn.addEventListener('click', addTask);
+  title.addEventListener('click', select);
 };
