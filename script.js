dda4dc1598b68f8b0c740939779f93b5d6c9d399
Caroline Benichio Teixeira <benichiocarol@gmail.com>
2021-05-06 17:28:11 -0300

    Requisito 5 e 6 resolvido

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-* {
-  margin: 0;
+window.onload = function () {
+
+//5
+let addTaskButton = document.getElementById('criar-tarefa');
+function addTask() {
+    const input = document.getElementById('texto-tarefa');
+    if (input.value === '') {
+      alert('Insira uma tarefa!');
+    } else {
+    const taskList = document.getElementById('lista-tarefas');
+    const task = document.createElement('li');
+    task.className = 'task-item';
+    task.innerHTML = input.value;
+    taskList.appendChild(task);
+    input.value = '';
+    }
+  }
+  addTaskButton.addEventListener('click', addTask); 
+
+//6 
 }
