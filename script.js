0461dcdf1d621045711f28c9c0d42a8135973350
alestori <alestori7@gmail.com>
2021-05-06 15:30:19 -0300

    adds event to toggle bg color of selected task

--- script.js
+++ script.js
@@ -2,14 +2,24 @@ const addTaskButton = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const taskInput = document.getElementById('texto-tarefa');
 
+let selectedTask;
+
+const selectTask = (event) => {
+  const task = event.target;
+  task.classList.add('selected');
+  if (selectedTask) {
+    selectedTask.classList.remove('selected');
+  }
+  selectedTask = task;
+};
+
 const addTask = () => {
   const newTask = document.createElement('li');
   newTask.innerText = taskInput.value;
   newTask.classList.add('task');
   taskList.appendChild(newTask);
   taskInput.value = '';
+  newTask.addEventListener('click', selectTask);
 };
 
 addTaskButton.addEventListener('click', addTask);
-
-
