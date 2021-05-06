b9fa6047d86a3b8f69aeea8c468140b33b8750ca
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-05-06 14:31:48 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -5,12 +5,26 @@ function createTask() {
 
   button.addEventListener('click', () => {
     const createItem = document.createElement('li');
+    createItem.className = 'list-item';
     createItem.innerHTML = input.value;
     list.appendChild(createItem);
     input.value = '';
   });
 }
 
+function selectedItem() {
+  const list = document.getElementById('lista-tarefas');
+  list.addEventListener('click', (event) => {
+    const selected = document.querySelector('.selected-item');
+
+    if (selected != null) {
+      selected.classList.remove('selected-item');
+    }
+    event.target.classList.add('selected-item');
+  });
+}
+
 window.onload = () => {
   createTask();
+  selectedItem();
 };
