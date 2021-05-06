5b4424d0d230c769f690abe8db1480abf9032657
rafa-el-souza <rafaelmvsouza@gmail.com>
2021-05-06 13:22:27 -0300

    Requisito 11

--- script.js
+++ script.js
@@ -7,20 +7,32 @@ newTaskButton.addEventListener('click', () => {
   const li = document.createElement('li');
   li.innerHTML = inputTask.value;
   inputTask.value = '';
-  taskList.appendChild(li);
-  selectLi();
-  taskFinished()
-});
-
-function selectLi() {
-  const lastLi = taskList.lastElementChild;
-  lastLi.addEventListener('click', (event) => {
+  li.addEventListener('click', (event) => {
     deselectOtherLis();
     const targ = event.target;
-    targ.className = 'selected';
+    targ.classList.add('selected');
     targ.style.backgroundColor = 'rgb(128, 128, 128)';
   });
-};
+  li.addEventListener('dblclick', (event) => {
+    const targ = event.target;
+    if (targ.style.textDecoration === 'line-through') {
+      targ.style.textDecoration = '';
+    } else {
+      targ.style.textDecoration = 'line-through';
+    }
+    targ.classList.toggle('completed');
+  });
+  taskList.appendChild(li);
+});
+
+removeFinishedTasksButton.addEventListener('click', () => {
+  const li = document.querySelectorAll('li');
+  for (let index = 0; index < li.length; index += 1) {
+    if (li[index].classList.contains('completed')) {
+      taskList.removeChild(li[index]);
+    }
+  }
+});
 
 function deselectOtherLis() {
   const li = document.querySelectorAll('li');
@@ -30,25 +42,4 @@ function deselectOtherLis() {
   }
 }
 
-function taskFinished() {
-  const lastLi = taskList.lastElementChild;
-  lastLi.addEventListener('dblclick', (event) => {
-    const targ = event.target;
-    if (targ.style.textDecoration === 'line-through') {
-      targ.style.textDecoration = '';
-      targ.classList.remove('completed');
-    } else {
-      targ.style.textDecoration = 'line-through';
-      targ.classList.add('completed');
-    }
-  });
-};
 
-console.log(removeFinishedTasksButton);
-removeFinishedTasksButton.addEventListener('click', () => {
-  for (let index = 0; index < taskList.children.length; index += 1) {
-    if (taskList.children[index].classList.contains('completed')) {
-      taskList.removeChild(taskList.children[index]);
-    }
-  }
-});
