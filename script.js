9b3ab93958e70cb4580323c51800f4bfda36bda1
Armando <a.guerra58@hotmail.com>
2021-05-06 16:03:57 -0300

    ate a 5

--- script.js
+++ script.js
@@ -10,3 +10,30 @@ par.id = "funcionamento";
 par.innerText = "Clique duas vezes em um item para marcá-lo como completo"
 main.appendChild(par);
 
+let section = document.querySelector("#section");
+let inp = document.createElement("input");
+inp.id = "texto-tarefa";
+section.appendChild(inp);
+
+let bt = document.createElement("button");
+bt.id = "criar-tarefa";
+bt.innerHTML = "Add Task"
+section.appendChild(bt);
+
+let button = document.querySelector("#criar-tarefa");
+let list = document.querySelector("#lista-tarefas");
+let textTasks = document.querySelector("#texto-tarefa");
+
+button.addEventListener("click" , function (){
+  let makeLi = document.createElement("li");
+  makeLi.textContent = textTasks.value;
+  list.appendChild(makeLi);
+  textTasks.value = "";
+
+});
+
+
+    
+    
+
+
