7d0819503cf130b37f58129ea004fcec0c227b61
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-06 19:23:19 -0300

    adiciona botoes de mover

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const botaoAddTarefa = document.querySelector('#criar-tarefa');
 const botaoApagaTudo = document.querySelector('#apaga-tudo');
 const botaoApagaFinalizadas = document.querySelector('#remover-finalizados');
 const botaoApagaSelecionado = document.querySelector('#remover-selecionado');
+const botaoCima = document.querySelector('#mover-cima');
+const botaoBaixo = document.querySelector('#mover-baixo');
 
 function addTarefa() {
     if (tarefaNova.value.length > 0) {
@@ -65,3 +67,49 @@ function apagarSelecionado() {
 }
 
 botaoApagaSelecionado.addEventListener('click', apagarSelecionado);
+
+function moverParaCima () {
+    let selecionado = document.querySelector('.selected');
+    let anterior = selecionado.previousSibling;
+    let classesSelecionado = selecionado.className;
+    let classesDeCima = anterior.className;
+    let conteudoSelecionado = selecionado.innerHTML;
+    let conteudoDeCima = anterior.innerHTML;
+    let estiloTextoSelecionado = selecionado.style.textDecoration;
+    let estiloTextoDeCima = anterior.style.textDecoration;
+    selecionado.className = classesDeCima;
+    anterior.className = classesSelecionado;
+    selecionado.innerHTML = conteudoDeCima;
+    anterior.innerHTML = conteudoSelecionado;
+    selecionado.style.textDecoration = estiloTextoDeCima;
+    anterior.style.textDecoration = estiloTextoSelecionado;
+    if (anterior.classList.contains('selected') === true) {
+        anterior.style.backgroundColor = 'rgb(128, 128, 128)';
+        selecionado.style.backgroundColor = 'white';
+    }
+}
+
+botaoCima.addEventListener('click', moverParaCima);
+
+function moverParaBaixo () {
+    let selecionado = document.querySelector('.selected');
+    let proximo = selecionado.nextSibling;
+    let classesSelecionado = selecionado.className;
+    let classesDeBaixo = proximo.className;
+    let conteudoSelecionado = selecionado.innerHTML;
+    let conteudoDeBaixo = proximo.innerHTML;
+    let estiloTextoSelecionado = selecionado.style.textDecoration;
+    let estiloTextoDeBaixo = proximo.style.textDecoration;
+    selecionado.className = classesDeBaixo;
+    proximo.className = classesSelecionado;
+    selecionado.innerHTML = conteudoDeBaixo;
+    proximo.innerHTML = conteudoSelecionado;
+    selecionado.style.textDecoration = estiloTextoDeBaixo;
+    proximo.style.textDecoration = estiloTextoSelecionado;
+    if (proximo.classList.contains('selected') === true) {
+        proximo.style.backgroundColor = 'rgb(128, 128, 128)';
+        selecionado.style.backgroundColor = 'white';
+    }
+}
+
+botaoBaixo.addEventListener('click', moverParaBaixo);
