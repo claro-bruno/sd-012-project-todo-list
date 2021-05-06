87f1cf7d6664ede2aa0f1bb7889a5529e58343af
Kevin Oliveira <kevin.zero@hotmail.com>
2021-05-06 16:00:22 -0300

    (6) Adiciona evento de cor de fundo ao clicar nos itens da lista

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-document.getElementById('criar-tarefa').addEventListener('click' , function () {
+document.getElementById('criar-tarefa').addEventListener('click', function () {
     task = document.createElement('li');
-    text = document.getElementById('texto-tarefa')
+    text = document.getElementById('texto-tarefa');
+    task.className = 'taskItem'
     task.innerHTML = text.value;
-    document.getElementById('lista-tarefas').appendChild(task)
-    text.value = ''
+    document.getElementById('lista-tarefas').appendChild(task);
+    text.value = '';
+})
+
+document.body.addEventListener('click', function (event) {
+    if (event.target.className === 'taskItem') {
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+    }
 })
