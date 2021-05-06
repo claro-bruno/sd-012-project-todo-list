ae328f240c525bcd445f387d7bc2cf4819c26c22
Patrick Dack <patrickdack89@gmail.com>
2021-05-06 18:32:51 -0300

    Requisito 9 e Requisito 10 - os mesmos ainda precisam de ajustes.

--- script.js
+++ script.js
@@ -10,18 +10,46 @@ function catchInput() {
     taskList.appendChild(createLi);
     input.value = '';
     selectItem();
+    markAsCompleted();
+    resetAll();
   });
 }
 catchInput();
 // Altera a cor de fundo do item selecinado
 function selectItem() {
   const itemList = document.querySelectorAll('.item-list');
-  for (let item of itemList) {
+  for (const item of itemList) {
     item.addEventListener('click', () => {
-      for (let item2 of itemList) {
+      for (const item2 of itemList) {
         item2.style.backgroundColor = null;
       }
       item.style.backgroundColor = 'rgb(128, 128, 128)';
-    })
+    });
   }
+}
+// Marca item como completed
+function markAsCompleted() {
+  const itemList = document.querySelectorAll('.item-list');
+  for (let index = 0; index < itemList.length; index += 1) {
+    let item = itemList[index];
+    item.addEventListener('dblclick', () => {
+      if (item.className === 'item-list') {
+        item.className = 'item-list completed';
+      } else {
+        item.className = 'item-list';
+      }
+    });
+  }
+}
+// Botao apaga tudo
+function resetAll() {
+  const taskList = document.querySelector('#lista-tarefas');
+  const btnX = document.getElementById('apaga-tudo');
+  const itemList = document.querySelectorAll('.item-list');
+  btnX.addEventListener('click', () => {
+    for (let index = (itemList.length - 1); index >= 0; index -= 1) {
+      let item = itemList[index];
+      taskList.removeChild(item);
+    }
+  });
 }
