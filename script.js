96cff0f82630fc2bf1cfcc68d5fd004d9266c90a
Eduardo Linhares <modsgtasagame@gmail.com>
2021-05-06 15:51:34 -0300

    Resolve requisito 7 e 8

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 window.onload = function () {
   criarTarefa();
+
+  clicarItemLista()
 }
 
 function criarTarefa() {
   const pegarBotaoTarefa = document.getElementById('criar-tarefa');
   const pegarInput = document.getElementById('texto-tarefa');
-  const pegarLista = document.getElementById('lista-tarefas')
+  const pegarLista = document.getElementById('lista-tarefas');
   pegarBotaoTarefa.addEventListener('click', function () {
     let li = document.createElement('li');
     li.innerHTML = pegarInput.value;
     pegarLista.appendChild(li);
     pegarInput.value = '';
+  })
+}
+
+function resetarCorLista() {
+  const getAllLi = document.querySelectorAll('#lista-tarefas li');
+  for (let i = 0; i < getAllLi.length; i += 1) {
+    getAllLi[i].style = '';
+  }
+}
+
+function clicarItemLista() {
+  const pegarLista = document.getElementById('lista-tarefas');
+  pegarLista.addEventListener('click', function (event) {
+    resetarCorLista();
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
   })
 }
