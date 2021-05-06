db488637feabe9aae8debee99080f496637ed1b8
Rodrigo <rodrigo.facury14@gmail.com>
2021-05-06 18:36:38 -0300

    requisito 8 terceira tentativa

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-const input = document.querySelector('#texto-tarefa');
-const button = document.querySelector('#criar-tarefa');
-const list = document.querySelector('#lista-tarefas');
+window.onload = function () {
 
-function changeBgColor(event) {
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-  event.target.classList.add('selected');
+  const input = document.querySelector('#texto-tarefa');
+  const button = document.querySelector('#criar-tarefa');
+  const list = document.querySelector('#lista-tarefas');
+
+  function changeBgColor(event) {
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    event.target.classList.add('selected');
+  }
+
+  function changeSelected() {
+    const selected = document.querySelector('.selected');
+    selected.style.backgroundColor = '';
+    selected.classList.remove('selected');
+  }
+
+  function createItem() {
+    const newItem = document.createElement('li');
+    newItem.addEventListener('click', changeSelected);
+    newItem.addEventListener('click', changeBgColor);
+    newItem.innerHTML = input.value;
+    list.appendChild(newItem);
+    input.value = '';
+  }
+
+  button.addEventListener('click', createItem);
+
 }
-
-function changeSelected() {
-  const selected = document.querySelector('.selected');
-  selected.style.backgroundColor = '';
-  selected.classList.remove('selected');
-}
-
-function createItem() {
-  const newItem = document.createElement('li');
-  newItem.addEventListener('click', changeSelected);
-  newItem.addEventListener('click', changeBgColor);
-  newItem.innerHTML = input.value;
-  list.appendChild(newItem);
-  input.value = '';
-}
-
-button.addEventListener('click', createItem);
