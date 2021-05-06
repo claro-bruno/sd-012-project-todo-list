0ed0a7d6831d4b917b4811271d8286bd832519d9
Sarah <sarahgsb@hotmail.com>
2021-05-06 17:27:31 -0300

    refatorando codigo via eslint

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
-let addButton = document.getElementById("criar-tarefa");
-let taskList = document.getElementById("lista-tarefas");
-let inputText = document.getElementById("texto-tarefa");
-let removeAllTasks = document.getElementById('apaga-tudo');
+const addButton = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefas');
+const inputText = document.getElementById('texto-tarefa');
+const removeAllTasks = document.getElementById('apaga-tudo');
 
-addButton.addEventListener("click", function () {
-    let createList = document.createElement("li");
-    createList.classList.add("create-list");
-    createList.innerText = inputText.value;
-    inputText.value = "";
-    taskList.appendChild(createList);
+addButton.addEventListener('click', () => {
+  const createList = document.createElement('li');
+  createList.classList.add('create-list');
+  createList.innerText = inputText.value;
+  inputText.value = '';
+  taskList.appendChild(createList);
 
-    createList.addEventListener("dblclick", function (event) {
-        if (event.target.classList.contains('completed')) {
-            event.target.classList.remove('completed');
-        } else {
-            event.target.classList.add('completed');
-        }
+  createList.addEventListener('dblclick', (event) => {
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+    } else {
+      event.target.classList.add('completed');
+    }
+  });
+});
 
-       });  
-    })
+function backgroundGreyItem() {
+  taskList.addEventListener('click', (event) => {
+    const selectedItem = document.querySelector('.grey');
+    if (selectedItem != undefined) {
+      selectedItem.classList.remove('grey');
+    }
+    event.target.classList.add('grey');
+  });
+}
+backgroundGreyItem();
 
-    function backgroundGreyItem() {
-        taskList.addEventListener('click', function(event) {
-          const selectedItem = document.querySelector('.grey');
-          if (selectedItem != undefined) {
-          selectedItem.classList.remove('grey');
-          }
-          event.target.classList.add('grey');
-        });
-      }
-      backgroundGreyItem();
-
-function removeTasks(){
-    removeAllTasks.addEventListener('click', function(){
-        taskList.innerHTML = '';
-    })
-};
+function removeTasks() {
+  removeAllTasks.addEventListener('click', () => {
+    taskList.innerHTML = '';
+  });
+}
 removeTasks();
