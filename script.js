20c918e3e5e79c1d2885023ec62e8ffe82f6e303
vinicius-patriota <vinicius.patriota@outlook.com.br>
2021-05-06 20:07:38 -0300

    Finalizados requisitos obrigatorios

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 let input = document.getElementById("texto-tarefa");
-let btn = document.getElementById("criar-tarefa");
+let btnCriar = document.getElementById("criar-tarefa");
 let lista = document.getElementById("lista-tarefas");
+let btnApagarTudo = document.getElementById("apaga-tudo");
+let btnApagarFinalizados = document.getElementById("remover-finalizados");
 
 document.addEventListener("click", function (event) {
-    if (event.target == btn) {
+    if (event.target == btnCriar) {
         let item = document.createElement("li");
         item.className = "item";
         item.innerText = input.value;
         input.value = "";
         lista.appendChild(item);
+    }
+    if (event.target == btnApagarFinalizados) {
+        completos = document.getElementsByClassName("completed");
+
+        while (completos.length > 0) {
+            completos[0].remove(completos[0]);
+        }
+    }
+    if (event.target == btnApagarTudo) {
+        while (lista.firstChild) {
+            lista.removeChild(lista.lastChild);
+        }
     }
 
     if (event.target.classList.contains("item")) {
@@ -21,12 +35,12 @@ document.addEventListener("click", function (event) {
 })
 
 document.addEventListener("dblclick", function (event) {
-    if (event.target.classList.contains("item") && event.target.classList.contains("completed")) {
-        event.target.style.textDecoration = "none";
-        event.target.classList.remove("completed");
-    }
-    else {
+    if (event.target.classList.contains("item") && !(event.target.classList.contains("completed"))) {
         event.target.style.textDecoration = "line-through";
         event.target.classList.add("completed")
+    }
+    else {
+        event.target.style.textDecoration = "none";
+        event.target.classList.remove("completed");
     }
 })
