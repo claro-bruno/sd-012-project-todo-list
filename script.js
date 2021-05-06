0b44f4dcc1ab876c35a770676a592d9aaac72a1b
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-06 17:02:39 -0300

    Requirement 07 and 08-> Add background color to the task when clicked

--- script.js
+++ script.js
@@ -5,10 +5,22 @@ window.onload = function page() {
 
   function taskCreator() {
     const taskItem = document.createElement('li');
+    taskItem.className = 'tarefa';
     taskItem.innerText = textInput.value;
     textInput.value = '';
     taskList.appendChild(taskItem);
   }
 
   btnTaskCreate.addEventListener('click', taskCreator);
+
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('tarefa')) {
+      const currentTaskSelected = document.querySelector('.selected');
+      console.log(currentTaskSelected);
+      if (currentTaskSelected !== null) {
+        currentTaskSelected.classList.remove('selected');
+      }
+      event.target.classList.add("selected")
+    }
+  })
 };
