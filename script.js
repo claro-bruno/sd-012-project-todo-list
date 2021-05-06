5ac1d0b62ecb38cece7e19b93d8dfc8fbaf3d64e
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-05-06 18:08:27 -0300

    Resolve requisito 7

--- script.js
+++ script.js
@@ -2,13 +2,22 @@ function insertTask(value) {
   const orderList = document.createElement('li');
   orderList.className = 'task-list';
   orderList.innerHTML = value;
+  orderList.addEventListener('click', greyItem);
   const taskList = document.querySelector('#lista-tarefas');
-  
   taskList.appendChild(orderList);
 }
 
- function receiveInput(){
-    let contentInput = document.querySelector('#texto-tarefa').value;
-    insertTask(contentInput);
-    document.querySelector('#texto-tarefa').value = '';
+function receiveInput() {
+  const contentInput = document.querySelector('#texto-tarefa').value;
+  insertTask(contentInput);
+  document.querySelector('#texto-tarefa').value = '';
 }
+
+function greyItem(){
+    console.log('xablau');
+  let orderList = document.querySelectorAll('.task-list')
+  for (let index = 0; index < orderList.length; index += 1){
+    orderList[index].style.backgroundColor = 'rgb(128, 128, 128)';
+  }
+}
+greyItem();
