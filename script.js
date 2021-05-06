f55d4b430468c582bcaec297a60d56b60cf7d6a7
Rodrigo <rodrigo.facury14@gmail.com>
2021-05-06 18:16:16 -0300

    requisito 8

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ const input = document.querySelector('#texto-tarefa');
 const button = document.querySelector('#criar-tarefa');
 const list = document.querySelector('#lista-tarefas');
 
+function changeBgColor(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  event.target.classList.add('selected');
+}
+
+function changeSelected() {
+  const selected = document.querySelector('.selected');
+  selected.style.backgroundColor = 'white';
+  selected.classList.remove('selected');
+}
+
 function createItem() {
   const newItem = document.createElement('li');
-  newItem.addEventListener('click', changeItemBgColor);
+  newItem.addEventListener('click', changeSelected);
+  newItem.addEventListener('click', changeBgColor);
   newItem.innerHTML = input.value;
   list.appendChild(newItem);
   input.value = '';
 }
 
 button.addEventListener('click', createItem);
-
-function changeItemBgColor(event) {
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-}
