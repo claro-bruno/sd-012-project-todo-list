8e5ae13716763c64f8a3de035c9bc97f47de8620
PeDD1 <pedrohr0909@gmail.com>
2021-05-06 15:45:24 -0300

    Requisitos 2 e 3 Realizados

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
 document.getElementsByTagName("h1")[0].innerHTML = "Minha Lista de Tarefas"
+document.getElementById("funcionamento").innerHTML = "Clique duas vezes em um item para marcá-lo como completo"
