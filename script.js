90136fd85a04ad82f71a5f5fbb2e73c9e93da11e
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2021-05-06 20:21:23 -0300

    feito o requisito 5

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-window.onload = function () {
+let listaDeItem = document.getElementById("lista-tarefa");
+let botaoAdicionarTarefa = document.getElementById("criar-tarefa");
+
+function createTask() {
+    let tarefa = document.getElementById("texto-tarefa");
+    let itemDaLista = document.createElement('li');
+    itemDaLista.innerHTML = tarefa.value;
+    if (tarefa.value !== null) {
+        listaDeItem.appendChild(itemDaLista);
+    }
+    inputText = "";
 
 }
+
+
+
+botaoAdicionarTarefa.addEventListener("click", createTask);
