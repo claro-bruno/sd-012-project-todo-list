ccde3a09260d2269be07fda9a4f2b2d91ee51f19
Pedro Gonçalves <peg.gep27@gmail.com>
2021-05-06 16:37:33 -0300

    requisito 7 e 8

--- script.js
+++ script.js
@@ -10,8 +10,20 @@ window.onload = function() {
         if(texto_tarefa.value != '') {
             let li = document.createElement('li');
             li.innerHTML = texto_tarefa.value;
+            li.className = 'no-select'
+            li.addEventListener('click', selectTarefa);
             lista_tarefas.appendChild(li);
             texto_tarefa.value = '';
         }
+    }
+
+    function selectTarefa(event) {
+        let lis = document.getElementsByTagName('li');
+        for(let i = 0; i < lis.length; i++) {
+            if(lis[i].className = 'select') {
+                lis[i].className = 'no-select'
+            }
+        }
+        event.target.className = 'select';
     }
 }
