4474ccb5409280b92113c7010d27f5ae09062f16
Leonardo Ferreira <leonardoferreira@protonmail.com>
2021-05-06 15:26:21 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function addTask() {
   let addButton = document.getElementById('criar-tarefa');
   let taskList = document.getElementById('lista-tarefas');
 
-  addButton.addEventListener('click', function(){
+  addButton.addEventListener('click', function () {
     let createLi = document.createElement('li');
     let input = document.getElementById('texto-tarefa');
     createLi.innerHTML = input.value;
@@ -11,3 +11,14 @@ function addTask() {
   })
 }
 addTask();
+
+function changeItemBG() {
+  let listItems = document.getElementById('lista-tarefas');
+
+  listItems.addEventListener('click', function (event) {
+    console.log(event.target);
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  })
+
+}
+changeItemBG();
