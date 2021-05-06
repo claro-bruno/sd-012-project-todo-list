76c56a16e6449ff3f5e5bd41a647aca22518dd3d
Ygor Saturnino <ygorsaturnino@gmail.com>
2021-05-06 15:25:02 -0300

    Resolvendo requisito 3

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 window.onload = function () {
-    let header = document.createElement('header');
-    let title = document.createElement('h1');
-    title.innerHTML = 'Minha Lista de Tarefas'
-    let main = document.querySelector('main');
-    let paragraph = document.createElement('p');
-    paragraph.id = 'funcionamento';
-    paragraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
-    main.appendChild(header);
-    document.querySelector('header').appendChild(title);
-    main.appendChild(paragraph);
+
+  const header = document.createElement('header');
+  const title = document.createElement('h1');
+  title.innerHTML = 'Minha Lista de Tarefas';
+  const main = document.querySelector('main');
+  const paragraph = document.createElement('p');
+  paragraph.id = 'funcionamento';
+  paragraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
+  main.appendChild(header);
+  document.querySelector('header').appendChild(title);
+  main.appendChild(paragraph);
+
+  const input = document.createElement('input');
+  input.id = 'texto-tarefa';
+  main.appendChild(input);
 }
