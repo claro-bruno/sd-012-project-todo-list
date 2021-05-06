50d6876a62b52598e85d31341366c277d6efd73b
Daniel <danielbped@gmail.com>
2021-05-06 15:42:57 -0300

    Resolve requisito 8

--- script.js
+++ script.js
@@ -2,24 +2,27 @@ let inputTarefa = document.querySelector('#texto-tarefa');
 let botaoTarefa = document.querySelector('#criar-tarefa');
 let listaTarefas = document.querySelector('#lista-tarefas');
 
+botaoTarefa.addEventListener('click', function () {
+  let novaTarefa = document.createElement('li');
+  novaTarefa.innerHTML = inputTarefa.value;
+  novaTarefa.className = 'tarefa';
+  listaTarefas.appendChild(novaTarefa);
+  inputTarefa.value = '';
+  pintaTarefa();
+});
 
-botaoTarefa.addEventListener('click', function() {
-    let novaTarefa = document.createElement('li');
-    novaTarefa.innerHTML = inputTarefa.value;
-    novaTarefa.className = 'tarefa';
-    listaTarefas.appendChild(novaTarefa);
-    inputTarefa.value = '';
-    pintaTarefa();
-})
+function pintaTarefa() {
+  let tarefas = document.querySelectorAll('.tarefa');
 
-
-function pintaTarefa () {
-    let tarefas = document.querySelectorAll('.tarefa');
-    for (let index = 0; index < tarefas.length; index += 1) {
-        tarefas[index].addEventListener('click', function (event) {
-            event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-        })
-    }
+  for (let index = 0; index < tarefas.length; index += 1) {
+    tarefas[index].addEventListener('click', function (event) {
+      let tarefaSelecionada = document.querySelectorAll('.tarefa-selecionada');
+      if (tarefaSelecionada.length > 0) {
+        tarefaSelecionada[0].className = 'tarefa';
+        event.target.className = 'tarefa-selecionada';
+      } else {
+        event.target.className = 'tarefa-selecionada';
+      }
+    });
+  }
 }
-
-
