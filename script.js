9909569e21e81d54cfada4a2f64ac69427039060
Euller Braz <euller.braz99@gmail.com>
2021-05-06 16:02:09 -0300

    Resolvido Requisito 6, Requisito 5 foi resolvido no commit anterior

--- script.js
+++ script.js
@@ -5,8 +5,18 @@ function addTask() {
     button.addEventListener('click', () => {
         let taskLi = document.createElement('li');
         taskLi.innerHTML = input.value;
+        taskLi.classList.add('item-lista');
         input.value = '';
         list.appendChild(taskLi);
     })
 }
 addTask();
+
+function selectedItems() {
+    let list = document.querySelector('#lista-tarefas');
+    list.addEventListener('click', (event) => {
+        let evento = event;
+        evento.target.classList.add('selected');
+    })
+}
+selectedItems();
