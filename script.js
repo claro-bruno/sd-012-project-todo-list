69b127a799b6d61c085f4c3107cd5a99e8ed0707
Renan_Miyashita <renanmiyashita@gmail.com>
2021-05-06 16:52:40 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 let input = document.querySelector('#texto-tarefa');
 let button = document.querySelector('#criar-tarefa');
 let getOl = document.querySelector('#lista-tarefas');
+let list = document.querySelectorAll('li');
 
-button.innerHTML = 'Submit';
+
+button.innerHTML = 'Adicionar';
 
 
-button.addEventListener('click', function (event) {
+button.addEventListener('click', () => {
     let createList = document.createElement('li');
     getOl.appendChild(createList);
-  
     createList.innerText = input.value
+    
+    let list = document.querySelectorAll('li');
 
-    if (input.value != '') {
-        input.value = '';
-    }
-})
+        if (input.value != '') {
+            input.value = '';
+        };
+        createList.addEventListener('click', changeBColor)
+});
+
+function changeBColor(event) {
+    let target = event.target;
+    target.style.backgroundColor = 'rgb(128,128,128)';
+    
+};
+
+
+
