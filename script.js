2ff697a57dec0dbc5228cf870288fc95db883519
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-06 20:52:17 -0300

    Resolve requisito 9

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
 const botaoCriarTarefa = document.getElementById('criar-tarefa');
 const listaTarefas = document.querySelector('#lista-tarefas');
 const inputTxtTarefa = document.getElementById('texto-tarefa');
-// Remove Seleção de item anterior
-function removeSelecao() {
-  const tarefas = document.getElementsByClassName('tarefa');
-  for (let index = 0; index < tarefas.length; index += 1) {
-    const tarefa = tarefas[index];
-    tarefa.classList.remove('marcado');
-  }
-}
-// Adiciona tarefas a lista e aplica cor a tarefa selecionada
-function adicionaTarefa() {
+// Adiciona tarefas a lista e aplica sistemas de marcação
+function sistemaDeTarefas() {
   botaoCriarTarefa.addEventListener('click', () => {
     const criarTarefa = document.createElement('li');
-    criarTarefa.className = 'tarefa';
+    criarTarefa.addEventListener('click', (event) => {
+      const marcado = document.querySelector('.marcado');
+      if (marcado) {
+        marcado.classList.toggle('marcado');
+      }
+      event.target.classList.toggle('marcado');
+    });
+    criarTarefa.addEventListener('dblclick', (event) => {
+      event.target.classList.toggle('completed');
+    });
     criarTarefa.innerHTML = inputTxtTarefa.value;
     listaTarefas.appendChild(criarTarefa);
     inputTxtTarefa.value = '';
-    criarTarefa.addEventListener('click', () => {
-      removeSelecao();
-      criarTarefa.classList.add('marcado');
-    });
   });
 }
-adicionaTarefa();
+sistemaDeTarefas();
