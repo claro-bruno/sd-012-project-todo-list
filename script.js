5db46ba9d488d95f5e2cbbc5b37a4252073942ef
Eric Faria <ericdeofaria@gmail.com>
2021-05-06 15:48:26 -0300

    Requisito 08

--- script.js
+++ script.js
@@ -3,12 +3,26 @@ const addBtn = document.getElementById('criar-tarefa');
 const taskInput = document.getElementById('texto-tarefa');
 const title = document.getElementById('title');
 
+function clearSelection() {
+  const list = document.querySelectorAll('li');
+  for (let index = 0; index < list.length; index += 1) {
+    if (list[index].classList.contains('selected')) {
+      list[index].classList.remove('selected');
+    }
+  };
+}
+
+function select(event) {
+  clearSelection();
+  event.target.classList.add('selected');
+}
+
 function upgradeList() {
   const list = document.querySelectorAll('li');
   for (let index = 0; index < list.length; index += 1) {
     list[index].addEventListener('click', select);
   };
-};
+}
 
 function addTask() {
   const task = document.createElement('li');
@@ -18,11 +32,7 @@ function addTask() {
   upgradeList();
 }
 
-function select(event) {
-    event.target.classList.add('selected');
-}
-
 window.onload = function loadPage() {
   addBtn.addEventListener('click', addTask);
   title.addEventListener('click', select);
-};
+}
