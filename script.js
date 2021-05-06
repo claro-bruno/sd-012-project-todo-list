0159a17d7d77d42dec1062fab2c7ff976252638a
Guilherme Hermenegildo Jr <guihjunior7@gmail.com>
2021-05-06 16:22:53 -0300

    adding requiremetns 6 and 7

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const inputTarefas = document.querySelector('#texto-tarefa');
 const buttonNovaTarefa = document.querySelector('#criar-tarefa');
-const listaTarefas = document.querySelector('#lista-tarefas')
+const listaTarefas = document.querySelector('#lista-tarefas');
 
 function criandoNovaTarefa() {
-  buttonNovaTarefa.addEventListener('click',() => {
+  buttonNovaTarefa.addEventListener('click', () => {
     const novoItem = document.createElement('li');
     novoItem.classList.add('item-lista');
     novoItem.innerText = inputTarefas.value;
@@ -13,3 +13,26 @@ function criandoNovaTarefa() {
 }
 
 criandoNovaTarefa();
+
+function gerandoListadeItens() {
+  buttonNovaTarefa.addEventListener('click', () => {
+    const itensDaLista = document.querySelectorAll('.item-lista');
+    mudaCorDeFundo(itensDaLista);
+  });
+}
+
+gerandoListadeItens();
+
+function mudaCorDeFundo(elements) {
+  console.log(elements.length);
+  for (let index = 0; index < elements.length; index += 1) {
+    elements[index].addEventListener('click', (event) => { 
+      for (let itemKey = 0; itemKey < elements.length; itemKey += 1) {
+        elements[itemKey].classList.remove('selected');
+      }
+      event.target.classList.add('selected');
+    });
+  }
+}
+
+
