520ea4c8be4b0945ca1e9b06fd529f7fafd51f76
Jossany Moura <mjossany@gmail.com>
2021-05-06 15:48:26 -0300

    resolve requisito 3

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function createInstruction () {
+//Função que cria um parágrado de id funcionamento dentro da section de id instrucao
+function createInstruction() {
 const getInstrucao = document.getElementById('instrucao');
 
 const newInstruction = document.createElement('p');
@@ -7,3 +8,12 @@ newInstruction.id = 'funcionamento'
 getInstrucao.appendChild(newInstruction);
 }
 createInstruction();
+
+function createInput(inputId) {
+  const getCaixaInput = document.getElementById('caixaInput');
+
+  const newInput = document.createElement('input');
+  newInput.id = inputId;
+  getCaixaInput.appendChild(newInput);
+}
+createInput('texto-tarefa');
