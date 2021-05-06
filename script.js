4bd428d6d9009d36300e1994c1289d618648a011
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-05-06 20:51:44 -0300

    exercicio 8

--- script.js
+++ script.js
@@ -2,30 +2,30 @@
 
     // Será verificado que, ao se clicar em um item da lista, ele passa a ter o estilo CSS background-color: rgb(128, 128, 128)
 
-function addLista(){
-  let criarTarefa = document.querySelector('#criar-tarefa');
-  let listaDeTarefa = document.querySelector('#lista-tarefas');
-  criarTarefa.addEventListener('click', function(){
-    let textoTarefa = document.querySelector('#texto-tarefa').value;
-    let itemLista = document.createElement('li');
-    itemLista.innerHTML = textoTarefa;
-    itemLista.className = 'item-lista'
-    itemLista.addEventListener('click',alteraCorDeFundo)
-    listaDeTarefa.appendChild(itemLista)
-    textoTarefa = document.querySelector('#texto-tarefa').value = '';
-  });
-}
-
-function alteraCorDeFundo(){
-
-let itemLista = document.querySelectorAll('.item-lista')
-    for (let i =0;i<itemLista.length ; i+= 1){
-      if(itemLista[i].id == 'selected')  
-      itemLista[i].id = ''
-      else 
-      itemLista[i].id = 'selected'
-}
-}
-
+ function addLista(){
+   let criarTarefa = document.querySelector('#criar-tarefa');
+   let listaDeTarefa = document.querySelector('#lista-tarefas');
+   criarTarefa.addEventListener('click', function(){
+     let textoTarefa = document.querySelector('#texto-tarefa').value;
+     let itemLista = document.createElement('li');
+     itemLista.innerHTML = textoTarefa;
+     itemLista.className = 'item-lista'
+     itemLista.addEventListener('click',alteraCorDeFundo)
+     listaDeTarefa.appendChild(itemLista)
+     textoTarefa = document.querySelector('#texto-tarefa').value = '';
+   });
+ }
 addLista();
+ function alteraCorDeFundo(event){
 
+ let itemLista = document.querySelectorAll('.item-lista')
+     for (let i =0;i<itemLista.length ; i+= 1){
+       if(itemLista[i].id == 'selected')
+       itemLista[i].id = ''
+       else 
+       event.target.id= 'selected'
+ }
+ }
+
+
+ 
