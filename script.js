04d70765eca48384d58c25183a6c7862a203117e
Guilherme Cesconeto <cesconeto@gmail.com>
2021-05-06 17:12:02 -0300

    Versão com os fors bugados

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
-const listaTarefas = document.querySelector('#lista-tarefas')
-const inputTarefas = document.querySelector('#texto-tarefa')
+const listaTarefas = document.querySelector('#lista-tarefas');
+const inputTarefas = document.querySelector('#texto-tarefa');
 
 function clicker(eventObject) {
   const targetElement = eventObject.target;
-  if (targetElement.classList.contains('pixel')) {
-    targetElement.style.backgroundColor = selected;
-  } else if (targetElement.classList.contains('color')) {
-    document.querySelector('.selected').className = ('color');
-    targetElement.className = ('color selected');
-    selected = window.getComputedStyle(targetElement).backgroundColor;
+  if (targetElement.classList.contains('tarefa')) {
+    let selecionada = document.querySelector('.selecionada');
+    if (selecionada){ // Fonte: https://gomakethings.com/avoiding-errors-when-using-queryselector-in-vanilla-js/
+      selecionada.classList.remove('selecionada');
+    }
+    targetElement.classList.add('selecionada');
   }
 }
 
-document.addEventListener('click', clicker);
+function dblclicker(eventObject) {
+  const targetElement = eventObject.target;
+  if (targetElement.classList.contains('tarefa')) {
+    targetElement.classList.add("finalizada");
+  }
+}
+
 
 function criaTarefa (){
   const li = document.createElement('li');
   li.innerHTML = inputTarefas.value;
+  // inputTarefas.value = '';
   li.className = ('tarefa');
   listaTarefas.appendChild(li);
+  const tarefas = document.getElementsByClassName('tarefa');
+  const finalizadas = document.getElementsByClassName('finalizada');
+  const numTarefas = tarefas.length;
+  const numFinalizadas = finalizadas.length;
 }
 function apagaTodas (){
-  
+  for (let deletada of tarefas){
+    deletada.remove();
+  }
 }
 function apagaFinalizadas (){
-  
+  for (let index = 0; index < numFinalizadas; index += 1){
+    let deletada = finalizadas[index];
+    deletada.remove();
+  }
 }
 function apagaSelecionada (){
-  
+  let deletada = listaTarefas.querySelector('.selecionada');
+  deletada.remove();
 }
+
+
+
+
+document.addEventListener('click', clicker);
+document.addEventListener('dblclick', dblclicker);
