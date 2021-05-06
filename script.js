4b8fd50cb92af455221f73d2fe8de7155ce32cfc
Vinicius Tavares <vini_tavares10@hotmail.com>
2021-05-06 15:41:26 -0300

    Requisito 7 e 8 - Altera a cor de fundo da tarefa selecionada

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ window.onload = function () {
   const getTaskInput = document.querySelector('#texto-tarefa');
 
   function addTask() {
-    let task = document.createElement('li');
+    const task = document.createElement('li');
     task.innerHTML = getTaskInput.value;
     getToDoList.appendChild(task);
     getTaskInput.value = '';
   }
   getAddTaskButton.addEventListener('click', addTask);
+
+  function selectTask(event) {
+    const taskSelected = document.querySelector('.selected');
+    if (taskSelected !== null) {
+      taskSelected.classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  }
+  getToDoList.addEventListener('click', selectTask);
 };
