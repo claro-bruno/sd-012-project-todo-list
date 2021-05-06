58220616745ef1af834b890fc1b736810fb1bb95
igor <igorsife80@gmail.com>
2021-05-06 18:26:42 -0300

    captura todos os elementos html com getElementById e passa pra variaveis

---
+++ script.js
@@ -0,0 +1,10 @@
+let Adicionar = document.getElementById('criar-tarefa');
+let apagarGeralX = document.getElementById('apaga-tudo');
+let aCima = document.getElementById('mover-cima');
+let aBaixo = document.getElementById('mover-baixo');
+let limpaComp = document.getElementById('remover-finalizados');
+let salvaLis = document.getElementById('salvar-tarefas');
+let removeSel = document.getElementById('remover-selecionado');
+
+let caixaInput = document.getElementById('texto-tarefa');
+let list = document.getElementById('lista-tarefas');
