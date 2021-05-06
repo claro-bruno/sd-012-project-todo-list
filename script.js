b408af6bd861c6418dcc545dc7ebee00289db71f
David Azevedo <zevid.dev@outlook.com>
2021-05-06 16:12:01 -0300

    Done requirement 7

--- script.js
+++ script.js
@@ -18,6 +18,17 @@ function clearInput() {
   contentInput.value = '';
 }
 
+function addBgcItemList(event) {
+  let itemList = document.querySelectorAll('li');
+  for (let items = 0; items <= itemList.length; items += 1) {
+    if (event.target === itemList[items]) {
+      event.target.style.backgroundColor = 'rgb(128,128,128)';
+    }
+  }
+}
+
 let buttonSubmit = document.querySelector('#criar-tarefa');
+let ordenedList = document.querySelector('#lista-tarefas');
 buttonSubmit.addEventListener('click', createListItem);
 buttonSubmit.addEventListener('click', clearInput);
+ordenedList.addEventListener('click', addBgcItemList);
