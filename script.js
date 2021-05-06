f4ab9517d56bed9f6a96b1ab6d17c5a01f55ec69
Luciano AAP <lucianoalmeidaap@gmail.com>
2021-05-06 16:03:31 -0300

    corrige o click e impede seleçao de multiplas tarefas

--- script.js
+++ script.js
@@ -14,7 +14,15 @@ function addTarefa() {
 botaoAddTarefa.addEventListener('click', addTarefa);
 
 function alteraCor(event) {
-    event.target.style.backgroundColor = 'rgb(128,128,128)';
+    if (document.querySelector('.selected') === null) {
+    event.target.classList.add('selected');
+    document.querySelector('.selected').style.backgroundColor = 'rgb(128,128,128)';
+    } else {
+    document.querySelector('.selected').style.backgroundColor = 'white';
+    document.querySelector('.selected').classList.remove('selected');
+    event.target.classList.add('selected');
+    document.querySelector('.selected').style.backgroundColor = 'rgb(128,128,128)';
+    }
 }
 
-listaDeTarefas.addEventListener('dblclick', alteraCor);
+listaDeTarefas.addEventListener('click', alteraCor);
