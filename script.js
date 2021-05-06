2a53562b94959fddbb8237b0664751c75de86a0f
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-05-06 16:26:39 -0300

    Resolve requisito 5

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-// function createOrderList() {
-//   const orderList = document.createElement('ol');
-//   orderList.id = 'lista-tarefas';
-//   document.body.appendChild(orderList);
-// }
-// createOrderList();
+function insertTask(value) {
+  const orderList = document.createElement('li');
+  orderList.className = 'task-list';
+  orderList.innerHTML = value;
+  const taskList = document.querySelector('#lista-tarefas');
+  
+  taskList.appendChild(orderList);
+}
+
+ function receiveInput(){
+    let contentInput = document.querySelector('#texto-tarefa').value;
+    insertTask(contentInput);
+    document.querySelector('#texto-tarefa').value = '';
+}
+
+
+
+
