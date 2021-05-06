0397bb85aed7ab872acd96f6e6d02795ec5f8a80
Stefano <stefanooliveira1@gmail.com>
2021-05-06 16:24:22 -0300

    Ex 5 - termina requisito 5

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function buttonClick () {
     let getButton = document.querySelector('#criar-tarefa');
     let getUL = document.querySelector('#lista-tarefas');
+    let inputValue = document.querySelector('#texto-tarefa').value;
     let newListItem = document.createElement('li');
 
     getButton.addEventListener('click', function() {
-        
+        newListItem.innerHTML = inputValue;
+        console.log(newListItem)
+        getUL.appendChild(newListItem)
+
+        inputValue = '';
     })
 }
+console.log(newListItem)
+buttonClick();
