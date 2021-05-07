054a691ec57b4a8f69357dfb0cd1d8386c00a5c0
MiguelRetroz <miguel.retroz@outlook.com>
2021-05-07 10:00:31 -0300

    Requirement 12-> Storing tasks in the localStorage

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btnTaskCreate = document.querySelector('#criar-tarefa');
 const btnRemoveAllTasks = document.querySelector('#apaga-tudo');
 const btnRemoveCompletedTasks = document.querySelector('#remover-finalizados');
 const btnRemoveSelectedTask = document.querySelector('#remover-selecionado');
+const btnSaveTasks = document.querySelector('#salvar-tarefas');
 const textInput = document.querySelector('#texto-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 
@@ -58,11 +59,48 @@ function removeSelectedTask() {
   }
 }
 
+function saveTasks() {
+  if (typeof (Storage) !== 'undefined') {
+    let allTasks = document.getElementsByClassName('tarefa');
+    for (let index = 0; index < allTasks.length; index += 1) {
+      const taskText = allTasks[index].innerText;
+      const taskIsCompleted = allTasks[index].classList.contains('completed');
+      localStorage.setItem(`task${index}`, `${taskText},${taskIsCompleted}`);
+    }
+  } else {
+    alert(`Não foi possível salvar.
+Navegador sem suporte para Web Storage.`);
+  }
+}
+
+function taskRecreator(text, isCompleted) {
+  const taskItem = document.createElement('li');
+  if (isCompleted === "true") {
+    taskItem.className = 'tarefa completed';
+  } else {
+    taskItem.className = 'tarefa';
+  }
+  taskItem.innerText = text;
+  taskList.appendChild(taskItem);
+}
+
+function restoreSaveTasks() {
+  console.log(localStorage.length);
+  for (let index = 0; index < (localStorage.length); index += 1) {
+    const taskSaveValues = localStorage[`task${index}`].split(',');
+    taskRecreator(taskSaveValues[0], taskSaveValues[1]);
+  }
+}
+
+console.log()
+
 window.onload = function page() {
   btnTaskCreate.addEventListener('click', taskCreator);
   btnRemoveAllTasks.addEventListener('click', removeAllTasks);
   btnRemoveCompletedTasks.addEventListener('click', removeTasksComplete);
   btnRemoveSelectedTask.addEventListener('click', removeSelectedTask);
+  btnSaveTasks.addEventListener('click', saveTasks);
   document.addEventListener('click', selectTask);
   document.addEventListener('dblclick', completeTask);
+  restoreSaveTasks();
 };
