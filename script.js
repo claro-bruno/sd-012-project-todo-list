166d9f2ad197b949090904fe16f1ed7142dee082
igor <igorsife80@gmail.com>
2021-05-07 13:20:26 -0300

    adiciona comentarios pra facilitar o code reveiw

--- script.js
+++ script.js
@@ -5,20 +5,21 @@ let aBaixo = document.getElementById('mover-baixo');
 let limpaComp = document.getElementById('remover-finalizados');
 let salvaLis = document.getElementById('salvar-tarefas');
 let removeSel = document.getElementById('remover-selecionado');
-
+//ate aqui caapturei todos os elementos html dos botões <3
 let caixaInput = document.getElementById('texto-tarefa');
 let listarray = document.getElementById('lista-tarefas');
-
+//aqu capturei os elementos html da caixa de input e do arrey que iremos criar com ela <3
 function addlist(){
  let cria = document.createElement('li')
  cria.innerHTML = caixaInput.value;
-    listarray.appendChild(cria)
+    listarray.appendChild(cria) // elemento_pai.appendchild(elemento_filho)
     return cria
-
+//aqui criamos uma variavel 'cria' para criamos as tags <li> dentro das nossas ul
 };
 function limpacaixa(){
     caixaInput.value= '';
     return caixaInput
+    //nessa função atribuimos o valor '' para limpar a caixa apor clicar em adicionar 
 }
 Adicionar.addEventListener('click',addlist,);
 Adicionar.addEventListener('click', limpacaixa);
