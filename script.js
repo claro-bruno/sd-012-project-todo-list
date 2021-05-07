47b2762329c0c1db9d12cae99f0b1380f5822754
Vinincius-Naoke <viniciusnaokert@gmail.com>
2021-05-07 16:51:48 -0300

    adiciona funcionalidade para lista

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const input = document.getElementById('texto-tarefa');
 const list = document.getElementById('lista-tarefas');
 const addTask = document.getElementById('cria-tarefa');
-
+const listItemClass = document.getElementsByClassName('list-item');
 addTask.addEventListener('click', addListItem);
+
+function giveListener(){
+   listItemClass.addEventListener('click', changeBgCOlor());
+}
 
 function addListItem(){
 let listItem = document.createElement('li');
 listItem.innerHTML = input.value;
+listItem.className ='list-item';
 list.appendChild(listItem);
+giveListener();
+}
+
+
+
+
+function changeBgCOlor (){
+    listItemClass
 }
