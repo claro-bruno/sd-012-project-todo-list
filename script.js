aa1db814248dfc3f24cac91cccd0f79b1f30d871
Nuwanda <morato_dev@protonmail.com>
2021-05-07 13:48:20 -0300

    exercicio 9 e 10 finalizados junto com refatoraçao de algumas partes

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 const addToListButton = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const typedTextInput = document.getElementById('texto-tarefa');
+const allItemList = document.getElementsByTagName('li');
+const eraserButton = document.getElementById('apaga-tudo');
 
 addToListButton.addEventListener('click', () => {
-  const textAsItemList = document.createElement('li');
-  textAsItemList.innerHTML = typedTextInput.value;
-  taskList.appendChild(textAsItemList);
+  const textAsListItem = document.createElement('li');
+  textAsListItem.innerHTML = typedTextInput.value;
+  taskList.appendChild(textAsListItem);
+  textAsListItem.addEventListener('click', (event) => {
+    for (let index = 0; index < allItemList.length; index += 1) {
+      allItemList[index].style.backgroundColor = '';
+    }
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
 });
 
-addToListButton.addEventListener('click', () => {
-  typedTextInput.value = '';
+function cleanTheImput() {
+  addToListButton.addEventListener('click', () => {
+    typedTextInput.value = '';
+  });
+}
+cleanTheImput();
+
+function completedTask() {
+  taskList.addEventListener('dblclick', (event) => {
+    event.target.classList.toggle('completed');
+  });
+}
+completedTask();
+
+eraserButton.addEventListener('click', () => {
+  taskList.innerHTML = '';
 });
