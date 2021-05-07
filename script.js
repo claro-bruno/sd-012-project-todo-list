b0a4646f0e1070ec915d07d20ed2b2477b5db25d
pedro21medeiros <pedro21medeiros@gmail.com>
2021-05-07 16:35:40 -0300

    Tenta resolver erros do Linter

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 function clearTextInput() {
-    document.querySelector('input').value = '';
+  document.querySelector('input').value = '';
 }
 
 let buttonAdd = document.querySelector('#criar-tarefa');
 let listOfItems = document.querySelector('#lista-tarefas');
 buttonAdd.addEventListener('click', () => {
-    let valueInput = document.querySelector('input').value;
-    let itemList = document.createElement('li');
-    itemList.innerHTML = valueInput;
-    itemList.className = 'task';
-    listOfItems.appendChild(itemList);
-    clearTextInput();
+  let valueInput = document.querySelector('input').value;
+  let itemList = document.createElement('li');
+  itemList.innerHTML = valueInput;
+  itemList.className = 'task';
+  listOfItems.appendChild(itemList);
+  clearTextInput();
 })
 
-//Consegui com ajuda do Ronald no plantão, também era dúvida de outro colega
+// Consegui com ajuda do Ronald no plantão, também era dúvida de outro colega:
 function selectTask(event) {
-    let targetedEvent = event.target;
-    if (targetedEvent.classList.contains('task')) {
-      let currentTaskSelected = document.querySelector('.selected');
-      if (currentTaskSelected !== null) {
-        currentTaskSelected.classList.remove('selected');
-      }
-      targetedEvent.classList.add('selected');
+  let targetedEvent = event.target;
+  if (targetedEvent.classList.contains('task')) {
+    let currentTaskSelected = document.querySelector('.selected');
+    if (currentTaskSelected !== null) {
+      currentTaskSelected.classList.remove('selected');
     }
+    targetedEvent.classList.add('selected');
   }
+}
 
-//Consegui realizar com ajuda de um colega e do Ronald no plantão:
-function completeTask(event) { 
+// Consegui realizar com ajuda de um colega e do Ronald no plantão:
+function completeTask(event) {
   let targetedEvent = event.target;
   if (targetedEvent.classList.contains('task')) {
     if (targetedEvent.classList.contains('completed')) {
@@ -37,7 +37,7 @@ function completeTask(event) {
   }
 }
 
-//Consegui com ajuda do post no Slack do colega:
+// Consegui com ajuda do post no Slack do colega:
 let eraseListButton = document.getElementById('apaga-tudo');
 function listClear() {
   let taskList = document.getElementById('lista-tarefas');
@@ -46,10 +46,8 @@ function listClear() {
   }
 }
 
-
-
-window.onload =  function() {
-    document.addEventListener('click', selectTask);
-    document.addEventListener('dblclick', completeTask);
-    eraseListButton.addEventListener('click', listClear);
-}
+window.onload = function () {
+  document.addEventListener('click', selectTask);
+  document.addEventListener('dblclick', completeTask);
+  eraseListButton.addEventListener('click', listClear);
+};
