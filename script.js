1fb6b53ab0214e1bcbfee174d47e2b74aa75426b
Eric Kreis <erickreis793@gmail.com>
2021-05-07 01:03:38 -0300

    Requisitos 13 e 14 refatorados :rocket:

--- script.js
+++ script.js
@@ -51,8 +51,8 @@ function clearList() {
 function clearCheckListClick() {
   const list = document.getElementById('lista-tarefas');
   const checkedElements = document.querySelectorAll('.completed');
-  for (const checkedElement of checkedElements) {
-    list.removeChild(checkedElement);
+  for (let index = 0; index < checkedElements.length; index += 1) {
+    list.removeChild(checkedElements[index]);
   }
 }
 
@@ -61,50 +61,32 @@ function clearCheckList() {
   clearCheckedButton.addEventListener('click', clearCheckListClick);
 }
 
-function moveUpOrDownBlock(items, index, upOrDown) {
-  if (upOrDown === 'up') {
-    const elementUp = items[index - 1].innerHTML;
-    items[index - 1].innerHTML = items[index].innerHTML;
-    items[index].innerHTML = elementUp;
-    items[index - 1].classList.add('selected');
-    items[index].classList.remove('selected');
-  } else if (upOrDown === 'down') {
-    const elementUp = items[index + 1].innerHTML;
-    items[index + 1].innerHTML = items[index].innerHTML;
-    items[index].innerHTML = elementUp;
-    items[index + 1].classList.add('selected');
-    items[index].classList.remove('selected');
+function moveRules(selected, items, reference) {
+  if (selected === items[0] && reference === 'up') {
+    return true;
+  } if (selected === items[items.length - 1] && reference === 'down') {
+    return true;
   }
 }
 
-function upConditionals(selected, index, items) {
-  if (selected === items[index]) {
-    if (index === 0) {
-      alert('[ERROR] Não pode mover para cima');
-    } else {
-      moveUpOrDownBlock(items, index, 'up');
-    }
-  }
-}
-
-function moveUpExec() {
-  const items = document.querySelectorAll('.item');
-  const selected = document.querySelector('.selected');
+function moveNull(selected) {
   if (selected === null) {
-    alert('NÃO É POSSIVEL MOVER PARA CIMA');
-  } else {
-    for (let index = 0; index < items.length; index += 1) {
-      upConditionals(selected, index, items);
-    }
+    return alert('Selecione uma tarefa para mover.');
   }
 }
 
 function moveUpClick() {
+  const list = document.getElementById('lista-tarefas');
   const selected = document.querySelector('.selected');
-  if (selected === null) {
-    alert('NÃO É POSSIVEL MOVER');
-  } else {
-    moveUpExec();
+  const items = document.querySelectorAll('.item');
+  moveNull(selected);
+  if (moveRules(selected, items, 'up')) {
+    return alert('Não é possível mover para cima');
+  }
+  for (let index = 0; index < items.length; index += 1) {
+    if (selected === items[index]) {
+      list.insertBefore(items[index], items[index - 1]);
+    }
   }
 }
 
@@ -113,30 +95,18 @@ function moveUp() {
   buttonDown.addEventListener('click', moveUpClick);
 }
 
-function downConditionals(selected, items, index) {
-  if (selected === items[index]) {
-    if (index === (items.length - 1)) {
-      alert('[ERROR] Não pode mover para baixo');
-    } else {
-      moveUpOrDownBlock(items, index, 'down');
-    }
-  }
-}
-
-function moveDownExec() {
-  const items = document.querySelectorAll('.item');
-  const selected = document.querySelector('.selected');
-  for (let index = 0; index < items.length; index += 1) {
-    downConditionals(selected, items, index);
-  }
-}
-
 function moveDownClick() {
+  const list = document.getElementById('lista-tarefas');
   const selected = document.querySelector('.selected');
-  if (selected === null) {
-    alert('NÃO É POSSIVEL MOVER');
-  } else {
-    moveDownExec();
+  const items = document.querySelectorAll('.item');
+  moveNull(selected);
+  if (moveRules(selected, items, 'down')) {
+    return alert('Não é possível mover para baixo');
+  }
+  for (let index = 0; index < items.length; index += 1) {
+    if (selected === items[index]) {
+      list.insertBefore(items[index], items[index + 2]);
+    }
   }
 }
 
