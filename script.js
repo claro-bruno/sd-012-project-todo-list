eabb60ac4a37307a93565c92e4780e445de2df17
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-07 12:32:03 -0300

    Resolve requisito 13

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ const inputTxtTarefa = document.getElementById('texto-tarefa');
 const botaoLimparLista = document.getElementById('apaga-tudo');
 const botaoRemFin = document.getElementById('remover-finalizados');
 const botaoSalvTaref = document.getElementById('salvar-tarefas');
+const botaoMovCima = document.getElementById('mover-cima');
+const botaoMovBaixo = document.getElementById('mover-baixo');
 // Adiciona tarefas a lista e aplica sistemas de marcação
 function sistemaDeTarefas() {
   botaoCriarTarefa.addEventListener('click', () => {
     const criarTarefa = document.createElement('li');
     criarTarefa.addEventListener('click', (event) => {
-      const marcador = document.querySelector('.marcado');
+      const marcador = document.querySelector('.marcador');
       if (marcador) {
-        marcador.classList.toggle('marcado');
+        marcador.classList.toggle('marcador');
       }
-      event.target.classList.toggle('marcado');
+      event.target.classList.toggle('marcador');
     });
     criarTarefa.addEventListener('dblclick', (event) => {
       event.target.classList.toggle('completed');
@@ -36,14 +38,28 @@ botaoRemFin.addEventListener('click', () => {
     listaTarefas.removeChild(completos[index]);
   }
 });
-// Configura botão Salvar Tarefas
+// Configura botão Salvar Tarefas para salvar no localStorage
 botaoSalvTaref.addEventListener('click', () => {
   localStorage.setItem('tarefas', listaTarefas.innerHTML);
 });
-// Carrega tarefas salvas
+// Carrega tarefas salvas no localStorage se houver
 window.onload = () => {
   const tarefasSalvas = localStorage.getItem('tarefas');
   if (tarefasSalvas !== null) {
     listaTarefas.innerHTML = tarefasSalvas;
   }
 };
+// Configura botão Mover para Cima
+botaoMovCima.addEventListener('click', () => {
+  const tarefaMarcada = document.querySelector('.marcador');
+  if (tarefaMarcada !== null && tarefaMarcada.previousElementSibling !== null) {
+    listaTarefas.insertBefore(tarefaMarcada, tarefaMarcada.previousElementSibling);
+  }
+});
+// Configura botão Mover para Baixo
+botaoMovBaixo.addEventListener('click', () => {
+  const tarefaMarcada = document.querySelector('.marcador');
+  if (tarefaMarcada !== null && tarefaMarcada.nextElementSibling !== null) {
+    listaTarefas.insertBefore(tarefaMarcada, tarefaMarcada.nextElementSibling.nextElementSibling);
+  }
+});
