3958c0d6f64a540a5d495c8a10c3f242bca45e74
Igor.Duarte <igorhdsouza@gmail.com>
2021-05-07 15:35:16 -0300

    Requisito 02

---
+++ script.js
@@ -0,0 +1,5 @@
+// Requisito 2 
+let funcionamento = document.createElement('p');
+funcionamento.setAttribute('id', 'funcionamento');
+document.body.appendChild(funcionamento);
+funcionamento.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo'
