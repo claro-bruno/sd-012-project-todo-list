6a660982bea475a89eed00a827630a90f043804c
Igor.Duarte <igorhdsouza@gmail.com>
2021-05-07 15:39:31 -0300

    Requisito 03

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-// Requisito 2 
+// Requisito 02 
 let funcionamento = document.createElement('p');
 funcionamento.setAttribute('id', 'funcionamento');
 document.body.appendChild(funcionamento);
 funcionamento.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo'
+
+//Requisito 03
+let textoTarefa = document.createElement('input');
+textoTarefa.setAttribute('id', 'texto-tarefa');
+document.body.appendChild(textoTarefa);
