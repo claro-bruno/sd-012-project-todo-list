ee21a01844426c7c2e5d5f38d542ce7472664f2f
PeDD1 <pedrohr0909@gmail.com>
2021-05-07 17:18:28 -0300

    Requisitos Obrigatorios Revisados

--- script.js
+++ script.js
@@ -1,59 +1,48 @@
-window.onload = function () {
-    let buttonAdd = document.querySelector("#criar-tarefa");
+function eventSelected(e) {
+    let selected = document.querySelector('.selected')
+    if(selected !== null) {
+        selected.classList.remove('selected');
+        e.target.classList.add('selected');
+    } else {
+        e.target.classList.add('selected');
+    };
+};
+
+function eventCompleted(e) {
+    e.target.classList.toggle('completed');
+};
+
+function taskGenerator(taskString) {
     let taskList = document.querySelector("#lista-tarefas");
+    let taskItem = document.createElement('li');
+    taskItem.innerHTML = taskString;
+    taskItem.classList.add('tarefa');
+    taskItem.addEventListener("click", eventSelected);
+    taskItem.addEventListener('dblclick', eventCompleted);
+    taskList.appendChild(taskItem);
+};
+
+function rmBtn() {
+    let taskList = document.querySelector("#lista-tarefas");
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    };
+};
+
+function rmCompBtn () {
+    let completed = document.querySelectorAll(".completed");
+    for(index = 0; index < completed.length; index += 1) {
+        completed[index].remove();
+    };
+};
+
+window.onload = () => {
     let inputSection = document.querySelector("#texto-tarefa");
-    let buttonRm = document.querySelector("#apaga-tudo");
-    let buttomRmComp = document.querySelector("#remover-finalizados");
 
-    rmBtnCall();
-    addBtn();
-    rmCompBtnCall();
-
-    // Adiciona item na lista ordenada e Adiciona marcação ao clique
-    function taskGenerator() {
-        let taskItem = document.createElement('li');
-        taskItem.classList.add('tarefa');
-        taskItem.addEventListener("click", function(e) {
-            let selected = document.querySelector('.selected')
-            if(selected !== null) {
-                selected.classList.remove('selected');
-                e.target.classList.add('selected');
-            } else {
-                e.target.classList.add('selected');
-            }
-            
-        });
-        taskItem.addEventListener('dblclick', function(e) {
-            e.target.classList.toggle('completed');
-        });
-        taskItem.innerHTML = inputSection.value;
-        inputSection.value = "";
-        taskList.appendChild(taskItem);
-    };
-    
-    // Funcionamento do botão de Adicionar Tarefa
-    function addBtn() {
-        buttonAdd.addEventListener ("click", taskGenerator);
-    };
-
-    function rmBtn() {
-        while(taskList.firstChild) {
-            taskList.removeChild(taskList.firstChild);
-        };
-    };
-
-    function rmBtnCall () {
-        buttonRm.addEventListener('click', rmBtn);
-    };
-
-    function rmCompBtn () {
-        let completed = document.querySelectorAll(".completed");
-        for(index = 0; index < completed.length; index += 1) {
-            completed[index].remove();
-        };
-    };
-
-    function rmCompBtnCall () {
-        buttomRmComp.addEventListener('click', rmCompBtn);
-    };
+    document.querySelector("#criar-tarefa").addEventListener ("click", () => {
+        taskGenerator(inputSection.value);
+        inputSection.value = null;
+    });
+    document.querySelector("#apaga-tudo").addEventListener('click', rmBtn);
+    document.querySelector("#remover-finalizados").addEventListener('click', rmCompBtn);
 };
