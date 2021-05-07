fad97aba7072f2402b5b3740b8e432a964f84702
André Moreno <deco616@hotmail.com>
2021-05-07 19:28:13 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
 let buttonAdicionar = document.getElementById('criar-tarefa');
 let listaTarefas = document.getElementById('lista-tarefas');
 let inputTarefas = document.getElementById('texto-tarefa');
+let buttonApagar = document.getElementById('apaga-tudo');
 let elementosDaLista = listaTarefas.childNodes;
-
+ 
 buttonAdicionar.addEventListener('click', () => {
   let criarLi = document.createElement('li');
   criarLi.innerHTML = inputTarefas.value;
   listaTarefas.appendChild(criarLi);
-  //listaTarefas.insertBefore(criarLi, listaTarefas.childNodes[0]);
   inputTarefas.value = '';
-  marcarTarefa();
-  tarefaCompleta()
 })
 
-function marcarTarefa() {
-  listaTarefas.addEventListener('click', (event) => {
-    let element = document.querySelector('.selected');
-    if (element != null) {
-      element.classList.remove('selected');
-    }
-    event.target.classList.add('selected');
-  })
-}
+listaTarefas.addEventListener('click', (event) => {
+  let element = document.querySelector('.selected');
+  if (element != null) {
+    element.classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+})
 
-function tarefaCompleta() {
-  listaTarefas.addEventListener('dblclick', (event) => {
-    if (event.target.classList.contains('completed')) {
-      event.target.classList.remove('completed');
-    } else {
-      event.target.classList.add('completed');
-    }
-  })
-}
+listaTarefas.addEventListener('dblclick', (event) => {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+})
+
+buttonApagar.addEventListener('click', () => {
+  listaTarefas.innerHTML = '';
+})
