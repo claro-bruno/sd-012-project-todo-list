682d72e7d96134e001b464af330fa7eb4a536cfb
thales daniel <thalesdaniel2013@gmail.com>
2021-05-07 17:50:29 -0300

    feito a etapa 8

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ function criarItem(event) {
     document.querySelector('#lista-tarefas').appendChild(itemLista)
     let valorList = document.getElementById('texto-tarefa').value
     itemLista.addEventListener('click', function() {
-        itemLista.className = 'lista'
+
+        if (itemLista.classList.contains('lista')) {
+            itemLista.classList.remove('lista')
+        } else {
+            itemLista.className = 'lista'
+        }
     })
     itemLista.innerHTML = valorList
     document.getElementById('texto-tarefa').value = ""
+
 
 }
 
 let clickButton = document.getElementById("criar-tarefa")
 clickButton.addEventListener('click', criarItem)
+
+let clickList = document.querySelector('.lista')
