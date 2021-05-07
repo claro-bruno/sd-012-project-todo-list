366e7e3eefbf94d458cde0ecb3dc2c4aeaeb1e59
Gletison Ramos da Silva Junior <gletisonjr@gmail.com>
2021-05-06 21:45:46 -0300

    Adiciona requisito 10

--- script.js
+++ script.js
@@ -15,11 +15,23 @@ function criarTarefa() {
   });
   listItem.addEventListener('dblclick', () => {
     if (listItem.classList.contains('completed') === true) {
-     console.log(listItem.classList.remove('completed'));
-    }else {
+      console.log(listItem.classList.remove('completed'));
+    } else {
       listItem.classList.add('completed');
     }
   });
   list.appendChild(listItem);
   text.value = '';
+}
+
+function apagaLista() {
+  const list = document.querySelector('#lista-tarefas');
+  const listItemGroup = list.children;
+
+  for (let index = 0; index < listItemGroup.length; index += 1) {
+    listItemGroup[index].remove();
+  }
+  if (list.children.length > 0) {
+    apagaLista();
+  }
 }
