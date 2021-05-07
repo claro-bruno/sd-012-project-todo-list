009e2f94dade27810cda59ead7436b4ad780fcfd
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-05-07 17:33:37 -0300

    Resolve o requisito 05

---
+++ script.js
@@ -0,0 +1,16 @@
+
+//Requisito 05
+let botao = document.getElementById('criar-tarefa');
+botao.addEventListener('click', criaLista);
+//Criando a lista de tarefas
+function criaLista(event){
+    event.preventDefault();
+    let item = document.createElement('li');
+    item.className = 'lista';
+    document.querySelector('#lista-tarefas').appendChild(item);
+    let returnList = document.getElementById('texto-tarefa').value;
+    item.innerHTML = returnList;
+    
+
+}
+//console.log(valorList);
