d1382460bf55805f5cf749ec848ae02e0b820b32
Diogo Sant'Anna <diogo.santanna@outlook.com.br>
2021-05-07 02:09:43 -0300

    Corrige requisito 12

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const listaTarefas = document.querySelector('#lista-tarefas');
 const inputTxtTarefa = document.getElementById('texto-tarefa');
 const botaoLimparLista = document.getElementById('apaga-tudo');
 const botaoRemFin = document.getElementById('remover-finalizados');
+const botaoSalvTaref = document.getElementById('salvar-tarefas');
 // Adiciona tarefas a lista e aplica sistemas de marcação
 function sistemaDeTarefas() {
   botaoCriarTarefa.addEventListener('click', () => {
@@ -35,3 +36,14 @@ botaoRemFin.addEventListener('click', () => {
     listaTarefas.removeChild(completos[index]);
   }
 });
+// Configura botão Salvar Tarefas
+botaoSalvTaref.addEventListener('click', () => {
+  localStorage.setItem('tarefas', listaTarefas.innerHTML);
+});
+// Carrega tarefas salvas
+window.onload = () => {
+  const tarefasSalvas = localStorage.getItem('tarefas');
+  if (tarefasSalvas !== null) {
+    listaTarefas.innerHTML = tarefasSalvas;
+  }
+};
