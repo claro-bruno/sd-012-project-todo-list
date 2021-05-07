55169cd24790cf142b4a872a2563a9c39350df30
Guilherme Moreno <guilherme.lmoreno23@gmail.com>
2021-05-07 18:21:29 -0300

    Terminando exercise 5

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let list = document.querySelector("#list")
-function createOl (result){
-    for(let index = 0; index < result; index += 1){
-     let criaol = document.createElement("ol")
-     criaol.id = "lista-tarefas"   
-     list.appendChild(criaol)
 
+
+function createList(event){
+    event.preventDefault();
+     let criaList = document.createElement("li")
+     criaList.id = "listas"   
+     document.querySelector("#lista-tarefas").appendChild(criaList)
+     let valor = document.getElementById("texto-tarefa")
+     criaList.innerHTML = valor.value
+     valor.value = "";
+    
     }
-}
-createOl(3)
+    
+let buttonClick = document.getElementById("criar-tarefa")
+buttonClick.addEventListener("click", createList)
+
+  
