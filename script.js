78a0a2b8f48b077d1dbdc13e57db660ec51a9102
Milton Castro <castro.milton07@gmail.com>
2021-05-06 22:58:13 -0300

    02 - Requisito 2

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ const headerContent = document.querySelector('#header-content');
 
 function createHeaderContent() {
   headerContent.innerHTML = 'Minha Lista de Tarefas';
+  const paragraph = document.createElement('p');
+  paragraph.id = 'funcionamento';
+  paragraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
+  headerContent.appendChild(paragraph);
 }
 createHeaderContent();
