cedf2917f81784396b4c113df6611f3ea8548a63
Josimar Souza <contact.josimarsouza@gmail.com>
2021-05-07 20:12:15 -0300

    Refatora o codigo

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 window.onload = function() {
     let addButton = document.getElementById('criar-tarefa');
     let taskInput = document.getElementById('texto-tarefa');
-    let taskList = document.getElementById('lista-tarefas');
-    let listItems;    
+    let taskList = document.getElementById('lista-tarefas');    
 
     addButton.addEventListener('click', addTask);
 
     function addTask() {        
-        let listItem = document.createElement('li');
+        let listItem = document.createElement('li');        
         listItem.className = 'list-item';
         listItem.innerHTML = taskInput.value;
+        listItem.addEventListener('click', selectItem);
+        listItem.addEventListener('dblclick', itemCompleted);
         taskInput.value = '';
         taskList.appendChild(listItem);
 
-        getListItems();
+        //getListItems();
     }
-
+/*
     function getListItems() {
-        listItems = document.getElementsByClassName('list-item');        
-        
+        listItems = document.querySelectorAll('.list-item');
+        console.log(listItems);
         for (let index = 0; index < listItems.length; index += 1) {
             listItems[index].addEventListener('click', selectItem); 
             listItems[index].addEventListener('dblclick', itemCompleted);
         }
     }
-
+*/
     function selectItem(eventSource) {
-        for (let index = 0; index < (listItems.length); index += 1) {                       
+        let listItems = document.querySelectorAll('.list-item');
+
+        for (let index = 0; index < (listItems.length); index += 1) {
             listItems[index].classList.remove('item-selected');            
         }
-
-        if (eventSource.target.classList.contains('completed')) {
-            eventSource.target.className = 'list-item item-selected completed'
-        } else {
-            eventSource.target.classList.add('item-selected');
-        }        
+        
+        eventSource.target.classList.add('item-selected');
+               
     }
 
-    function itemCompleted(eventSource) {
-        eventSource.target.classList.toggle('completed');
+    function itemCompleted(event) {
+        event.target.classList.toggle('completed');
     }
 }
