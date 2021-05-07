db6fd5d64806067f260674b0797bd5b84e2a24ef
Milton Castro <castro.milton07@gmail.com>
2021-05-06 22:56:53 -0300

    01 - Requisito 1

---
+++ script.js
@@ -0,0 +1,6 @@
+const headerContent = document.querySelector('#header-content');
+
+function createHeaderContent() {
+  headerContent.innerHTML = 'Minha Lista de Tarefas';
+}
+createHeaderContent();
