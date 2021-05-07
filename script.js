645e79e70993110b413263c31ff932ddaa91000c
Patrick Dack <patrickdack89@gmail.com>
2021-05-07 04:23:28 -0300

    Requisito 14

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const taskList = document.getElementById('lista-tarefas');
 const btnSave = document.getElementById('salvar-tarefas');
 const btnUP = document.getElementById('mover-cima');
 const btnDown = document.getElementById('mover-baixo');
+const btnRemoveSelected = document.getElementById('remover-selecionado')
 // Captura o texto do input e adiciona à lista
 function catchInput() {
   btnAdd.addEventListener('click', () => {
@@ -52,7 +53,7 @@ function removeAll() {
 }
 // Botao para remover finalizados
 btnXX.addEventListener('click', removeCompleted);
-function removeCompleted () {
+function removeCompleted() {
   const completed = document.querySelectorAll('.completed');
   for (const item of completed) {
     taskList.removeChild(item);
@@ -67,24 +68,31 @@ window.onload = function () {
   taskList.innerHTML = localStorage.getItem('task');
 }
 // Move tarefas para cima
-function moveUp () {
-  const itemList = document.querySelectorAll('.item-list');
+function moveUp() {
+  const ol = document.querySelector('#lista-tarefas');
+  console.log(ol);
   for (let index = 0; index < itemList.length; index += 1) {
     let item = itemList[index];
-    if(item.style.backgroundColor === 'rgb(128, 128, 128)' && item === itemList[0]) {
-      break;
-    }
+
+    // if(item.style.backgroundColor === 'rgb(128, 128, 128)' && item === itemList[0]) {
+    //   break;
+    // }
     if (item.style.backgroundColor === 'rgb(128, 128, 128)') {
-      item.style.backgroundColor = null
-      item.style.fontSize = '16px';
-      item.previousElementSibling.style.backgroundColor = 'rgb(128, 128, 128)';
-      item.previousElementSibling.style.fontSize = '20px';
+      let temp;
+      temp = itemList[index]
+      itemList[index - 1] = itemList[index];
+      // itemList[index -1] = temp;
+      break;
+      // item.style.backgroundColor = null
+      // item.style.fontSize = '16px';
+      // item.previousElementSibling.style.backgroundColor = 'rgb(128, 128, 128)';
+      // item.previousElementSibling.style.fontSize = '20px';
     }
   }
 }
 btnUP.addEventListener('click', moveUp);
 // Move tarefas para baixo
-function moveDown () {
+function moveDown() {
   const itemList = document.querySelectorAll('.item-list');
   for (let index = 0; index < itemList.length; index += 1) {
     let item = itemList[index];
@@ -101,3 +109,14 @@ function moveDown () {
   }
 }
 btnDown.addEventListener('click', moveDown);
+//Remove selecionados
+function removeSelected() {
+  const itemList = document.querySelectorAll('.item-list');
+  for (let index = 0; index < itemList.length; index += 1) {
+    let item = itemList[index];
+    if (item.style.backgroundColor === 'rgb(128, 128, 128)') {
+      taskList.removeChild(item);
+    }
+  }
+}
+btnRemoveSelected.addEventListener('click', removeSelected);
