204e6f6dce119e35d000f3705d0837a132119977
MGSiqueira <marcos.siqueira98@gmail.com>
2021-05-07 19:21:54 -0300

    Commitão bomba dos requisitos 12, 13 e 14

--- script.js
+++ script.js
@@ -1,71 +1,107 @@
+const input = document.getElementById('texto-tarefa');
+const createButton = document.getElementById('criar-tarefa');
+const orderedList = document.getElementById('lista-tarefas');
+const saveButton = document.getElementById('salvar-tarefas');
+const eraseButton = document.getElementById('apaga-tudo');
+const rmCompletedsButton = document.getElementById('remover-finalizados');
+const upButton = document.getElementById('mover-cima');
+const downButton = document.getElementById('mover-baixo');
+const rmSelected = document.getElementById('remover-selecionado');
 // faz o botao criar uma tarefa
-function criarTarefa() {
-  let input = document.getElementById('texto-tarefa');
-  let botao = document.getElementById('criar-tarefa');
-  let orderedList = document.getElementById('lista-tarefas');
-  botao.addEventListener('click', function() {
-    if (input.value !== '') {
-      let listItem = document.createElement('li');
-      listItem.innerHTML = input.value;
-      orderedList.appendChild(listItem);
-      textEqualWidth(orderedList);
-      input.value = ''
-    } else {alert('Nenhuma tarefa adicionada!')}
-  });
+function criarTarefa(texto) {
+  if (texto.value !== '') {
+    let listItem = document.createElement('li');
+    listItem.innerHTML = texto.value;
+    orderedList.appendChild(listItem);
+    texto.value = '';
+  } else { alert('Nenhuma tarefa adicionada!') }
 }
-criarTarefa();
 
-// cursor pointer do tamanho do conteúdo
-function textEqualWidth(lista) {
-  let text = document.getElementById('texto-tarefa').value;
-  let textLength = text.length;
-  //lista.style.width = `${textLength}px`
-  lista.style.cursor = 'pointer'
-}
+createButton.addEventListener('click', () => criarTarefa(input));
 
 // pinta só um de cinza
-function clicar128() {
-  let orderedList = document.getElementById('lista-tarefas');
-  orderedList.addEventListener('click', function(event) {
-    let previous = document.querySelector('.selected');
-    if (!previous) {
-    } else { previous.classList.remove('selected') }
+function clicar128(event) {
+  let previous = document.querySelector('.selected');
+  if (!previous) { 
+    event.target.classList.add('selected') 
+  } else if (event.target.classList.contains('selected')){ 
+    event.target.classList.remove('selected');
+  } else {
+    previous.classList.remove('selected');
     event.target.classList.add('selected');
-  })
+  }
 }
-clicar128();
+orderedList.addEventListener('click', clicar128);
 
 // double click para riscar
-function clicarRiscar() {
-  let orderedList = document.getElementById('lista-tarefas');
-  orderedList.addEventListener('dblclick', function(event) {
-    if (event.target.classList.contains('completed')) {
-      event.target.classList.remove('completed');
-    } else {
-      event.target.classList.add('completed');
-    }
-  });
+function clicarRiscar(event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
 }
-clicarRiscar();
+orderedList.addEventListener('dblclick', clicarRiscar);
 
 // faz botão apagar tudo
 function apagaTudo() {
-  let botao = document.getElementById('apaga-tudo');
-  let orderedList = document.getElementById('lista-tarefas');
-  botao.addEventListener('click', function() {
     orderedList.innerHTML = '';
-  });
+    localStorage.clear();
 }
-apagaTudo();
+eraseButton.addEventListener('click', apagaTudo);
 
 // apaga tarefas completadas
 function removeCompleted() {
-  let botao = document.getElementById('remover-finalizados');
-  botao.addEventListener('click', function() {
-  let completed = document.querySelectorAll('.completed');
-    for (let i = 0; i < completed.length; i++) {
-      completed[i].remove();
-    }
-  });
+    let completed = document.querySelectorAll('.completed');
+      for (let i = 0; i < completed.length; i++) {
+        completed[i].remove();
+      }
+    localStorage.clear()
+    saveStorage();
 }
-removeCompleted();
+rmCompletedsButton.addEventListener('click', removeCompleted);
+
+// salva o html da orderedList
+function saveStorage() {
+    localStorage.setItem('ol', orderedList.innerHTML)
+  console.log(localStorage);
+}
+saveButton.addEventListener('click', saveStorage);
+
+// carrega o html da orderedList salvo na página
+window.onload = () => {
+  let storage = localStorage.getItem('ol');
+  orderedList.innerHTML = storage;
+}
+
+function moverCima() {
+  let selected = document.querySelector('.selected');
+  let upElement = selected.previousSibling;
+  if (upElement) {
+    let copy = document.createElement('li');
+    copy.innerHTML = selected.innerHTML;
+    copy.classList = selected.classList;
+    orderedList.insertBefore(copy, upElement);
+    selected.remove();
+  }
+}
+upButton.addEventListener('click', moverCima);
+
+function moverBaixo() {
+  let selected = document.querySelector('.selected');
+  let downElement = selected.nextSibling;
+  if (downElement) {
+    let copy = document.createElement('li');
+    copy.innerHTML = selected.innerHTML;
+    copy.classList = selected.classList;
+    orderedList.insertBefore(copy, downElement.nextSibling);
+    selected.remove();
+  }
+}
+downButton.addEventListener('click', moverBaixo);
+
+function removeSelecionado() {
+  let selected = document.querySelector('.selected');
+  selected.remove();
+}
+rmSelected.addEventListener('click', removeSelecionado);
