6ebb29c6ef8b9952f89001e0af977291a55513a5
Bruno Carv <bruno.fox_carvalho@hotmail.com>
2021-05-07 09:36:22 -0300

    format da res req5

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let bornTodo = document.querySelector("#criar-tarefa")
 
 function funcBornTodo() {
   let contentNewTodo = document.querySelector("#texto-tarefa");
-  if (contentNewTodo.value == '') {}
-  else{
-  let countList = document.getElementById("lista-tarefas");
-  let newItem = document.createElement("li");
-  countList.appendChild(newItem);
-  document.getElementById("lista-tarefas").lastChild.innerHTML = contentNewTodo.value;
-  contentNewTodo.value = "";};
+  if (contentNewTodo.value == '') {} else {
+    let countList = document.getElementById("lista-tarefas");
+    let newItem = document.createElement("li");
+    countList.appendChild(newItem);
+    document.getElementById("lista-tarefas").lastChild.innerHTML = contentNewTodo.value;
+    contentNewTodo.value = "";
+  };
 
 };
 
