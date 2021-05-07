9c794e47f4dea8a5992ae4c693c13b4f741e8b54
Josimar Souza <contact.josimarsouza@gmail.com>
2021-05-07 10:21:03 -0300

    Resolvido o requisito 7 e adicionado alguns estilos

--- script.js
+++ script.js
@@ -2,13 +2,33 @@ window.onload = function() {
     let addButton = document.getElementById('criar-tarefa');
     let taskInput = document.getElementById('texto-tarefa');
     let taskList = document.getElementById('lista-tarefas');
+    let listItems;
 
     addButton.addEventListener('click', addTask);
 
-    function addTask() {
+    function addTask() {        
         let listItem = document.createElement('li');
+        listItem.className = 'list-item';
         listItem.innerHTML = taskInput.value;
         taskInput.value = '';
         taskList.appendChild(listItem);
+
+        getListItems();
+    }
+
+    function getListItems() {
+        listItems = document.getElementsByClassName('list-item');        
+        
+        for (let index = 0; index < listItems.length; index += 1) {
+            listItems[index].addEventListener('click', selectItem);
+        }
+    }
+
+    function selectItem(eventSource) {        
+        for (let index = 0; index < (listItems.length); index += 1) {
+            listItems[index].className = 'list-item';
+        }
+
+        eventSource.target.className = 'list-item item-selected';
     }
 }
