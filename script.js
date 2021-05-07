ee091860d3cc19b077d24320dc7655c60af60823
matheus <matheus111904@hotmail.com>
2021-05-07 13:35:59 -0300

    requesito 7 concluido

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-const listaTarefa = document.getElementById('lista-tarefas');
+const listaTarefas = document.getElementById('lista-tarefas');
+const inputTexto = document.getElementById('texto-tarefa');
+const criarTarefa = document.getElementById('criar-tarefa');
+const task = document.getElementsByTagName('li');
+
 
 function createLi(){
-    const inputTexto = document.getElementById('texto-tarefa');
-    const criarTarefa = document.getElementById('criar-tarefa');
-
     criarTarefa.addEventListener('click', () => {
         const criarLista = document.createElement('li');
         criarLista.className = 'task';
         criarLista.innerHTML = inputTexto.value;
-        listaTarefa.appendChild(criarLista);
+        listaTarefas.appendChild(criarLista);
         inputTexto.value = '';
     })
-}
+};
 createLi();
+
+function backgroundColor() {
+    listaTarefas.addEventListener('click', (event) => {
+      const classSelected = document.querySelector('.selected');
+      if (classSelected) {
+        classSelected.classList.remove('selected');
+      }
+      event.target.classList.add('selected');
+    });
+  }
+  backgroundColor();
