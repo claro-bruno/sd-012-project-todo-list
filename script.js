e6330cf2c7bcbc1e3f23ddf226fd8629979aab04
Bruno Gomes <brunognovaes.cm@gmail.com>
2021-05-07 17:18:34 -0300

    Limpei o código e tudo continua funcionando

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
 function openSavedTasks() {
   const tasks = JSON.parse(localStorage.getItem('tasks'));
   for (let key in tasks) {
-    let taskText = key;
-    let isCompleted = tasks[key];
-    let task = document.createElement('li');
-    if (isCompleted) {
-      task.className = 'tarefa completed';
-    } else {
-      task.className = 'tarefa';
-    }
-    task.innerHTML = taskText;
-    let list = document.querySelector('#lista-tarefas');
-    list.appendChild(task);
-    deleteAllList();
-  }
+    const taskText = key;
+    const isCompleted = tasks[key];
+    createTask(taskText, isCompleted);
+  };
 }
 
 function saveTasks() {
-  let tasks = document.querySelectorAll('.tarefa');
-  console.log(tasks)
-  let tasksObj = {};
+  const tasks = document.querySelectorAll('.tarefa');
+  const tasksObj = {};
   for (let taskIndex = 0; taskIndex < tasks.length; taskIndex += 1) {
-    let task = tasks[taskIndex];
-    let taskValue = task.innerHTML;
-    console.log(task.classList)
+    const task = tasks[taskIndex];
+    const taskValue = task.innerHTML;
     let isTaskCompleted = false;
-    for (let classIndex = 0; classIndex < task.classList.length; classIndex += 1) {
-      if (task.classList[classIndex] === 'completed') {
-        isTaskCompleted = true;
-      };
+    if(task.classList.contains('completed')){
+      isTaskCompleted = true;
     };
     tasksObj[taskValue] = isTaskCompleted;
   };
@@ -39,100 +26,91 @@ function addToStorage(obj) {
   localStorage.setItem('tasks', JSON.stringify(obj));
 }
 
-function unmarkTask() {
-  let selectedTask = document.querySelector('#selected');
-  selectedTask.id = '';
+function selectTask(e) {
+  const selectedTask = document.querySelector('#selected');
+  if (selectedTask !== null) {
+    selectedTask.id = '';
+  };
+  e.target.id = 'selected';
 }
 
-function changeTaskColor() {
-  const taskList = document.querySelector('#lista-tarefas');
-  taskList.addEventListener('click', (e) => {
-    if (e.target.className === 'tarefa' || e.target.className === 'tarefa completed') {
-      if (document.querySelector('#selected') !== null) {
-        unmarkTask();
-      }
-      e.target.id = 'selected';
+function removeCompleted() {
+  const tasks = document.querySelectorAll('.tarefa');
+  for (let taskIndex = 0; taskIndex < tasks.length; taskIndex += 1) {
+    const task = tasks[taskIndex];
+    if (task.classList.contains('completed')) {
+      task.remove();
     };
-  });
+  }
 }
 
-function deleteCompletedTasks() {
-  const btn = document.querySelector('#remover-finalizados');
-  btn.addEventListener('click', () => {
-    const completedTasks = document.querySelectorAll('.tarefa.completed');
-    console.log(completedTasks)
-    for (let taskIndex = 0; taskIndex < completedTasks.length; taskIndex += 1) {
-      completedTasks[taskIndex].remove();
-    };
-  });
+function completeTask(e) {
+  const task = e.target;
+  if (task.classList.contains('completed')) {
+    task.classList.remove('completed');
+  } else {
+    task.classList.add('completed')
+  };
 }
 
-function completeTask() {
-  const taskList = document.querySelector('#lista-tarefas');
-  taskList.addEventListener('dblclick', (e) => {
-    if (e.target.className === 'tarefa') {
-      e.target.className += ' completed';
-    } else if (e.target.className === 'tarefa completed') {
-      e.target.className = 'tarefa';
-    };
-    deleteCompletedTasks();
-  });
-}
 
 function deleteAllList() {
-  const btn = document.querySelector('#apaga-tudo');
-  btn.addEventListener('click', () => {
-    const tasks = document.querySelectorAll('.tarefa');
-    for (let taskIndex = 0; taskIndex < tasks.length; taskIndex += 1) {
-      tasks[taskIndex].remove();
-    };
-  });
+  const tasks = document.querySelectorAll('.tarefa');
+  for (let taskIndex = 0; taskIndex < tasks.length; taskIndex += 1) {
+    const task = tasks[taskIndex];
+    task.remove();
+  };
 }
 
-function createTask() {
-  let btn = document.querySelector('#criar-tarefa');
-  btn.addEventListener('click', () => {
-    let input = document.querySelector('#texto-tarefa');
-    let task = document.createElement('li');
-    task.className = 'tarefa';
-    task.innerHTML = input.value;
-    input.value = '';
-    let list = document.querySelector('#lista-tarefas');
-    list.appendChild(task);
-    deleteAllList();
-  });
+function createTask(taskText, isCompleted) {
+  const task = document.createElement('li');
+  task.innerHTML = taskText;
+  task.classList.add('tarefa');
+  if(isCompleted){
+    task.classList.add('completed')
+  };
+  task.addEventListener('dblclick', completeTask);
+  task.addEventListener('click', selectTask);
+  const list = document.querySelector('#lista-tarefas');
+  list.appendChild(task);
 }
 
 function moveUp() {
-  let taskSelected = document.querySelector('#selected');
-  if(taskSelected !== null && taskSelected !== taskSelected.parentNode.firstElementChild){
-    let previousTask = taskSelected.previousElementSibling;
+  const taskSelected = document.querySelector('#selected');
+  if (taskSelected !== null && taskSelected !== taskSelected.parentNode.firstElementChild) {
+    const previousTask = taskSelected.previousElementSibling;
     taskSelected.parentNode.insertBefore(previousTask, taskSelected.nextElementSibling);
   };
 }
 
 function moveDown() {
   let taskSelected = document.querySelector('#selected');
-  if(taskSelected !== null && taskSelected !== taskSelected.parentNode.lastElementChild){
+  if (taskSelected !== null && taskSelected !== taskSelected.parentNode.lastElementChild) {
     let nextTask = taskSelected.nextElementSibling;
     taskSelected.parentNode.insertBefore(nextTask, nextTask.previousElementSibling);
   };
 }
 
 function removeSelected() {
-    const selected = document.querySelector('#selected');
+  const selected = document.querySelector('#selected');
+  if (selected !== null) {
     selected.remove();
+  };
 }
 
 window.onload = () => {
   openSavedTasks();
-  createTask();
-  changeTaskColor();
-  completeTask();
-
-  const btnSaveTasks = document.querySelector('#salvar-tarefas');
-  btnSaveTasks.addEventListener('click', saveTasks);
+  document.querySelector('#salvar-tarefas').addEventListener('click', saveTasks);
   document.querySelector('#mover-cima').addEventListener('click', moveUp);
   document.querySelector('#mover-baixo').addEventListener('click', moveDown);
   document.querySelector('#remover-selecionado').addEventListener('click', removeSelected);
+  document.querySelector('#remover-finalizados').addEventListener('click', removeCompleted);
+  document.querySelector('#apaga-tudo').addEventListener('click', deleteAllList);
+  document.querySelector('#criar-tarefa').addEventListener('click', () => {
+    const input = document.querySelector('#texto-tarefa');
+    input
+    if(input.value)
+    createTask(input.value);
+    input.value = '';
+  });
 };
