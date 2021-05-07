ed03ed3d5d5dbe81e6813f0f9cbc2cfcaf8f5b1e
Elias Forte <eliasforte.dev@gmail.com>
2021-05-07 20:19:54 -0300

    teste de resoluçao do requisito 5

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ let textInput = document.querySelector('#texto-tarefa');
 const buttonCreate = document.querySelector('.submit');
 
 buttonCreate.addEventListener('click',function () {
-    let catOl = document.getElementById('lista-tarefas');
-    let text = textInput.value;
-    let createLi = document.createElement('li');
+    const catOl = document.getElementById('lista-tarefas');
+    const text = textInput.value;
+    const createLi = document.createElement('li');
     createLi.className = 'unselected';
     createLi.innerHTML = text;
     catOl.appendChild(createLi);
-    document.getElementById('texto-tarefa').value = '';
-} );
+    document.getElementById('texto-tarefa').value = null;
+});
