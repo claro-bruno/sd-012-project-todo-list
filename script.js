fd59ae0188f554c80e8b4353a21009eb69d0130f
Bruno Carv <bruno.fox_carvalho@hotmail.com>
2021-05-07 00:01:20 -0300

    Cont do req 5

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-document.querySelector("#criar-tarefa")
-
+let bornTodo = document.querySelector("#criar-tarefa")
 let countList = document.getElementById("lista-tarefas");
-let newItem = document.createElement ("li");
-countList.appendChild(newItem);
+let newItem = document.createElement("li");
 
+function funcBornTodo() {
+  countList.appendChild(newItem);
+  let contentNewTodo = document.querySelector("#texto-tarefa").value;
+  
+};
+
+bornTodo.addEventListener("click", funcBornTodo);
