cf82ea28f68e076a18b2441d72c498f739c80e50
Sass <dudu_sass12@hotmail.com>
2021-05-07 17:10:58 -0300

    consertando lint

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
-const form = document.getElementById("form");
-const ol = document.getElementById("lista-tarefas");
-const botao = document.getElementById("criar-tarefa");
-const botao1 = document.getElementById("apaga-tudo");
+const form = document.getElementById('form');
+const ol = document.getElementById('lista-tarefas');
+const botao = document.getElementById('criar-tarefa');
+const botao1 = document.getElementById('apaga-tudo');
+const botao2 = document.getElementById('remover-finalizados');
 
-botao.addEventListener("click", (e) => {
+botao.addEventListener('click', (e) => {
   e.preventDefault();
 
-  let input = document.getElementById("texto-tarefa");
-  let listaOrganizada = document.createElement("li");
-	listaOrganizada.innerHTML = input.value
+  const input = document.getElementById('texto-tarefa');
+  const listaOrganizada = document.createElement('li');
+  listaOrganizada.innerHTML = input.value;
   ol.appendChild(listaOrganizada);
 
-  input.value = ""
+  input.value = "";
 
-  listaOrganizada.addEventListener("click", () => {
-  listaOrganizada.style.backgroundColor= "rgb(128, 128, 128)"
-  })
+  listaOrganizada.addEventListener('click', () => {
+    listaOrganizada.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
 
-  listaOrganizada.addEventListener("dblclick", () => {
-  listaOrganizada.classList.toggle('completed');
+  listaOrganizada.addEventListener('dblclick', () => {
+    listaOrganizada.classList.toggle('completed');
+  });
+  ol.appendChild(listaOrganizada);
+  input.value = "";
 });
-  ol.appendChild(listaOrganizada);
-  input.value = ""
 
-})
-
-botao1.addEventListener("click", () => {
-  let removerDaLista = document.querySelectorAll("li");
-  for(index = 0; index < removerDaLista.length; index += 1);
+botao1.addEventListener('click', () => {
+  const removerDaLista = document.querySelectorAll('li');
   ol.remove(removerDaLista);
-})
+});
+
+botao2.addEventListener('click', (e) => {
+  e.preventDefault()
+  const removerFinalizado = document.getElementsByClassName('completed');
+  console.log(removerFinalizado);
+  ol.remove(removerFinalizado); 
+});
