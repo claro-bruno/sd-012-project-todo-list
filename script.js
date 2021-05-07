a382f2475f6533c5eb9893cbd584afc85bf0c79e
WanderDinizVeloso <wanveloso@yahoo.com.br>
2021-05-06 22:47:03 -0300

    conclusao do Desafio_07 e 08, onde, com um click, altero a cor de fundo da lista de tarefas.

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ const inputIdTextoTarefaCria = document.createElement('input');
 inputIdTextoTarefaCria.id = 'texto-tarefa';
 bodyPagina.appendChild(inputIdTextoTarefaCria);
 
-const olIdListaTarefaCria = document.createElement('ol');
-olIdListaTarefaCria.id = 'lista-tarefas';
-bodyPagina.appendChild(olIdListaTarefaCria);
-
 const botaoIdCriarTarefaCria = document.createElement('button');
 botaoIdCriarTarefaCria.id = 'criar-tarefa';
 botaoIdCriarTarefaCria.innerText = 'Criar Tarefa';
 bodyPagina.appendChild(botaoIdCriarTarefaCria);
+
+const olIdListaTarefaCria = document.createElement('ol');
+olIdListaTarefaCria.id = 'lista-tarefas';
+bodyPagina.appendChild(olIdListaTarefaCria);
 
 function adicionaTarefas() {
   const inputIdTextoTarefa = document.querySelector('#texto-tarefa');
@@ -25,14 +25,30 @@ function adicionaTarefas() {
 
   botaoIdCriarTarefa.addEventListener('click', () => {
     const novaLi = document.createElement('li');
-    novaLi.innerText = inputIdTextoTarefa.value;
+    novaLi.innerHTML = inputIdTextoTarefa.value;
+    novaLi.className = 'alteraCorCinza';
     olIdListaTarefa.appendChild(novaLi);
     inputIdTextoTarefa.value = '';
   });
 
   inputIdTextoTarefa.addEventListener('keyup', () => {
     const novaLi = document.createElement('li');
-    novaLi.innerText = inputIdTextoTarefa.value;
+    novaLi.innerHTML = inputIdTextoTarefa.value;
   });
 }
 adicionaTarefas();
+
+function alteraCor() {
+  const liAlteraCorCinza = document.querySelectorAll('p');
+  const liCorCinza = 'rgb(128, 128, 128)';
+  for (let index = 0; index < liAlteraCorCinza.length; index += 1) {
+    liAlteraCorCinza[index].addEventListener('click', () => {
+      if (liAlteraCorCinza[index].style.backgroundColor !== liCorCinza) {
+        liAlteraCorCinza[index].style.backgroundColor = liCorCinza;
+      } else if (liAlteraCorCinza[index].style.backgroundColor === liCorCinza) {
+        liAlteraCorCinza[index].style.backgroundColor = null;
+      }
+    });
+  }
+}
+alteraCor();
