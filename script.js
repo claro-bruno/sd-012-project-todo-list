af7ca6958b14a31ac2b7cf450cec19faac5b96e0
Kevin Oliveira <kevin.zero@hotmail.com>
2021-05-06 21:06:44 -0300

    (14) Corrige funcao de salvar lista

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById('criar-tarefa').addEventListener('click', function () {
-    task = document.createElement('li');
-    text = document.getElementById('texto-tarefa');
+    let task = document.createElement('li');
+    let text = document.getElementById('texto-tarefa');
     task.className = 'taskItem';
     task.innerHTML = text.value;
     document.getElementById('lista-tarefas').appendChild(task);
     text.value = '';
-})
+});
 
 document.body.addEventListener('click', function (event) {
     if (event.target.className === 'taskItem') {
@@ -15,7 +15,7 @@ document.body.addEventListener('click', function (event) {
         }
         event.target.className = 'taskItem selected';
     }
-})
+});
 
 document.getElementById('lista-tarefas').addEventListener('dblclick', function (event) {
     if (event.target.className === 'completed') {
@@ -23,7 +23,7 @@ document.getElementById('lista-tarefas').addEventListener('dblclick', function (
     } else {
         event.target.className = 'completed';
     }
-})
+});
 
 document.getElementById('apaga-tudo').addEventListener('click', function () {
     let taskList = document.getElementById('lista-tarefas');
@@ -32,44 +32,39 @@ document.getElementById('apaga-tudo').addEventListener('click', function () {
     for (let task = 0; task < quantity; task += 1) {
         taskList.removeChild(taskList.firstChild);
     }
-})
+});
 
 document.getElementById('remover-finalizados').addEventListener('click', function () {
     let finished = document.getElementsByClassName('completed');
     for (let task = 0; task < finished.length;) {
         finished[task].remove();
     }
-})
+});
 
 document.getElementById('remover-selecionado').addEventListener('click', function () {
     let selected = document.getElementsByClassName('selected');
     for (let task = 0; task < selected.length;) {
         selected[task].remove();
     }
-})
-
-
-
+});
 
 document.getElementById('salvar-tarefas').addEventListener('click', function () {
-    taskItems = document.getElementsByTagName('li')
+    let taskItems = document.getElementsByTagName('li');
+    let order = []
     for (let item = 0; item < taskItems.length; item += 1) {
-        localStorage.setItem(taskItems[item].innerHTML, taskItems[item].className)
+        localStorage.setItem((taskItems[item].innerText), taskItems[item].className);
+        order.push(taskItems[item].innerText)
     }
-})
+    localStorage.setItem('ordem', JSON.stringify(order))
+});
 
-window.onload = function () {
-    if (localStorage.length > 0) {
-        for (let data = 0; data < localStorage.length; data +=1) {
-            task = document.createElement('li');
-            text = document.getElementById('texto-tarefa');
-            task.innerHTML = localStorage.key(data);
-            task.className = localStorage.getItem(task.innerHTML);
-            document.getElementById('lista-tarefas').appendChild(task);
-        }
+if (localStorage.length > 0) {
+    
+    for (let data = 0; data < localStorage.length - 1; data +=1) {
+        let correctOrder = JSON.parse(localStorage.getItem('ordem'))
+        let task = document.createElement('li');
+        task.innerHTML = correctOrder[data];
+        task.className = localStorage.getItem(correctOrder[data]);
+        document.getElementById('lista-tarefas').appendChild(task);
     }
 }
-
-
-// salvar classe e valor 
-// ao carregar, gerar li de acordo com web storage
