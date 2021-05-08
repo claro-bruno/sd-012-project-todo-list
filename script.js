8ab25cad54c747c2a4369d5367b8e4a527c6284f
Nuwanda <morato_dev@protonmail.com>
2021-05-08 14:32:18 -0300

    desafio 11 feito

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
+// Getting all elements from HTML
 const addToListButton = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const typedTextInput = document.getElementById('texto-tarefa');
 const allItemList = document.getElementsByTagName('li');
 const eraserButton = document.getElementById('apaga-tudo');
+const removeDoneButton = document.getElementById('remover-finalizados');
 
+// Event that gets what is typed on the input and puts on an ordered list.
 addToListButton.addEventListener('click', () => {
   const textAsListItem = document.createElement('li');
   textAsListItem.innerHTML = typedTextInput.value;
   taskList.appendChild(textAsListItem);
+  // Each 'li' that was created gets an event by the time of its creation that changes the bgColor.
   textAsListItem.addEventListener('click', (event) => {
+    const evt = event.target;
     for (let index = 0; index < allItemList.length; index += 1) {
       allItemList[index].style.backgroundColor = '';
     }
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    evt.style.backgroundColor = 'rgb(128, 128, 128)';
   });
 });
 
@@ -33,3 +38,14 @@ completedTask();
 eraserButton.addEventListener('click', () => {
   taskList.innerHTML = '';
 });
+
+function removeAllDone() {
+  removeDoneButton.addEventListener('click', () => {
+    const allDone = document.querySelectorAll('.completed');
+    // Genial use of the forEach idea by: https://stackoverflow.com/questions/44984867/javascript-remove-elements-by-class-name/44984940
+    allDone.forEach((a) => {
+      a.remove();
+    });
+  });
+}
+removeAllDone();
