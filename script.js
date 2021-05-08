c58489338ad0a2d007dda3bd84cbdb9bf3e32f5a
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-05-08 14:15:57 -0300

    ajusta resultado do requisito 9 e adiciona botao requisito 10

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const textoTarefa = document.querySelector('#texto-tarefa');
-let texto = '';
 const criarTarefa = document.getElementById('criar-tarefa');
-const listaTarefa = document.getElementById('lista-tarefas');
+const listaTarefa = document.querySelector('#lista-tarefas');
+let texto = '';
 
 function tarefas() {
   const itemLista = document.createElement('li');
@@ -14,21 +14,25 @@ function tarefas() {
   } else {
     alert('Insira uma tarefa');
   }
+
+  listaTarefa.addEventListener('click', (evento) => {
+    const clica = evento.target;
+    if (clica.className === 'itemDaLista') {
+      const clicado = document.querySelector('.mudandoCor');
+      if (clicado !== null) {
+        clicado.classList.remove('mudandoCor');
+      }
+      clica.classList.add('mudandoCor');
+    }
+  });
 }
 criarTarefa.addEventListener('click', tarefas);
 
-document.addEventListener('click', (evento) => {
-  const clica = evento.target;
-  if (clica.className === 'itemDaLista') {
-    const clicado = document.querySelector('.mudandoCor');
-    if (clicado !== null) {
-      clicado.classList.remove('mudandoCor');
-    }
-    clica.classList.add('mudandoCor');
+//item resolvido após pesquisa no repositorio do Kevin Oliveira Turma-12, consegui entender a lógica da inserção das classes nesse item
+document.body.addEventListener('dblclick', function (event) {
+  if (event.target.className === 'completed') {
+    event.target.className = 'itemDaLista';
+  } else {
+    event.target.className = 'completed';
   }
 });
-
-listaTarefa.addEventListener('dblclick', (evento2) => {
-    const clicado = evento2.target;
-    clicado.classList.add('completed');
-})
