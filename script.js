ddeab4da1ecb637c7d536639f91ceb6d7ddc7c56
Juliana de Assis Fernandes <julidanes@gmail.com>
2021-05-08 01:20:45 -0300

    Resolve o requisito 12

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 /* eslint-disable prefer-arrow-callback */
-let listPlace = document.querySelector('#list-place');
-let orderedList = document.querySelector('#lista-tarefas');
-
-addBackgroundColor();
-completed();
-
+const listPlace = document.querySelector('#list-place');
+const orderedList = document.querySelector('#lista-tarefas');
 
 function makeList() {
-  let li = document.createElement('li');
-  let userTask = document.getElementById('texto-tarefa').value;
-  let listItem = document.createTextNode(userTask);
+  const li = document.createElement('li');
+  const userTask = document.getElementById('texto-tarefa').value;
+  const listItem = document.createTextNode(userTask);
   li.appendChild(listItem);
   if (userTask === '') {
     alert('Escreva uma tarefa!');
@@ -20,10 +16,10 @@ function makeList() {
 }
 
 function addBackgroundColor() {
-  listPlace.addEventListener('click', function(event) {
+  listPlace.addEventListener('click', function (event) {
     const selected = event.target;
     selected.style.backgroundColor = 'rgb(128, 128, 128)';
-    listPlace.addEventListener('click', function(event) {
+    listPlace.addEventListener('click', function (event) {
       const selected2 = event.target;
       selected2.style.backgroundColor = 'rgb(128, 128, 128)';
       selected.style.backgroundColor = 'white';
@@ -31,14 +27,14 @@ function addBackgroundColor() {
   });
 }
 function completed() {
-  listPlace.addEventListener('dblclick', function(event) {
+  listPlace.addEventListener('dblclick', function (event) {
     const itemCompleted = event.target;
     itemCompleted.classList.toggle('completed');
   });
 }
 
-function deleteAll(){
-  orderedList.innerHTML = "";
+function deleteAll() {
+  orderedList.innerHTML = '';
 }
 
 function deleteCompleted() {
@@ -46,14 +42,18 @@ function deleteCompleted() {
   while (completedItens[0]) {
     completedItens[0].parentNode.removeChild(completedItens[0]);
   }
-}  
+}
 
+function saveUserList() {
+  localStorage.setItem('userList', orderedList.innerHTML);
+}
 
+window.onload = function () {
+  const savedList = localStorage.getItem('userList');
+  if (savedList !== null) {
+    orderedList.innerHTML = savedList;
+  }
+};
 
-
-
-
-
-
-
-
+addBackgroundColor();
+completed();
