fcc8072c211bb1c8b9f73509a8aba5ad17439151
ArturAlvaro <artur.alvaro73@gmail.com>
2021-05-08 17:52:24 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ const toDoList = document.getElementById('lista-tarefas');
 let input = document.getElementById('texto-tarefa');
 
 
+
+
 function addTarefa() {
     let toDoListItems = document.createElement('li');
+    toDoListItems.className = 'tasks';
     toDoListItems.innerHTML = input.value;
     input.value = '';
     toDoList.appendChild(toDoListItems);
+    if (toDoListItems !== null) {
+        toDoListItems.addEventListener('click', function(event) {
+            event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+        })
+    }
 }
 
 button.addEventListener('click', addTarefa);
