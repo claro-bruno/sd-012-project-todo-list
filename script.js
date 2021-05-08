a9d68bae92f97623f3f7e206eea924ffa8de0636
Waltton Coelho <60485084+walttoncoelho@users.noreply.github.com>
2021-05-08 18:32:37 -0300

    Seleciona alista ordenada

--- script.js
+++ script.js
@@ -22,3 +22,28 @@ finish.addEventListener('dblclick', (evento) => {
   }
 });
 // <==
+
+
+// ==> Seleciona a lista ordenada
+
+/*
+const clikColor = document.getElementById('li');
+clikColor.addEventListener('click', clicar)
+
+function clicar()
+{
+  clikColor.style.backgroundColor = 'red';
+
+}
+*/
+
+taskList.addEventListener('click', clikColor);
+function clikColor(event) {
+  const clikCollor = document.querySelector('.selected');
+  if (clikCollor !== null) {
+    clikCollor.classList.remove('selected')
+  } else {
+    event.target.classList.add('selected');
+  }
+};
+
