317e10001813cf3abfeb5f048e7da76634435f82
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-05-07 23:47:41 -0300

    ajuste do requisito 7, realiza a mudança de cor

--- script.js
+++ script.js
@@ -4,25 +4,26 @@ const criarTarefa = document.getElementById('criar-tarefa');
 const listaTarefa = document.getElementById('lista-tarefas');
 
 function tarefas() {
-    const itemLista = document.createElement('li');
-    itemLista.classList.add('itemDaLista');
-    texto = textoTarefa.value;
-    itemLista.innerHTML = texto;
-    if (texto != '') {
-        listaTarefa.appendChild(itemLista);
-        textoTarefa.value = '';
-    } else {
-        alert('Insira uma tarefa');
-    }
+  const itemLista = document.createElement('li');
+  itemLista.classList.add('itemDaLista');
+  texto = textoTarefa.value;
+  itemLista.innerHTML = texto;
+  if (texto !== '') {
+    listaTarefa.appendChild(itemLista);
+    textoTarefa.value = '';
+  } else {
+    alert('Insira uma tarefa');
+  }
 }
 criarTarefa.addEventListener('click', tarefas);
 
-const li = document.querySelector('#lista-tarefas');
-const item = document.getElementsByTagName('li')
-function mudaCor (evento) {
-    for(let itemX; itemX < item.length; itemX += 1) {
-        item[itemX].classList.remove('mudandoCor');
-    } 
-    evento.target.classList.add('mudandoCor')
-}
-li.addEventListener('click', mudaCor);
+document.addEventListener('click', (evento) => {
+  const clica = evento.target;
+  if (clica.className === 'itemDaLista') {
+    const clicado = document.querySelector('.mudandoCor');
+    if (clicado !== null) {
+      clicado.classList.remove('mudandoCor');
+    }
+    clica.classList.add('mudandoCor');
+  }
+})
