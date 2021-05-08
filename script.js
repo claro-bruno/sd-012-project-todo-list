4056fb41855ec1724d72e4fd56e17470e89fadc2
Elias Forte <eliasforte.dev@gmail.com>
2021-05-08 15:03:51 -0300

    funçao de escolher somente um item

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ const textInput = document.querySelector('#texto-tarefa');
 const buttonCreate = document.querySelector('.submit');
 
 buttonCreate.addEventListener('click', function () {
-  let catOl = document.getElementById('lista-tarefas');
-  const text = textInput.value;
-  const createLi = document.createElement('li');
-  createLi.innerHTML = text;
-  catOl.appendChild(createLi);
-  document.getElementById('texto-tarefa').value = null;
+  if (textInput.value === ''){
+    alert('Adicione alguma tarefa!')
+  } else {
+    let catOl = document.getElementById('lista-tarefas');
+    const text = textInput.value;
+    const createLi = document.createElement('li');
+    createLi.className = 'task';
+    createLi.innerHTML = text;
+    catOl.appendChild(createLi);
+    document.getElementById('texto-tarefa').value = null;
+  }
 });
 
 
@@ -19,3 +24,13 @@ let removeAll = document.getElementById('lista-tarefas');
 buttonRemove.addEventListener('click', function() {
     removeAll.innerHTML = '';
 })
+
+let selecteIten = document.getElementById('lista-tarefas');
+
+selecteIten.addEventListener('click', function (event) {
+  let itenColor = document.getElementsByTagName('li');
+  for (let key in itenColor){
+    itenColor[key].className = 'task';
+    event.target.className = 'task-selected';
+  }
+});
