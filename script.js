4221a8d04dea3cb11c6c1cac820664e7cbdbe47e
Marcela Silva <marcela.ops@hotmail.br>
2021-05-07 21:28:39 -0300

    r5 começando

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-window.onload = function() {
+// window.onload = function() {
+
+//     let addTask = document.getElementById('criar-tarefa');
+//     function addTarefa() {
+//         const textoTarefa = document.getElementById('texto-tarefa');
+//         if (textoTarefa.value === '') {
+//             alert ('Insira uma nova tarefa.');
+//         } else {
+//             const listaTarefa = document.getElementById('lista-tarefa');
+//             const tarefa = document.createElement('li');
+//             tarefa.innerHTML = textoTarefa.value;
+//             listaTarefa.appendChild(tarefa);
+//             textoTarefa.value = '';
+//         }
+//     }
+//    addTarefa.addEventListener('click', addTarefa);
 
 
-
-    
-}
+// }
