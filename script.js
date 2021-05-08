a2aaff7fbc75bc6afb3dc190c2ce9b167f449c6e
Elias Forte <eliasforte.dev@gmail.com>
2021-05-07 21:30:07 -0300

    adicionando botao de remover

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const toDoList = [''];
 
-let textInput = document.querySelector('#texto-tarefa');
+const textInput = document.querySelector('#texto-tarefa');
 
 const buttonCreate = document.querySelector('.submit');
 
-buttonCreate.addEventListener('click',function () {
-    const catOl = document.getElementById('lista-tarefas');
-    const text = textInput.value;
-    const createLi = document.createElement('li');
-    createLi.className = 'unselected';
-    createLi.innerHTML = text;
-    catOl.appendChild(createLi);
-    document.getElementById('texto-tarefa').value = null;
+buttonCreate.addEventListener('click', function () {
+  let catOl = document.getElementById('lista-tarefas');
+  const text = textInput.value;
+  const createLi = document.createElement('li');
+  createLi.className = 'unselected';
+  createLi.innerHTML = text;
+  catOl.appendChild(createLi);
+  document.getElementById('texto-tarefa').value = null;
 });
+
+
+let buttonRemove = document.getElementById('apaga-tudo')
+let removeAll = document.getElementById('lista-tarefas');
+buttonRemove.addEventListener('click', function() {
+    removeAll.innerHTML = '';
+})
