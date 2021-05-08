1c0e95f28d6c9b70d54ecd8bf3324b2d918edb89
Julio Pereira <jandrade.pereira1@gmail.com>
2021-05-07 22:56:57 -0300

    Insere as respostas dos resquisitos 7 e 8

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ const taskList = document.getElementById('lista-tarefas');
 
 function createNewListItem() {
   const listItem = document.createElement('li');
+  // eslint-disable-next-line sonarjs/no-duplicate-string
   listItem.className = 'item-tarefa';
   listItem.innerHTML = inputContent.value;
   taskList.appendChild(listItem);
   inputContent.value = '';
 }
+
 addTask.addEventListener('click', createNewListItem);
+
+function selectedItem(event) {
+  if (event.target.className === 'item-tarefa') {
+    const selected = document.getElementsByClassName('seleciona');
+    for (let index = 0; index < selected.length; index += 1) {
+      selected[index].className = 'item-tarefa';
+    }
+    // eslint-disable-next-line no-param-reassign
+    event.target.className = 'item-tarefa seleciona';
+  }
+}
+
+taskList.addEventListener('click', selectedItem);
