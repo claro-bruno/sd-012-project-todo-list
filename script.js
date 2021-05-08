1685f5ecdd5953af800366dc203b2f5247d31b02
Waltton Coelho <60485084+walttoncoelho@users.noreply.github.com>
2021-05-08 15:50:18 -0300

    Risca a lista ordenada

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const input = document.querySelector('#texto-tarefa');
 const button = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 
-
+// ==> Gera a lista ordenada
 button.addEventListener("click", function () {
   const createItem = document.createElement('li');
   createItem.className = 'task-item';
@@ -10,3 +10,15 @@ button.addEventListener("click", function () {
   taskList.appendChild(createItem);
   input.value = '';
 });
+// <==
+
+// ==> Risca a lista ordenada
+const finish = document.querySelector('#lista-tarefas');
+finish.addEventListener('dblclick', (evento) => {
+  if (evento.target.classList.contains('completed')) {
+    evento.target.classList.remove('completed')
+  } else {
+    evento.target.classList.add('completed');
+  }
+});
+// <==
