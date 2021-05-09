9c316e3303616d03f59efd99714f61d6f8355f2e
cristianocsl <cslcristiano@gmail.com>
2021-05-09 16:29:32 -0300

    organizando o código

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// Requisitos 5 e 6
+const getTaskListLo = document.getElementById('lista-tarefas');
+const getTextInput = document.getElementById('texto-tarefa');
+const clickButton = document.getElementById('criar-tarefa');
+
+function inputNewTask() {
+  clickButton.addEventListener ('click', function eventosParaLi() {
+    const newLi = document.createElement('li');
+    newLi.innerText = getTextInput.value;
+    newLi.addEventListener('click', clickNoLi); // evento de clique no li //
+    newLi.addEventListener('dblclick', clickdoble); // Requisito 8
+    newLi.classList.add('li-estilo');
+    getTaskListLo.appendChild(newLi);
+    getTextInput.value = '';
+  });
+}
+inputNewTask();
+
+function eventEraseButton() {
+  const clickButtonErase = document.getElementById('apaga-tudo'); // requisito 10
+
+  clickButtonErase.addEventListener ('click', function eraseButton(event) { // requisito 10
+    const listItems = document.querySelectorAll('.li-estilo');
+    for (let index = 0; index < listItems.length; index += 1) {
+      const element = listItems[index];
+      getTaskListLo.removeChild(element);
+    }
+  });
+}
+eventEraseButton();
+
 // Requisito 7 e 8 - Clicar em um item da lista deve alterar
 // a cor de fundo do item para cinza rgb(128,128,128)
 // Requisitos 7 e 8: necessário para o Requisito 5, logo deve estar antes dele.
@@ -16,31 +47,4 @@ function clickdoble(event) {
     event.target.classList.add('completed'); // se não houver a classe específica (.completed), adicione esta classe específica.
   }
 }
-// Requisitos 5 e 6
-function inputNewTask() {
-  const getTextInput = document.getElementById('texto-tarefa');
-  const clickButton = document.getElementById('criar-tarefa');
-  const getTaskListLo = document.getElementById('lista-tarefas');
-  const clickButtonErase = document.getElementById('apaga-tudo'); // requisito 10
-
-  clickButtonErase.addEventListener ('click', eraseButton);
-  clickButton.addEventListener ('click', eventosParaLi);
-}
-function eraseButton(event) { // requisito 10
-  const listItems = document.querySelectorAll('.li-estilo');
-  for (let index = 0; index < listItems.length; index += 1) {
-    const element = listItems[index];
-    getTaskListLo.removeChild(element);
-  }
-}
-function eventosParaLi() {
-  const newLi = document.createElement('li');
-  newLi.innerText = getTextInput.value;
-  newLi.addEventListener('click', clickNoLi); // evento de clique no li //
-  newLi.addEventListener('dblclick', clickdoble); // Requisito 8
-  newLi.classList.add('li-estilo');
-  getTaskListLo.appendChild(newLi);
-  getTextInput.value = '';
-}
-inputNewTask();
-
+// Requisito 10: botão que apaga tudo
