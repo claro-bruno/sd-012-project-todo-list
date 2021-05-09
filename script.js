046ed0c430700f1e9df7c47ce8e654b9a3560959
Waltton Coelho <60485084+walttoncoelho@users.noreply.github.com>
2021-05-09 15:40:57 -0300

    Refazendo deletar selecionados

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const button = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 
 // ==> Gera a lista ordenada
-button.addEventListener("click", function () {
+button.addEventListener("click", function ()  {
   const createItem = document.createElement('li');
   createItem.className = 'task-item';
   createItem.innerText = input.value;
@@ -14,35 +14,59 @@ button.addEventListener("click", function () {
 
 // ==> Risca a lista ordenada
 const finish = document.querySelector('#lista-tarefas');
-finish.addEventListener('dblclick', (evento) => {
-  if (evento.target.classList.contains('completed')) {
-    evento.target.classList.remove('completed')
+finish.addEventListener('dblclick', (event) => {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed')
   } else {
-    evento.target.classList.add('completed');
+    event.target.classList.add('completed');
   }
 });
 // <==
 
-
 // ==> Seleciona a lista ordenada
 
-
-taskList.addEventListener('click', clikColor);
-
 function clikColor(event) {
-  const clikCollor = document.querySelector('.selected');
-  if (clikCollor !== null) {
-    clikCollor.classList.remove('selected');
+  const liTaskItem = document.querySelector('.selected');
+  if (liTaskItem !== null) {
+    liTaskItem.classList.remove('selected');
   } else {
     event.target.classList.add('selected');
   }
-};
+}
+taskList.addEventListener('click', clikColor);
+
 // ==> Apaga tudo!!!
 
 const deleteAll = document.querySelector('#apaga-tudo');
-
-deleteAll.addEventListener('click', function () {
+deleteAll.addEventListener('click', function ()  {
   let itensLista = document.querySelector('#lista-tarefas');
   itensLista.innerText = '';
 });
 
+
+
+/* última atualização
+    const deleteSelect = document.querySelector('#remover-finalizados');
+
+    deleteSelect.addEventListener('click', function () {
+      const deletarLista = document.querySelector('.task-item');
+      if (deletarLista == 'selected') {
+        taskList.removeChild(selected);
+        errorMessage.innerText = '';
+      } else {
+        errorMessage.innerText = 'Nenhuma tarefa selecionada';
+      }
+    });
+*/
+
+
+const deleteSelect = document.querySelector('#remover-finalizados');
+deleteSelect.addEventListener('click', function ()  {
+  let deletar = document.querySelectorAll('.task-item');
+
+  for (let i = 0; i < deletar.length; i += 1) {
+    if (deletar[i].classList.contains('completed')) {
+      deletar[i].remove();
+    }
+  }
+});
