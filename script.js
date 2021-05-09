1517061a8e4e395c3380f392268a7d2fd11eae1a
Janderson Soares <jandinsm@gmail.com>
2021-05-09 18:47:36 -0300

    Desafio 2, add p discreto

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const paiBody = document.getElementsByTagName("body")[0];
 
 
+
+
 function addTitle(){
     let title = document.createElement("header");
-    title.innerHTML= "Minha Lista de Tarefas";
+    title.innerHTML = "Minha Lista de Tarefas";
     paiBody.appendChild(title);
 }
 addTitle();
+
+function addDiscretP(){
+    let p = document.createElement("p");
+    p.innerHTML = "Clique duas vezes em um item para marcá-lo como completo";
+    p.id = "funcionamento";
+    paiBody.appendChild(p);
+}
