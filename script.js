f610ea290e95a2ee6c16ae68ef1a37c9b876a862
rachidb5 <jordan.rachid@gmail.com>
2021-05-09 20:21:13 -0300

    Adiciona funçao done conforme requisito com desfazer o done

--- script.js
+++ script.js
@@ -1,37 +1,28 @@
 const input = document.getElementById('texto-tarefa');
 const listaTarefa = document.getElementById("lista-tarefas");
 
+function grey(event) {
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+}
+function done(event) {
+    if(event.target.classList.contains('completed')){
+    event.target.classList.remove('completed')
+    event.target.style.textDecoration = 'none'
+    } else {
+    event.target.classList.add('completed')
+    event.target.style.textDecoration = 'line-through'
+    }
+}
+
 function createTask() {
     const tarefa = document.createElement("li");
-    tarefa.className = 'tarefa';
+    tarefa.classList.add('tarefa');
     tarefa.innerHTML = input.value;
     listaTarefa.appendChild(tarefa);
     console.log(input.value)
     input.value =''
-
-    function grey(event) {
-        event.target.style.backgroundColor = 'rgb(128,128,128)';
-    }
-    function done(event) {
-        if(event.target.style.textDecoration == "line-through solid rgb(0, 0, 0)"){
-        event.target.style.textDecoration = "none";
-        event.target.classList.remove('completed')
-        } else {
-        event.target.style.textDecoration = "line-through solid rgb(0, 0, 0)";
-        event.target.classList.add('completed')
-        }
-    }
-
-    const task = document.querySelectorAll(".tarefa")
-
-    for (let i = 0; i < task.length; i += 1) {
-        task[i].addEventListener('click', grey);
-      }
-
-    for (let i = 0; i < task.length; i += 1) {
-        task[i].addEventListener('dblclick', done);
-      }
-    
+    tarefa.addEventListener('click', grey);
+    tarefa.addEventListener('dblclick', done);
 }
 
 document.getElementById("criar-tarefa").addEventListener('click', createTask);
