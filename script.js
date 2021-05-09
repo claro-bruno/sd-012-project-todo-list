65d1c98f7cfd363e5031cc20f340fc73cbe0d15b
rachidb5 <jordan.rachid@gmail.com>
2021-05-08 22:55:46 -0300

    Adiciona funçao que muda o background da tarefa quando clicada

--- script.js
+++ script.js
@@ -8,6 +8,29 @@ function createTask() {
     listaTarefa.appendChild(tarefa);
     console.log(input.value)
     input.value =''
+
+    function done(event) {
+        event.target.style.backgroundColor = 'rgb(128,128,128)';
+        event.target.style.textDecoration = "line-through";
+    }
+
+    const task = document.querySelectorAll(".tarefa")
+
+    for (let i = 0; i < task.length; i += 1) {
+        task[i].addEventListener('click', done);
+      }
+    
 }
 
 document.getElementById("criar-tarefa").addEventListener('click', createTask);
+
+/*function done(event) {
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+    event.target.style.textDecoration = "line-through";
+}
+
+const tarefa = document.querySelectorAll(".tarefa")
+
+for (let i = 0; i < tarefa.length; i += 1) {
+    tarefa[i].addEventListener('click', done);
+  }*/
