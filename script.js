b007204434b5615fad01e9dc76172b01378470e7
Luiz Henrique <luishenriqueee2010@hotmail.com>
2021-05-09 19:42:02 -0300

    Tenta corrigir exercicio 7

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const input = document.querySelector('#texto-tarefa');
 const list = document.querySelector('#lista-tarefas');
 // Adiciona tarefas
 function makeJob() {
-  const list = document.querySelector('#lista-tarefas');
   const newJob = document.createElement('li');
   newJob.innerHTML = input.value;
+  newJob.className = 'selected';
   list.appendChild(newJob);
   input.value = '';
 }
@@ -14,13 +14,11 @@ button.addEventListener('click', makeJob);
 // Adiciona cor a tarefa selecionada
 function addColor(event) {
   const classElement = document.querySelector('.selected-item');
+  if (classElement !== null) {
+    classElement.classList.remove('selected-item');
+  }
   event.target.classList.add('selected-item');
-  classElement.classList.remove('selected-item');
+  console.log(classElement);
 }
 
-function addSelected(event) {
-  const selectedElement = document.querySelector('.selected');
-  selectedElement.classList.remove('selected');
-  event.target.classList.add('selected');
-}
 list.addEventListener('click', addColor);
