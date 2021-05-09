e4b2168baddf178498a3a7318409733a75706cac
rachidb5 <jordan.rachid@gmail.com>
2021-05-09 00:54:57 -0300

    Adiciona funçao done conforme requisito com desfazer o done

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const listaTarefa = document.getElementById("lista-tarefas");
 
 function createTask() {
     const tarefa = document.createElement("li");
-    //tarefa.className = 'tarefa';
+    tarefa.className = 'tarefa';
     tarefa.innerHTML = input.value;
     listaTarefa.appendChild(tarefa);
     console.log(input.value)
@@ -13,16 +13,16 @@ function createTask() {
         event.target.style.backgroundColor = 'rgb(128,128,128)';
     }
     function done(event) {
-        if(event.target.style.textDecoration =="line-through solid rgb(0, 0, 0)"){
+        if(event.target.style.textDecoration == "line-through solid rgb(0, 0, 0)"){
         event.target.style.textDecoration = "none";
-        event.target.classList.remove('completed')
+        //event.target.classList.remove('completed')
         } else {
         event.target.style.textDecoration = "line-through solid rgb(0, 0, 0)";
         event.target.classList.add('completed')
         }
     }
 
-    const task = document.querySelectorAll("li")
+    const task = document.querySelectorAll(".tarefa")
 
     for (let i = 0; i < task.length; i += 1) {
         task[i].addEventListener('click', grey);
