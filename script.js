470777df24f2db00a78f002a2fa3421ce862cc7c
lucasnahas <lucasahas@gmail.com>
2021-05-09 18:31:42 -0300

    adiciona funcao de subir e descer tarefa

--- script.js
+++ script.js
@@ -14,11 +14,12 @@ let btnSaveList      = document.getElementById('btn-save-list');
 // EVENT LISTENERS
 btnAdicionar.    addEventListener('click', addTask);
 btnDelete.       addEventListener('click', deleteTask);
-// btnUp.           addEventListener('click', upTask);
-// btnDown.         addEventListener('click', downTask);
-// btnClearComplete.addEventListener('click', clearCompleteTasks);
-// btnClearAll.     addEventListener('click', clearAllTasks);
+btnUp.           addEventListener('click', upTask);
+btnDown.         addEventListener('click', downTask);
+btnClearComplete.addEventListener('click', clearCompleteTasks);
+btnClearAll.     addEventListener('click', clearAllTasks);
 // btnSaveList.     addEventListener('click', saveList);
+// document.body.   addEventListener('click', removeClass);
 
 
 
@@ -32,17 +33,17 @@ function addTask(){
 }
 
 function selectTask(event){
-  if (event.target.classList.contains('selected')){
-    removeClass();
-  }else{
-    removeClass();
-    event.target.classList.add('selected');
-  }
+  // if (event.target.classList.contains('selected')){
+  //   removeClass();
+  // }else{
+  // }
+  removeClass();
+  event.target.classList.add('selected');
 }
 
 function removeClass(){
   for (let i = 0; i < document.querySelectorAll('.selected').length; i++){
-    document.querySelectorAll('.selected')[i].classList.remove('selected')
+    document.querySelectorAll('.selected')[i].classList.remove('selected');
   }
 }
   
@@ -58,6 +59,42 @@ function deleteTask(){
   if (document.getElementsByClassName('selected').length > 0){
     document.getElementsByClassName('selected')[0].remove();
   }
+}
+
+function clearCompleteTasks(){
+  let list = document.getElementsByClassName('done');
+  let len  = list.length;
+  for (let i = len-1; i >=0; i--){
+    list[i].remove();
+  }
+}
+
+function clearAllTasks(){
+  list.innerHTML = '';
+}
+
+function downTask(){
+  try{
+    let task = document.getElementsByClassName('selected')[0];
+    let nextTask = task.nextElementSibling;
+    let temp = nextTask.innerHTML;
+    nextTask.innerHTML = task.innerHTML;
+    task.innerHTML = temp;
+    removeClass();
+    nextTask.classList.add('selected');
+  }catch(e){}
+}
+
+function upTask(){
+  try{
+    let task = document.getElementsByClassName('selected')[0];
+    let previousTask = task.previousElementSibling;
+    let temp = previousTask.innerHTML;
+    previousTask.innerHTML = task.innerHTML;
+    task.innerHTML = temp;
+    removeClass();
+    previousTask.classList.add('selected');
+  }catch(e){}
 }
 
 
