6774c05595ec1840563d15a6480521e32256c9c5
José Carlos <kalarruda@hotmail.com>
2021-05-09 12:44:52 -0300

    11-adiciona botao e apaga tarefas completadas (finalizadas)

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ const btnCriaTarefa = document.getElementById('criar-tarefa');
 btnCriaTarefa.innerHTML = 'Adiciona Tarefa';
 const inputField = document.getElementById('texto-tarefa');
 const ordList = document.getElementById('lista-tarefas');
-let lista2 = document.createElement('li');
 
-btnCriaTarefa.addEventListener('click', function () {
-  let lista = document.createElement('li');
+btnCriaTarefa.addEventListener('click', () => {
+  const lista = document.createElement('li');
   lista.innerHTML = inputField.value;
   ordList.appendChild(lista);
   inputField.value = '';
@@ -16,14 +15,12 @@ function changeColor(event) {
   if (document.querySelector('.corCinza') === null) {
     event.target.classList.add('corCinza');
     document.querySelector('.corCinza').style.backgroundColor = 'rgb(128, 128, 128)';
-    
   }
   else {
     document.querySelector('.corCinza').style.backgroundColor = 'white';
     document.querySelector('.corCinza').classList.remove('corCinza');
     event.target.classList.add('corCinza');
-    document.querySelector('.corCinza').style.backgroundColor = 'rgb(128, 128, 128)';
-    
+    document.querySelector('.corCinza').style.backgroundColor = 'rgb(128, 128, 128)';    
   }
 }
 
@@ -31,23 +28,34 @@ ordList.addEventListener('click', changeColor);
 
 function taskEnd(event) {
   if (event.target.classList.contains('completed') === false) {
-      event.target.classList.add('completed');
+    event.target.classList.add('completed');
   } else {
-    event.target.classList.remove('completed');     
+    event.target.classList.remove('completed');
   }
 }
 ordList.addEventListener('dblclick', taskEnd);
 
 const btnApaga = document.querySelector('#apaga-tudo');
-btnApaga.innerHTML = 'apagar';
+btnApaga.innerHTML = 'Apagar todos';
 
 function apagaTudo() {
-  let lista = document.querySelectorAll('li');
+  const lista = document.querySelectorAll('li');
   for (let index = 0; index <= lista.length; index += 1) {
-    let listaFilhos = ordList.childNodes[index];
-    btnApaga.addEventListener('click', function () {
-      console.log(lista.length);
+    const listaFilhos = ordList.childNodes[index];
+    btnApaga.addEventListener('click', () => {
       listaFilhos.remove();
-    })  
+    });
   }
 }
+
+const btnApagaFinalizados = document.querySelector('#remover-finalizados');
+btnApagaFinalizados.innerHTML = 'Apagar Selecionados';
+
+function apagaFinalizados() {
+  const marcadosCinza = document.querySelectorAll('.completed');
+  for (index = 0; index < marcadosCinza.length; index += 1) {
+    ordList.removeChild(marcadosCinza[index]);
+  }
+}
+
+btnApagaFinalizados.addEventListener('click', apagaFinalizados);
