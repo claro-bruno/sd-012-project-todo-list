2a9ef8ff71546638c99a2e15d243d7f11342b2e6
Jossany Moura <mjossany@gmail.com>
2021-05-10 11:33:35 -0300

    Arruma lint

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Após estudar o repositório dos meus colegas de trybe Gabriel Bueno, Luciano Almeida e David Gonzaga decidi reescrever meu projeto do 0 tentando criar um código mais limpo e organizado.
+//  Após estudar o repositório dos meus colegas de trybe Gabriel Bueno, Luciano Almeida e David Gonzaga decidi reescrever meu projeto do 0 tentando criar um código mais limpo e organizado.
 
 const catchAddButton = document.getElementById('criar-tarefa');
 const catchInput = document.getElementById('texto-tarefa');
@@ -11,24 +11,24 @@ const catchUpButton = document.getElementById('mover-cima');
 const catchDownButton = document.getElementById('mover-baixo');
 const catchRemoveSelectedButton = document.getElementById('remover-selecionado');
 
-function addTask () {
+function addTask() {
   if (catchInput.value.length === 0) {
     alert('Adicione uma tarefa');
   } else {
     const newLi = document.createElement('li');
     newLi.innerHTML = catchInput.value;
-    newLi.classList.add('task')
+    newLi.classList.add('task');
     catchOl.appendChild(newLi);
   }
   catchInput.value = '';
 }
 
 catchAddButton.addEventListener('click', addTask);
-catchInput.addEventListener('keypress', function(e) {
+catchInput.addEventListener('keypress', (e) => {
   if (e.key === 'Enter') {
     addTask();
   }
-})
+});
 
 function addSelected(event) {
   for (let index = 0; index < catchTasks.length; index += 1) {
@@ -55,7 +55,6 @@ function apagarTudo() {
 
 catchApagaTudoButton.addEventListener('click', apagarTudo);
 
-
 function apagarFinalizados() {
   const catchCompleted = document.querySelectorAll('.completed');
   for (let index = 0; index < catchCompleted.length; index += 1) {
@@ -71,16 +70,15 @@ function salvarTarefas() {
 
 catchSalvarButton.addEventListener('click', salvarTarefas);
 
-window.onload = function() {
+window.onload = function load() {
   const saved = localStorage.getItem('Lista de tarefas');
   catchOl.innerHTML = saved;
-}
+};
 
 function capturaSelected() {
   const taskSelected = document.querySelector('.selected');
   return taskSelected;
 }
-
 
 function moverParaCima() {
   if (capturaSelected() === null) {
@@ -91,7 +89,6 @@ function moverParaCima() {
 }
 
 catchUpButton.addEventListener('click', moverParaCima);
-
 
 function moverParaBaixo() {
   if (capturaSelected() === null) {
@@ -109,5 +106,3 @@ function removeSelecionado() {
 }
 
 catchRemoveSelectedButton.addEventListener('click', removeSelecionado);
-
-
