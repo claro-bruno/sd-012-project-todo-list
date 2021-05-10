d3b0a33dbb82a255e8bbe63d6b6d059f4aa43640
Janderson Soares <jandinsm@gmail.com>
2021-05-09 21:20:20 -0300

    Desafio 5, add botao de input

--- script.js
+++ script.js
@@ -31,3 +31,30 @@ function listOrdened(){
     paiBody.appendChild(list);
 }
 listOrdened();
+
+function buttonCriarTarefea(){
+    let buttone = document.createElement("button");
+    buttone.id = "criar-tarefa";
+    paiBody.appendChild(buttone);
+    buttone.innerHTML = "Criar-Tarefa";
+
+    buttone.addEventListener("click", function createTarefa(event){
+        let selecOl = document.getElementById("lista-tarefas");
+        let criarli = document.createElement("li")
+        selecOl.appendChild(criarli);
+        let boxTexto =  document.getElementsByTagName("input")[0];
+        criarli.innerHTML = boxTexto.value;
+        boxTexto.value = "";   
+    })
+
+    buttone.addEventListener("mouseenter", function(event){
+        event.target.style.backgroundColor = "orange";
+    });
+    buttone.addEventListener("mouseout", function (event){
+        event.target.style.backgroundColor = "";
+    })
+}
+buttonCriarTarefea();
+
+
+
