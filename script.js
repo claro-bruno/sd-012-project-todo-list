d44154e95458c76e41602c1b73e6574267d49f00
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-09 21:05:36 -0300

    Adiciona requisito 7

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ criarTarefa.addEventListener('click', adicionaTarefa)
 function adicionaTarefa() {
   const novaTarefa = document.createElement('li');
   novaTarefa.innerHTML = document.querySelector('#texto-tarefa').value;
+  novaTarefa.addEventListener('click', changeColor);
   document.querySelector('#lista-tarefas').appendChild(novaTarefa);
+}
+
+function changeColor(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)'
 }
