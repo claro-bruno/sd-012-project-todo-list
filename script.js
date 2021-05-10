dbb1f05def5d4097fe3795fcb04ad5674d94d66f
Tiago Moreira <tiago.joke@yahoo.com.br>
2021-05-09 21:36:09 -0300

    Adiciona requisito 9

--- script.js
+++ script.js
@@ -5,13 +5,23 @@ function adicionaTarefa() {
   const novaTarefa = document.createElement('li');
   novaTarefa.innerHTML = document.querySelector('#texto-tarefa').value;
   novaTarefa.addEventListener('click', changeColor);
+  novaTarefa.addEventListener('dblclick', completedTask);
   document.querySelector('#lista-tarefas').appendChild(novaTarefa);
 }
 
 function changeColor(event) {
   const getLiGray = document.querySelector('.gray');
-  if(getLiGray !== null){
+  if (getLiGray !== null) {
     getLiGray.classList.remove('gray');
   }
   event.target.classList.add('gray');
+}
+
+function completedTask(event) {
+
+  if (event.target.className.includes('completed')){
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
 }
