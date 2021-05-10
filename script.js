066cae4d16d22d428c13aa7de41fe6f466b13a51
Marcela Silva <marcela.ops@hotmail.br>
2021-05-10 13:17:07 -0300

    exerc 5 e 6

--- script.js
+++ script.js
@@ -1,46 +1,27 @@
 window.onload = function() {
     //Tentando entender esse vídeo https://www.youtube.com/watch?v=Ttf3CEsEwMQ p ver se consigo aplicar algo aqui.
 
-   
+   //5 e 6
     const inputTask = document.querySelector('#texto-tarefa');
     const creatTaskButton = document.querySelector('#criar-tarefa');
     const taskList = document.querySelector('#lista-tarefas');
 
+    
+    function addTask() {
+        if (inputTask.value === '') {     //peguei a ideia de fazer if/else em um plantão
+            alert ('Escreva uma tarefa.');
+        } else {
+            const newTask = document.createElement('li');
+            newTask.className = 'tarefa';
+            newTask.innerHTML =  inputTask.value;
+            taskList.appendChild(newTask);
+            inputTask.value = '';
+        }
+    }
     creatTaskButton.addEventListener('click', addTask);
 
+    //7
     
-    function addTask(event) {
-        const newTask = document.createElement('li');
-        newTask.className = 'tarefa';
-        newTask.innerHTML =  inputTask.value;
-        taskList.appendChild(newTask);
-    }
-
-
-    // let taskButton = document.querySelector('#task-button');
-
-    // function addTask(inputTask) {
-    //     // let task = document.querySelector('#task-text');
-    //     if (inputTask.value === '') {
-    //         alert ('Insira uma nova tarefa.');
-    //     } else {
-    //         const olTaskList = document.querySelector('#task-list');
-    //         const task = document.createElement('li');
-    //         task.innerHTML = inputTask;
-    //         olTaskList.appendChild(task);
-    //         inputTask.value = '';
-    //     }
-
-    //     let taskButton = document.querySelector('#task-button');
-        
-    //     taskButton.addEventListener('click', function(event){
-    //         if(event.target.id === 'task-button') {
-                
-    //         }
-    //     })
-    // }
- 
-
    
 
 
