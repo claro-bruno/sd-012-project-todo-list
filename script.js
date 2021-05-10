5bd2b652ec802e14a0f6e9ccb9794a32360db69f
Guilherme Moreno <guilherme.lmoreno23@gmail.com>
2021-05-10 15:40:31 -0300

    Terminando exercise 7

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-let list = document.querySelector("#list")
+let list = document.querySelector("#list")   
 
 
 function createList(event){
-    event.preventDefault();
-     let criaList = document.createElement("li")
-     criaList.id = "listas"   
+  let criaList = document.createElement("li")
+  criaList.id = "listas" 
+    event.preventDefault();  
      document.querySelector("#lista-tarefas").appendChild(criaList)
      let valor = document.getElementById("texto-tarefa")
      criaList.innerHTML = valor.value
      valor.value = "";
-    
+     let changeColor = criaList
+     changeColor.addEventListener("click",change)
+
     }
     
 let buttonClick = document.getElementById("criar-tarefa")
 buttonClick.addEventListener("click", createList)
 
-  
+function change (event){
+  event.target.style.backgroundColor = "rgb(128, 128, 128)"
+    console.log(event)
+}     
