cc5e45c5e9b3891531c94552ea61e820daa5351f
Gabriel Viana <g.viana87@hotmail.com>
2021-05-09 21:34:13 -0300

    Item 7

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 //CRIA TAREFA
 const criarTarefa = document.querySelector('#criar-tarefa');
-criarTarefa.addEventListener('click', newAssignment);
-function newAssignment(){
+criarTarefa.addEventListener('click', addTarefa);
+function addTarefa(){
     const textoTarefa = document.querySelector('#texto-tarefa').value;
     if (textoTarefa !== '') {
         const addLi = document.createElement('li');
         addLi.innerHTML = textoTarefa;
-        addLi.classList.add('listItem');
+        addLi.classList.add('listIten');
+        addLi.addEventListener('click', selected);
+       
         document.querySelector('#lista-tarefas').appendChild(addLi);
         document.querySelector('#texto-tarefa').value = '';
     } else {
         alert('Adicione uma tarefa')
     };
 };
+
+//SELECIONA ITEM DA LISTA
+function selected(event) {
+    event.target.classList.add('selected');
+};
+
+// clearSelected = document.querySelector('.selected');
+// if (event.target.classList === clearSelected.classList) {
+//     clearSelected.classList.remove('selected');
+// };
+    
+    
+
+
 
 
 //APAGA TUDO
@@ -22,3 +38,5 @@ function limpaTarefa (){
     const itensList = document.querySelector('#lista-tarefas');
     itensList.innerText = '';
 };
+
+
