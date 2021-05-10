008d93136bc7a34c62f86aa0d394584e0c72ced8
Laryssa Martins <laryssamartinsoffice@gmail.com>
2021-05-09 22:43:52 -0300

    Refatorando requisito 2

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ window.onload = function () {
   titulo.innerHTML = "Minha Lista de Tarefas";
   document.body.appendChild(titulo);
 
+  let text = document.createElement("p");
+  text.innerHTML = "Clique duas vezes em um item para marcá-lo como completo";
+  text.id = "funcionamento";
+  document.body.appendChild(text);
   
 };
