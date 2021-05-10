f1e0209ed38f725fa981f402324cf933321edf12
rachidb5 <jordan.rachid@gmail.com>
2021-05-09 23:23:48 -0300

    deseleciona elemento selecionado anteriormente quando outro elemento e selecionado

--- script.js
+++ script.js
@@ -1,39 +1,30 @@
 const input = document.getElementById('texto-tarefa');
-const listaTarefa = document.getElementById("lista-tarefas");
+const listaTarefa = document.getElementById('lista-tarefas');
 
-function grey(event) {
-    event.target.style.backgroundColor = 'rgb(128,128,128)';
+function gray(event) {
+  const li = document.querySelectorAll('.tarefa');
+  for (let i = 0; i < li.length; i += 1) {
+    li[i].classList.remove('gray');
+  }
+  event.target.classList.add('gray');
 }
 function done(event) {
-    if(event.target.classList.contains('completed')){
-    event.target.classList.remove('completed')
-    event.target.style.textDecoration = 'none'
-    } else {
-    event.target.classList.add('completed')
-    event.target.style.textDecoration = 'line-through solid rgb(0, 0, 0)'
-    }
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
 }
 
 function createTask() {
-    const tarefa = document.createElement("li");
-    tarefa.classList.add('tarefa');
-    tarefa.innerHTML = input.value;
-    listaTarefa.appendChild(tarefa);
-    console.log(input.value)
-    input.value =''
-    tarefa.addEventListener('click', grey);
-    tarefa.addEventListener('dblclick', done);
+  const tarefa = document.createElement('li');
+  tarefa.classList.add('tarefa');
+  tarefa.innerHTML = input.value;
+  listaTarefa.appendChild(tarefa);
+  console.log(input.value);
+  input.value = '';
+  tarefa.addEventListener('click', gray);
+  tarefa.addEventListener('dblclick', done);
 }
 
-document.getElementById("criar-tarefa").addEventListener('click', createTask);
-
-/*function done(event) {
-    event.target.style.backgroundColor = 'rgb(128,128,128)';
-    event.target.style.textDecoration = "line-through";
-}
-
-const tarefa = document.querySelectorAll(".tarefa")
-
-for (let i = 0; i < tarefa.length; i += 1) {
-    tarefa[i].addEventListener('click', done);
-  }*/
+document.getElementById('criar-tarefa').addEventListener('click', createTask);
