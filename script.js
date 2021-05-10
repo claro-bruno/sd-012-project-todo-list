4e5c0d3b253851ac4e69547a6ff5c8dfe4b84c8f
Ramon Paschoal <rpaschoal90@gmail.com>
2021-05-10 10:58:38 -0300

    Tirar marcaçao

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ botao.innerHTML = 'Criar tarefa';
 let inputSelect = document.getElementById('texto-tarefa');
 let selectOl = document.getElementById('lista-tarefas');
 let selectClear = document.getElementById('apaga-tudo');
-
+let selectRemove = document.getElementById('remover-finalizados');
 
 function capturar () {
     botao.addEventListener('click', function (event) {
@@ -20,38 +20,44 @@ function capturar () {
             } 
             event.target.classList.add('selected');
         });
-        createList.addEventListener('dblclick', function (event) { // Exercício 9
-            event.target.classList.toggle('completed'); // função toggle ai adicionar a classe caso ela não exista e tirar caso exista
-            // if (event.target.classList.contains('completed')) {
-            //     event.target.classList.remove('completed');
-            // } else {
-            //     event.target.classList.add('completed');
-            // }
-        });
+    createList.addEventListener('dblclick', function (event) { // Exercício 9
+        event.target.classList.toggle('completed'); // função toggle ai adicionar a classe caso ela não exista e tirar caso exista
+        // if (event.target.classList.contains('completed')) {
+        //     event.target.classList.remove('completed');
+        // } else {
+        //     event.target.classList.add('completed');
+        // }
+    });
 
-        let valorCapturado = inputSelect.value;
-        createList.innerHTML = valorCapturado;
-        selectOl.appendChild(createList);
+    let valorCapturado = inputSelect.value;
+    createList.innerHTML = valorCapturado;
+    selectOl.appendChild(createList);
 
 
-        function clearInput () { // Exercício 6
-            if(inputSelect.value !== '') {
-                inputSelect.value = '';
-            };
+    function clearInput () { // Exercício 6
+        if(inputSelect.value !== '') {
+            inputSelect.value = '';
         };
-        clearInput ();
+    };
+    clearInput ();
 
        
     });
 };
+    function clearDone () { //Exercício 11
+        
+    }
+    clearDone ();
 
 capturar ();
 
  //Exercício 10:
  function clearList () {
-    selectClear.addEventListener('click', function() {
-        while (selectOl.firstElementChild)
-        selectOl.removeChild(selectOl.firstElementChild);
+    selectRemove.addEventListener('click', function() {
+        if(selectRemove.className === 'selected completed');{
+            while (selectOl.firstElementChild)
+            selectOl.removeChild(selectOl.firstElementChild);
+        }
     })
 }
 clearList ();
