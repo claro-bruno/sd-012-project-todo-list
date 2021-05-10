702797160a46bf35b695c0eef98583c61659e79f
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-05-10 12:31:52 -0300

    resolve o questao 07

--- script.js
+++ script.js
@@ -11,8 +11,20 @@ function criaLista(event){
     let returnList = document.getElementById('texto-tarefa').value;
     item.innerHTML = returnList;
     document.getElementById('texto-tarefa').value = '';
-    
+
 }
-    
+
+let listaTarefas = document.querySelector('#lista-tarefas');
+listaTarefas.addEventListener('click', alteraCor);  //está aguardano o momento do click por isso fora da função
+
+function alteraCor(event){
+    let buscaColor = document.getElementsByClassName('lista');
+    for(let index = 0; index < buscaColor.length; index += 1){
+    buscaColor[index].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+}
+
+//}   
     
 
