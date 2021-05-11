062608de5cb68ffccc70ab95251905a009c92c79
Marcela Silva <marcela.ops@hotmail.br>
2021-05-10 22:15:03 -0300

    requi 9

--- script.js
+++ script.js
@@ -16,35 +16,33 @@ window.onload = function() {
             newTask.innerHTML =  inputTask.value;
             taskList.appendChild(newTask);
             inputTask.value = '';
-            newTask.addEventListener('click', changeToGrey);
+            newTask.addEventListener('click', changeToGrey);   //essa e a de baixo eu já adicionei o escutador para as funções do 7 e 9.
             newTask.addEventListener('dblclick', addLineThrough);
         }
 
     }
     creatTaskButton.addEventListener('click', addTask);
 
-    //7 e 8
+    //7 e 8 
+    //Os meninos, Gabriel, Caio, Thalles e o Bruno me ajudaram a pensar assim: Primeiro selecionar/coletar, depois adicionar o escutador e pr fim fazer uma função para falar o q vai acontecer depois de eu clicar (ou qq outro evento).
     function changeToGrey(event) {
         let taskElement = document.querySelectorAll('.tarefa');
         for (let index = 0; index < taskElement.length; index += 1) {
-            taskElement[index].classList.remove('task-background-color');    
+            taskElement[index].classList.remove('task-background-color');    //A classList retorna uma array
         };
         event.target.classList.add('task-background-color');
     } 
 
     //9
     function addLineThrough(event){
-        let taskElement = document.querySelectorAll('.tarefa');
-        for (let index = 0; index < taskElement.length; index += 1) {
-            taskElement[index].classList.remove('completed');    
-        };
-        event.target.classList.add('completed');
+        if(event.target.classList.value.includes('completed')) { // Ele está usando o includes p ver se ao longo da array q contem as classes, tem alguma pálavra 'completed'
+            event.target.classList.remove('completed');
+        } else {
+            event.target.classList.add('completed');
+        }
     }
     
-        
     
-    
-
     
     
 
