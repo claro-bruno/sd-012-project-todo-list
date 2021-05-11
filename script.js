22e2b77b7e5d5edbaf0f62d68dd96a7c9790267c
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-11 19:53:51 -0300

    add funçao de criar tarefa e limpa input - no liverServer deu certo

--- script.js
+++ script.js
@@ -5,43 +5,21 @@ const completedTask = document.getElementById("funcionamento");
 const buttonDeleteAll = document.getElementById("apaga-tudo");
 const buttonRemoveComplet = document.getElementById("remover-finalizados");
 
+// funçao para mostrar as tarefas adicionadas
+// atribuir o conteudo digitado na caixa ao elementTask, assim, quando o conteúdo da caixa for diferente de vazio, o contéudo será adicionado em forma de lista ordenada pela hierarquica da TASKLIST e após, o element task é limpo. Assim, ao acionar o botao, a função é executada.
 
-
-
-
-
-
-
-/*input.addEventListener('click', () => {
-    const taskBoard = document.createElement("li");
-    taskBoard.innerHTML = inputButton.value;
-    inputButton.value = '';
-    event.preventDefault();
-    taskList.appendChild(taskBoard);
-});   
-
-// Em pesquisa foi localizada a função preventDefault(), evitando que o envio da tarefa resete o formulário, gravando seu conteúdo; acessado em (https://www.w3schools.com/jsref/event_preventdefault.asp)
-
-function backgroundColor () {
-    let selectText = document.querySelectorAll('#lista-tarefas');
-    for (let index = 0; index < selectText.length; index += 1){
-        selectText[index].addEventListener('click', function (event) {
-            let selectedItem = event.target;
-            let selectedColor = 'rgb(128,128,128)';
-            selectedItem.style.backgroundColor = selectedColor;
-          });  
-        selectText[index].style.backgroundColor = 'white';
+function createTask () {
+    const elementTask = inputTask;
+    if (elementTask !== '') {
+        const createItem = document.createElement('li');
+        createItem.innerText = elementTask.value;
+        event.preventDefault()
+        taskList.appendChild(createItem);
+        elementTask.value = '';
     }
 }
-backgroundColor();
-
-function changeBackgroundColor () {
-    let selectText = document.querySelectorAll('#lista-tarefas');
-    for (let index = 0; index < selectText.length; index += 1) {
-        selectText.addEventListener('click', backgroundColor);
-    };
-};
-changeBackgroundColor()*/
+buttonCreateTask.addEventListener('click', createTask);
+// Em pesquisa foi localizada a função preventDefault(), evitando que o envio da tarefa resete o formulário, gravando seu conteúdo; acessado em (https://www.w3schools.com/jsref/event_preventdefault.asp)
 
 
 
