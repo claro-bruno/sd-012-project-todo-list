32ddc06556ae3b8b7f2d3cf0e2c6ede85912ed8f
Martin <escorpmartin97@gmail.com>
2021-05-11 18:17:21 -0300

    adiciona imput

---
+++ script.js
@@ -0,0 +1,8 @@
+function lista () {
+    let section = document.querySelector = ('#tarefas')
+    for (let index = 0; index < 5; index += 1){
+let list = document.createElement('ol');
+list.id = 'lista-tarefas';
+section.appen567dChild(list);
+    }
+}
