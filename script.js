5150f2c5ae5c32deb0e0d3abfd26553024ada0c8
Bruno Carv <bruno.fox_carvalho@hotmail.com>
2021-05-11 13:57:03 -0300

    req 7, uso do target

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ let bornTodo = document.querySelector("#criar-tarefa")
 let countList = document.getElementById("lista-tarefas");
 
 function funcBornTodo() {
-  let contentNewTodo= document.querySelector("#texto-tarefa");
+  let contentNewTodo = document.querySelector("#texto-tarefa");
   if (contentNewTodo.value == '') {} else {
     let newItem = document.createElement("li");
     let newButton = document.createElement("button");
     countList.appendChild(newItem);
     let caughtItem = document.getElementById("lista-tarefas").lastChild
-        newItem.appendChild(newButton);
+    newItem.appendChild(newButton);
     caughtItem.innerHTML = contentNewTodo.value;
     contentNewTodo.value = "";
   };
@@ -17,7 +17,12 @@ function funcBornTodo() {
 bornTodo.addEventListener("click", funcBornTodo);
 
 function changecolor() {
- caughtButton.style.color = rgb(128, 128, 128)
+  for (let indice = 0; indice < countList.length; indice += 1) {
+    let selectItem = countList.children[indice];
+    if event.target.selectItem == true) {
+      selectItem.style.color = rgb(128, 128, 128);
+    }
+  }
 };
 
 countList.addEventListener("click", changecolor);
