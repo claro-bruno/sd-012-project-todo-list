cbd91461076adf26c6e0167251881536d34df2fb
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2021-05-10 21:40:28 -0300

    feito o requisito 7

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let listaDeItem = document.getElementById("lista-tarefas");
 let botaoAdicionarTarefa = document.getElementById("criar-tarefa");
 
+
 function createTask() {
     let tarefa = document.getElementById("texto-tarefa");
     let itemDaLista = document.createElement('li');
@@ -9,7 +10,12 @@ function createTask() {
         listaDeItem.appendChild(itemDaLista);
     }
     tarefa.value = '';
-
 }
 
 botaoAdicionarTarefa.addEventListener("click", createTask)
+
+listaDeItem.addEventListener("click", () => {
+    if (event.target.id != 'lista-tarefas' && event.target.id != "itens-tarefa") {
+        event.target.style.backgroundColor = "rgb(128, 128, 128)";
+    }
+});
