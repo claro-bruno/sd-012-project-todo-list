322fb75b88b9fa74db156ae98a954a2ad5a5c9b6
Max Alves <maxalves2@hotmail.com>
2021-05-11 19:44:05 -0300

    requisito 7 e 8 - concluido

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 window.onload = function(){
 let button = document.getElementById("criar-tarefa");
 let assignmentList = document.querySelector("#lista-tarefas");
+let lista = document.getElementsByClassName("tarefa");
+let selected = document.createAttribute("class");
+selected.value = "selected";
 
 function addList(){
     button.addEventListener('click', addAssignment)
@@ -9,16 +12,37 @@ function addList(){
 function addAssignment(){
     let input = document.querySelector("#texto-tarefa").value;
     
-    let assignment = document.createElement('li');
+    let assignment = document.createElement("li");
     assignment.innerText = input;
-
+    assignment.classList.add("tarefa");
+    assignment.classList.add("selected");
     assignmentList.appendChild(assignment);
 
     document.querySelector("#texto-tarefa").value = "";    
     
 };
 
+function changeBackground(){
+    assignmentList.addEventListener('click', function(event){
+        selected = document.querySelector(".selected");
+        selected.classList.remove('selected');
+        event.target.classList.add('selected');
+        
+        for (let index = 0; index < lista.length; index += 1){
+            lista[index].style.backgroundColor = "white";
+        };
+    
+        for(let index = 0; index < lista.length; index += 1){
+            if(lista[index].classList.contains('selected')){
+                event.target.style.backgroundColor = "rgb(128, 128, 128)";
+            };
+        };
+    });
+    
+};
+
 addList();
+changeBackground();
 
 };
 
