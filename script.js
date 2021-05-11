01b4300479fbce32f6d6d8df7dfc558a43809ee5
Eurides <euridesalves@yahoo.com.br>
2021-05-11 13:34:59 -0300

    refatorado requisito 5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let botaoClicado = document.getElementById('#criar-tarefa');
+/* let botaoClicado = document.getElementById('criar-tarefa');
 botaoClicado.addEventListener('click', acrescentaLista);
 
 function acrescentaLista(event) {
@@ -11,6 +11,15 @@ function acrescentaLista(event) {
     retornoLista.value = '';
 
     }
-
+ */
 /* window.onload = function () { */
- 
+    const botaoClicado = document.getElementById('criar-tarefa');
+    botaoClicado.addEventListener('click', function () {
+      let novaTarefa = document.createElement('li');
+      /* event.preventDefault(); */
+      novaTarefa.className='lista';
+      document.querySelector('#lista-tarefas').appendChild(novaTarefa);
+      let retornoLista = document.getElementById('texto-tarefa');
+      novaTarefa.innerHTML = retornoLista.value;
+      retornoLista.value = '';
+    })
