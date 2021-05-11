838f86696e420843b8612de0950cf43f2007d44f
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-05-11 18:00:58 -0300

    requisito 1

---
+++ script.js
@@ -0,0 +1,9 @@
+window.load = function() {
+    const header = document.getElementById('titulo');
+    const title = document.createElement('div');
+    title.innerText = 'Minha Lista de Tarefas';
+    header.appendChild(title);
+}
+    
+
+
