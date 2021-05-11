dac929da066d90990fb59efd76ad83ba1a4e0d6b
Eder Santos <3derms@gmail.com>
2021-05-11 11:05:38 -0300

    Resolve os requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-let button = document.getElementById("criar-tarefa")  
+let button = document.getElementById('criar-tarefa');  
 
-button.addEventListener('click', (event) => {
-    console.log("cheguei aqui")
+button.addEventListener('click', () => {
+    
     let itemDaLista = document.createElement('li');
     const inputTextoTarefa = document.querySelector('#texto-tarefa');
     const ListaTarefas = document.querySelector('#lista-tarefas');
-    ListaTarefas.appendChild(itemDaLista) 
-    itemDaLista.innerText = inputTextoTarefa.value;
-    inputTextoTarefa.value = '';
+    if(inputTextoTarefa.value !== '') {
+        itemDaLista.className = 'blankSpace';
+        ListaTarefas.appendChild(itemDaLista) 
+        itemDaLista.innerText = inputTextoTarefa.value;
+        inputTextoTarefa.value = '';
+                
+    } else {
+        alert('Digite um valor');
+    } 
+    
+    itemDaLista.addEventListener('click', (event) => {
+        let emBranco = document.querySelectorAll('.blankSpace');
+            for (let indice = 0; indice < emBranco.length; indice += 1) {
+                emBranco[indice].classList.remove('selected');
+            };
+        event.target.classList.add('selected');
+   
+    })   
+
 })
