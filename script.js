50562635986533ec0a104c5ec5db82d97a734183
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-11 15:51:39 -0300

    requisito 7 - com duas funçoes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputButton = document.getElementById("texto-tarefa");
 const taskList = document.getElementById("lista-tarefas");
 const input = document.getElementById("criar-tarefa");
+const itemList = document.querySelectorAll('.item-list');
 
 input.addEventListener('click', () => {
     const taskBoard = document.createElement("li");
@@ -12,18 +13,27 @@ input.addEventListener('click', () => {
 
 // Em pesquisa foi localizada a função preventDefault(), evitando que o envio da tarefa resete o formulário, gravando seu conteúdo; acessado em (https://www.w3schools.com/jsref/event_preventdefault.asp)
 
-const itemList = document.querySelectorAll('.item-list');
-
-function selectedList() {
-    const backgroundItem  = document.querySelectorAll('.item-list');
-  for (const item of itemList) {
-      item.addEventListener('click', () => {
-          for(const secondItem of itemList) {
-              secondItem.classList.remove('selected');
-          }
-          item.classList.add('selected');
-      });
-  }
+function backgroundColor () {
+    let selectText = document.querySelectorAll('#lista-tarefas');
+    for (let index = 0; index < selectText.length; index += 1){
+        selectText[index].addEventListener('click', function (event) {
+            let selectedItem = event.target;
+            let selectedColor = 'rgb(128,128,128)';
+            selectedItem.style.backgroundColor = selectedColor;
+          });  
+        selectText[index].style.backgroundColor = 'white';
+    }
 }
+backgroundColor();
+
+function changeBackgroundColor () {
+    let selectText = document.querySelectorAll('#lista-tarefas');
+    for (let index = 0; index < selectText.length; index += 1) {
+        selectText.addEventListener('click', backgroundColor);
+    };
+};
+changeBackgroundColor()
+
+
 
 // item 7 e 8 - tentativa de implemento de soluçao através do repositorio da colega Juliane Marques (https://github.com/tryber/sd-012-project-todo-list/pull/119/files#)
