f7e711b3605f07b404812acbd466b89ff90c59ce
Alexander <alexanderteodorocastro95@hotmail.com>
2021-05-11 13:29:40 -0300

    requisito 5/6 terminados

--- script.js
+++ script.js
@@ -5,15 +5,26 @@
 
 const botao = document.getElementById('criar-tarefa');
 
-function aperte(){
+function aperte() {
     const olist = document.getElementById('lista-tarefas');
     let valorInput = document.getElementById('texto-tarefa');
     let novoItem = document.createElement('li')
     novoItem.className = 'item-list';
     novoItem.innerText = valorInput.value
-    if(valorInput.value !== ''){
-    olist.appendChild(novoItem);
+    if (valorInput.value !== '') {
+        olist.appendChild(novoItem);
+        valorInput.value = '';
     }
-}
 
- botao.addEventListener('click',aperte)
+        let itemDaLista = document.querySelectorAll('.item-list');
+        for (let index = 0; index < itemDaLista.length; index += 1) {
+            itemDaLista[index].addEventListener, function (event) {
+                event.target.classList.add('cinza');
+                if(event.target.ClassName === 'cinza'){
+                    
+                }
+            }
+        };
+    }
+
+botao.addEventListener('click', aperte) 
