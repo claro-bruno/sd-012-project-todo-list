c8d9dd6dc6fe73d1a6c457b025368669fc874378
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-05-11 11:13:30 -0400

    Desafio 7 com abstraçao em comentarios

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const button = document.querySelector('#criar-tarefa');
 const listItems = document.querySelector('#lista-tarefas');
 const inputTask = document.querySelector('#texto-tarefa');
+// capturar a lista ordenada
+const ol = document.querySelector('#lista-tarefas');
 
 function addTaskList() {
   button.addEventListener('click', () => {
     if (inputTask.value !== '') {
       const newItem = document.createElement('li');
-      newItem.innerText = inputTask.value;
+      newItem.innerHTML = inputTask.value;
       listItems.appendChild(newItem);
       inputTask.value = '';
     }
   });
 }
 addTaskList();
+
+// Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128)
+
+// Fazer um escutador de eventos de click na lista toda
+ol.addEventListener('click', (event) => {
+  // acessar o bgColor da minha lista clicada usar o event.target
+  const clickedList = event.target.style;
+  clickedList.backgroundColor = 'rgb(128,128,128)';
+});
