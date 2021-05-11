deb29bbf69c8744d476a584ddaf7fec367638d0c
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-10 23:06:57 -0300

    tentativa de soluçao requisito 7 e 8 - com auxilio

--- script.js
+++ script.js
@@ -10,6 +10,20 @@ input.addEventListener('click', () => {
     taskList.appendChild(taskBoard);
 });   
 
-
 // Em pesquisa foi localizada a função preventDefault(), evitando que o envio da tarefa resete o formulário, gravando seu conteúdo; acessado em (https://www.w3schools.com/jsref/event_preventdefault.asp)
 
+const itemList = document.querySelectorAll('.item-list');
+
+function selectedList() {
+    const backgroundItem  = document.querySelectorAll('.item-list');
+  for (const item of itemList) {
+      item.addEventListener('click', () => {
+          for(const secondItem of itemList) {
+              secondItem.classList.remove('selected');
+          }
+          item.classList.add('selected');
+      });
+  }
+}
+
+// item 7 e 8 - tentativa de implemento de soluçao através do repositorio da colega Juliane Marques (https://github.com/tryber/sd-012-project-todo-list/pull/119/files#)
