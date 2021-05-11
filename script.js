a190671dca14899c287bd387720294b7015b9c49
Josimar Schneider <josimarschneider@gmail.com>
2021-05-11 19:57:47 -0400

    adicionando cor cinza do background

--- script.js
+++ script.js
@@ -5,4 +5,18 @@ function CreateTask() {//cria a tarefa
     document.getElementById('lista-tarefas').appendChild(task);
     task.innerText = text.value;
     text.value = null;
+    selectTask();
   }
+
+   function coloringbackground (event) {//atribui a class selected que contém o background cinza.
+    event.target.classList.add('selected');
+  }
+
+   function selectTask(){//captura o clique na tarefa
+    const task = document.getElementsByTagName('li');
+    for (let index=0; index < task.length; index +=1){
+        task[index].addEventListener('click', coloringbackground);
+    }
+   }
+   
+  
