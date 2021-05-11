54e6c9e6d40b09a160d0b128f963716d32ab87ff
Martin <escorpmartin97@gmail.com>
2021-05-11 18:27:28 -0300

    adiciona lista de tarefas

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function lista () {
-    let section = document.querySelector = ('#tarefas')
-    for (let index = 0; index < 5; index += 1){
-let list = document.createElement('ol');
-list.id = 'lista-tarefas';
-section.appen567dChild(list);
-    }
+let section = document.querySelector('#tarefas')
+
+function lista() {
+    let list = document.createElement('ol');
+    list.id = 'lista-tarefas';
+    section.appendChild(list);
 }
+lista()
