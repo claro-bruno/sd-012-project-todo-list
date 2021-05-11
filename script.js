19e259e1be784490bfe782234d311f86ee1b7191
Lucas Basques <lukasbasques@gmail.com>
2021-05-10 22:04:34 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let textInput = document.getElementById('texto-tarefa')
 let addButton = document.getElementById('criar-tarefa')
+let listItem = document.getElementsByTagName('li')
 
 function addList() {
     var createLi = document.createElement("LI");
@@ -8,7 +9,22 @@ function addList() {
     document.getElementById('lista-tarefas').appendChild(createLi);
 }
 
+function LiClass(){
+    for (let index = 0; index < listItem.length; index+= 1) {
+        listItem[index].setAttribute('class', 'listItem')
+    }
+}
+
+
 addButton.addEventListener("click", function(){
     addList()
+    LiClass()
     textInput.value = ''
 })
+
+document.body.addEventListener("click", function(event){
+    if(event.target.className === "listItem"){
+      event.target.style.backgroundColor = 'white'
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)'  
+    }
+  })
