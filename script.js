2d9f96155141da8c9e93b53fbb9fdaaa3809240d
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-05-11 22:08:44 -0400

    Desafio 8 - Refatorado

--- script.js
+++ script.js
@@ -17,29 +17,24 @@ function addTaskList() {
 addTaskList();
 
 function paintTask() {
-  // capture todos os elementos da lista em forma de array
   const itemSelected = document.querySelectorAll('.tasks');
-
   for (let index = 0; index < itemSelected.length; index += 1) {
-    // se o elemento tiver a classe selected pinte de vermelho
     if (itemSelected[index].classList.contains('selected')) {
       itemSelected[index].style.backgroundColor = 'rgb(128, 128, 128)';
-      // se não tiver a classe selected pint de azul
     } else {
       itemSelected[index].style.backgroundColor = 'white';
     }
   }
 }
 
-ol.addEventListener('click', (event) => {
-  // Capture o elemento que tenha a classe selected
-  const itemSelected = document.querySelector('.selected');
-  // se o elemento com a classe selected existir
-  if (itemSelected !== null) {
-    // remova a classe selected dele
-    itemSelected.classList.remove('selected');
-  }
-  // Se o elemento com a classe selected não existir, adione a classe selected ao elemento clicado.
-  event.target.classList.add('selected');
-  paintTask();
-});
+function selectItem() {
+  ol.addEventListener('click', (event) => {
+    const itemSelected = document.querySelector('.selected');
+    if (itemSelected !== null) {
+      itemSelected.classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+    paintTask();
+  });
+}
+selectItem();
