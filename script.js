5c92dc9e9e0e97ae14833985470159ce137f4cfc
Elias Forte <eliasforte.dev@gmail.com>
2021-05-11 22:38:36 -0300

    correçao no CSS requisito 9

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ selecteIten.addEventListener('dblclick', function (event) {
 
 
 
-buttonRmvSelected.addEventListener('click',function(event) {
+buttonRmvSelected.addEventListener('click', function() {
   const taskCompleted = document.querySelectorAll('.completed');
   for (let index = 0; index < taskCompleted.length; index += 1){
   taskCompleted[index].remove();
