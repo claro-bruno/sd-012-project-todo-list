45e9d27e6aefe4a14aeaeb7d6d35cd7e897f12fc
Victor Emmanuel <victorpassos2012@gmail.com>
2021-05-12 20:23:17 -0300

    7 e 8 feito

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const creatList = document.getElementById('criar-tarefa');
 
-function addtolist(event){
-    let li = document.createElement('li');
-    li.className = 'toDO';
-    li.innerHTML = document.querySelector('#texto-tarefa').value;
-    document.getElementById('lista-tarefas').appendChild(li);
-    document.querySelector('#texto-tarefa').value = '';
+function addtolist(event) {
+const li = document.createElement('li');
+li.className = 'toDO';
+li.innerHTML = document.querySelector('#texto-tarefa').value;
+document.getElementById('lista-tarefas').appendChild(li);
+document.querySelector('#texto-tarefa').value = '';
 }
 
 creatList.addEventListener('click', addtolist);
+
+function highlightBackground(event) {
+    let previous = document.querySelector('.highlighted');
+    if (!previous) { 
+      event.target.classList.add('highlighted') 
+    } else if (event.target.classList.contains('highlighted')){ 
+      event.target.classList.remove('highlighted');
+    } else {
+      previous.classList.remove('highlighted');
+      event.target.classList.add('highlighted');
+    }
+  }
+
+const highlightList = document.getElementById('lista-tarefas');
+
+highlightList.addEventListener('click', highlightBackground);
