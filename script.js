e27a5ff8351e98253ac8c202531f6e9d699fc8f9
Henrique Remedios <henriqueremedios@gmail.com>
2021-05-12 01:34:12 -0300

    07-08 - Adiciona clica no item e pinta o fundo, nao deixa pintar mais de um elemento ao mesmo tempo

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ function criaLista() {
     let funcao = document.createElement('li');
     funcao.classList.add('lista-tarefas');
     funcao.innerHTML = input.value;
-    document.querySelector('#lista-tarefas').appendChild(funcao);  
+    document.querySelector('#lista-tarefas').appendChild(funcao);
+
 };
 
 function limpaInput() {
-    input.value = ' ';
+    input.value = '';
 };
 
 function botaoAdd() {
@@ -20,3 +21,20 @@ function botaoAdd() {
 
 button.addEventListener('click', botaoAdd);
 
+function pintaFundoItem(event) {
+
+    let listaSelecionada = document.querySelector('.selected')
+    if (listaSelecionada) { 
+        listaSelecionada.classList.remove('selected');
+        listaSelecionada.style.backgroundColor = '';
+    };   
+
+    event.target.classList.add('selected');
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+};
+
+function corFundoItem() {
+    pintaFundoItem();
+};
+
+listaTarefas.addEventListener("click", pintaFundoItem);
