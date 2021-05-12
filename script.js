852a44e7f8d4b33ddeb758601e076f1d2ef25474
Ed Santos <edsantos2822@gmail.com>
2021-05-11 22:05:23 -0300

    Finalizando - Requisito 7

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-// ...
+// Adiciona funcionalidade ao botão para criar tarefas
 const pegaCriarTarefa = document.getElementById('criar-tarefa');
 const pegaListaTarefas = document.getElementById('lista-tarefas');
+pegaCriarTarefa.style.cursor = 'pointer';
 
 function criandoLi() {
-
   pegaCriarTarefa.addEventListener('click', function(){
     let pegaTextoTarefa = document.querySelector('#texto-tarefa').value;
-  
 
     let criaLi = document.createElement('li');
     pegaListaTarefas.appendChild(criaLi);
 
     criaLi.innerText = pegaTextoTarefa;
+    criaLi.style.cursor = 'pointer';
+    criaLi.addEventListener('click', selecionaLi);
     
     pegaTextoTarefa = document.querySelector('#texto-tarefa').value = '';
-    
   });
-  
 };
 criandoLi();
+
+// Seleciona um ítem da lista
+function selecionaLi(event) {
+  event.target.className = 'selected';
+}
