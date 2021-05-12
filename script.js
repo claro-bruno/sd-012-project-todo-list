ec8e24c1c64849965bd34efe5a1d490e64182f07
Eurides <euridesalves@yahoo.com.br>
2021-05-12 14:22:55 -0300

    adicionado requisito 7

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-/* let botaoClicado = document.getElementById('criar-tarefa');
-botaoClicado.addEventListener('click', acrescentaLista);
-
-function acrescentaLista(event) {
+//*
+function checarBotao() {
+  const botaoClicado = document.getElementById('criar-tarefa');
+  botaoClicado.addEventListener('click', function () {
     let novaTarefa = document.createElement('li');
-    event.preventDefault();
-    novaTarefa.className='lista';
+    /* event.preventDefault(); */
+    novaTarefa.className='tarefas';
     document.querySelector('#lista-tarefas').appendChild(novaTarefa);
     let retornoLista = document.getElementById('texto-tarefa');
     novaTarefa.innerHTML = retornoLista.value;
-    retornoLista.value = '';
+    checarTarefaSelecionada();
+  })
+}
 
-    }
- */
+function checarTarefaSelecionada() {
+  let listaClicada = document.getElementsByClassName("tarefas");
+  for (let indice = 0; indice < listaClicada.length; indice += 1) {
+    listaClicada[indice].addEventListener('click', function (event) {
+      event.target.style.background = "rgb(128, 128, 128)";
+    });
+  }
+}
+
 /* window.onload = function () { */
-    const botaoClicado = document.getElementById('criar-tarefa');
-    botaoClicado.addEventListener('click', function () {
-      let novaTarefa = document.createElement('li');
-      /* event.preventDefault(); */
-      novaTarefa.className='lista';
-      document.querySelector('#lista-tarefas').appendChild(novaTarefa);
-      let retornoLista = document.getElementById('texto-tarefa');
-      novaTarefa.innerHTML = retornoLista.value;
-      retornoLista.value = '';
-    })
+checarBotao();
+
