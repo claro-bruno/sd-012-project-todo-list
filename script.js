c5a97164deff7df502322cf18e368f2df47d1f48
Alice Gonçalves <galice9992@gmail.com>
2021-05-12 14:51:52 -0300

    resolve requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 window.onload = function () {   
     document.body.addEventListener('click', function(event){
         if (event.target.id === 'criar-tarefa') {
-            let lI = document.createElement('li') 
+        let lI = document.createElement('li')
+        lI.className = 'task' 
         lI.innerHTML = document.querySelector('#texto-tarefa').value;
         document.getElementById('lista-tarefas').appendChild(lI)
         document.querySelector('#texto-tarefa').value = ""
+        }
+    }
+    )
+    document.body.addEventListener('click', function (event){
+        let selected = document.querySelectorAll('.selected')
+        if (event.target.className === 'task') {
+            for (let taskIndex = 0; taskIndex < selected.length; taskIndex += 1) {
+            selected[taskIndex].className = 'task'   
+            }
+            event.target.className = 'selected'
         }
     }
     )
