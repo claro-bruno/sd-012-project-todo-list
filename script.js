cbd8c4b8ba195c837c11e0ab0e99023f7f293cfd
felipe_louzeiro <fl.louzeiro@gmail.com>
2021-05-12 12:57:47 -0300

    3 - Adicionando um input com o id=texto-tarefa onde a pessoa usuária poderá digitar o nome do item que deseja adicionar à lista

--- script.js
+++ script.js
@@ -22,3 +22,24 @@ let input = document.createElement("input");
 input.id = "texto-tarefa";
 document.body.appendChild(input);
 
+
+// ### 4 - Adicione uma lista ordenada de tarefas com o id="lista-tarefas"
+
+// **O que será verificado:**
+
+// - Será verificada a existência de um elemento `ol` com o id `lista-tarefas`.
+let listTask = document.createElement("ol");
+listTask.id = "lista-tarefas";
+document.body.appendChild(listTask);
+
+
+
+// ### 5 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo
+
+// **O que será verificado:**
+
+// - Será verificada a existência de um elemento do tipo `button` com o id `criar-tarefa`
+
+// - No campo de input será digitado o texto de uma tarefa qualquer e, em seguida, clicar-se-á no botão de criar tarefa. Será verificado que, após o clique, o texto digitado aparece na lista e desaparece do input.
+
+// - A adição de elementos na lista será feita algumas vezes, e será checado se todos os itens criados permanecem na lista na medida em que novos são adicionados.
