a1d44c4a17860de545bef60839d0f33a2d2315e4
Vinincius-Naoke <viniciusnaokert@gmail.com>
2021-05-12 18:28:29 -0300

    completa passos 8 e 9

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 const input = document.getElementById('texto-tarefa');
 const list = document.getElementById('lista-tarefas');
-const addTask = document.getElementById('cria-tarefa');
-const listItemClass = document.getElementsByClassName('list-item');
+const addTask = document.getElementById('criar-tarefa');
+const listElement = document.getElementsByClassName('list-item');
+
+
 addTask.addEventListener('click', addListItem);
 
-function giveListener(){
-   listItemClass.addEventListener('click', changeBgCOlor());
+function addSelected(event) {
+  const eventTarget = event.target;
+  for (let index = 0; index < listElement.length; index += 1) {
+    if (listElement[index].classList.contains('selected')) {
+      listElement[index].classList.remove('selected');
+    }
+  }
+  eventTarget.classList.add('selected')
 }
 
-function addListItem(){
-let listItem = document.createElement('li');
-listItem.innerHTML = input.value;
-listItem.className ='list-item';
-list.appendChild(listItem);
-giveListener();
+
+function addListItem() {
+  let listItem = document.createElement('li');
+  listItem.innerHTML = input.value;
+  listItem.classList.add('list-item');
+  list.appendChild(listItem);
+  input.value = '';
+  listItem.addEventListener('click', addSelected);
+  listItem.addEventListener('dblclick', addLineThrough);
 }
 
 
-
-
-function changeBgCOlor (){
-    listItemClass
+function addLineThrough(event) {
+  const eventTarget = event.target;
+  if (eventTarget.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
 }
