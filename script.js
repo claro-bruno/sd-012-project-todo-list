36e127f075b5dff9a83b125755bc74682d5b0aa1
Josimar Schneider <josimarschneider@gmail.com>
2021-05-11 20:25:25 -0400

    remove a classe selected que contém a cor cinza

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 document.getElementById('criar-tarefa').addEventListener('click', CreateTask);
 function CreateTask() {//cria a tarefa
-    const text = document.getElementById('texto-tarefa');
-    const task = document.createElement('li');
-    document.getElementById('lista-tarefas').appendChild(task);
-    task.innerText = text.value;
-    text.value = null;
-    selectTask();
-  }
-
-   function coloringbackground (event) {//atribui a class selected que contém o background cinza.
-    event.target.classList.add('selected');
-  }
-
-   function selectTask(){//captura o clique na tarefa
-    const task = document.getElementsByTagName('li');
-    for (let index=0; index < task.length; index +=1){
-        task[index].addEventListener('click', coloringbackground);
+  const text = document.getElementById('texto-tarefa');
+  const task = document.createElement('li');
+  document.getElementById('lista-tarefas').appendChild(task);
+  task.innerText = text.value;
+  text.value = null;
+  selectTask();
+}
+function removeSelectedClass() {//remove a classe selected
+  const task = document.getElementsByTagName('li');
+  for (let index=0; index < task.length; index += 1) {
+    if(task[index].classList.contains('selected')){
+      task[index].classList.remove('selected');
     }
-   }
+  }
+}
+function coloringbackground (event) {//atribui a class selected que contém o background cinza.
+  removeSelectedClass();
+  event.target.classList.add('selected');
+}
+function selectTask() {//captura o clique na tarefa
+  const task = document.getElementsByTagName('li');
+  for (let index=0; index < task.length; index += 1) {
+    task[index].addEventListener('click', coloringbackground);
+  }
+}
    
   
