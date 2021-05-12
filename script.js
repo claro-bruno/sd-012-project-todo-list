50134b831ec18e220f34282e4afffc559426e853
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-05-11 21:07:25 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-window.load = function() {
-    const header = document.getElementById('titulo');
-    const title = document.createElement('div');
-    title.innerText = 'Minha Lista de Tarefas';
-    header.appendChild(title);
+//window.load = function() {
+    //const cabeçalho = document.querySelector//('#titulo');
+    //const title = document.createElement('h1');
+    //cabeçalho.appendChild(title);
+    //title.innerText = 'Minha Lista de Tarefas';
  
-    const lista = document.createElement('ol');
-    const section = document.getElementsByTagName(section);
-    section.appendChild(lista);
-    
-    
-}
+    //const lista = document.createElement('ol');
+    //const section = document.getElementsByTagName//(section);
+    //section.appendChild(lista);
+
+const botao = document.querySelector('#criar-tarefa');
+        botao.addEventListener('click', addTarefa);
+        
+        function addTarefa() {
+            const input = document.querySelector('#texto-tarefa');
+            const lista = document.querySelector('#lista-tarefas');
+            let item = document.createElement('li');           
+            lista.appendChild(item);
+            item.innerText = input.value;
+            input.value = "";
+            
+        }
+//    }
