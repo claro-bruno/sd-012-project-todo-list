8ce4bfdb4ea49dca01a31daa0785bfa0959f239d
Josimar Schneider <josimarschneider@gmail.com>
2021-05-11 20:39:16 -0400

    adiciona a classe completed, que risca a tarefa

--- script.js
+++ script.js
@@ -15,14 +15,18 @@ function removeSelectedClass() {//remove a classe selected
     }
   }
 }
+function completedLineThrough(event){
+  event.target.classList.add('completed');
+}
 function coloringbackground (event) {//atribui a class selected que contém o background cinza.
-  removeSelectedClass();
+  removeSelectedClass();//remove a classe selected se houver
   event.target.classList.add('selected');
 }
 function selectTask() {//captura o clique na tarefa
   const task = document.getElementsByTagName('li');
   for (let index=0; index < task.length; index += 1) {
     task[index].addEventListener('click', coloringbackground);
+    task[index].addEventListener('dblclick', completedLineThrough);
   }
 }
    
