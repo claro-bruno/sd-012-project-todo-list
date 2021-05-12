ba2cc6f3ef21d3f72f8910615ac07b3774a9a856
Alice Gonçalves <galice9992@gmail.com>
2021-05-12 19:14:02 -0300

    resolve requisito 9

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.onload = function () {   
     document.body.addEventListener('click', function(event){
-        if (event.target.id === 'criar-tarefa') {
-        let lI = document.createElement('li')
-        lI.className = 'task' 
-        lI.innerHTML = document.querySelector('#texto-tarefa').value;
-        document.getElementById('lista-tarefas').appendChild(lI)
-        document.querySelector('#texto-tarefa').value = ""
+            if (event.target.id === 'criar-tarefa') {
+            let lI = document.createElement('li')
+            lI.className = 'task' 
+            lI.innerHTML = document.querySelector('#texto-tarefa').value;
+            document.getElementById('lista-tarefas').appendChild(lI)
+            document.querySelector('#texto-tarefa').value = ""
         }
     }
     )
@@ -15,8 +15,16 @@ window.onload = function () {
             for (let taskIndex = 0; taskIndex < selected.length; taskIndex += 1) {
             selected[taskIndex].className = 'task'   
             }
-            event.target.className = 'selected'
+            event.target.className = 'task selected'
         }
     }
     )
+    document.body.addEventListener('dblclick', function (event) {
+        if (event.target.className === 'task selected') {
+            event.target.className = 'task selected completed'
+        } else if (event.target.className === 'task selected completed') {
+            event.target.className = 'task selected'
+        }
+
+    })
 }
