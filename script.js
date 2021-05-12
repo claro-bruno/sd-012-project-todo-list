8e610f219b81dd896ee64ae200939b27c847d4e6
Augusto <augustocm2002@yahoo.com.br>
2021-05-12 20:23:07 -0300

    Fazendo requisito 10

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let createTask = document.querySelector('#texto-tarefa');
 let listOl = document.querySelector('#lista-tarefas');
 let buttonText = document.querySelector('#criar-tarefa');
@@ -6,48 +7,50 @@ let buttonText = document.querySelector('#criar-tarefa');
 
 function addItem (event) {
     
+ 
     if (createTask.value != "") {
         let newLi = document.createElement('li');
         newLi.innerHTML = document.querySelector('#texto-tarefa').value;
         listOl.appendChild(newLi);
         newLi.className = 'lista';
-
+    
     
 function changeColor (event){
+ 
     let grey = document.querySelector('.grey');
-    event.target.classList.add('grey');
-    
+   
+  
+        event.target.classList.add('grey');
+        // grey.classList.remove('grey');
 }
+    
     let allList = document.querySelector('.lista');
-    // grey.classList.remove('grey');
-    
-//     function changeColor (event){
-//         let grey = document.getElementsByClassName('grey');
-//         event.target.classList.add('grey')
-//         grey.classList.remove('grey');
-//     allList.addEventListener('click', changeColor)
-// }
 
-allList.addEventListener('click', changeColor)
-    }
-    //Apaga campo de texto
-createTask.value = "";
+    allList.addEventListener('click', changeColor)
+
+  
 }
-buttonText.addEventListener('click', addItem)
+    //Apaga campo de texto
+    createTask.value = "";
+}
+    buttonText.addEventListener('click', addItem)
+
 
 //Tarefa 7
 
-let allList = document.querySelector('.lista');
-let greyColor = 'rgb(128, 128, 128)';
-let resetColor = 'white'
-let grey = document.querySelector('.grey');
+    let allList = document.querySelector('.lista');
+    let greyColor = 'rgb(128, 128, 128)';
+    let resetColor = 'white'
+    let grey = document.querySelector('.grey');
 
-// function changeColor (event){
-//     let grey = document.querySelector('.grey');
-   
-//     allList.className = 'lista grey';
-// allList.classList.remove('grey');
-//     event.target.classList.add('grey');
-   
-// }
-// let allList = document.querySelector('.lista');
+
+
+//Tarefa 10
+    let deleteButton = document.querySelector('#apaga-tudo');
+
+function deleteList () {
+    
+    listOl.innerHTML = "";
+
+    }
+    deleteButton.addEventListener('click', deleteList)
