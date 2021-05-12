6cd0e5df2c9dcdd22d466a8c4a6789c814583bec
Rafael_Primon <rafaelfernandesprimon@gmail.com>
2021-05-11 23:38:23 -0300

    altera funçao para acrescentar classe selected e atraves dela, alterar background color

--- script.js
+++ script.js
@@ -21,15 +21,19 @@ function createTask () {
 buttonCreateTask.addEventListener('click', createTask);
 // Em pesquisa foi localizada a função preventDefault(), evitando que o envio da tarefa resete o formulário, gravando seu conteúdo; acessado em (https://www.w3schools.com/jsref/event_preventdefault.asp)
 
-// função para acrescentar a classe selected, através do evento click, em um item. Com a classe, ocorrerá a alteração do background color do item selecionado.
-const createItem = document.querySelectorAll('li');
+// altera função anterior para acrescentar a classe selected em um único item que é removida quando clicado em outro, ao mesmo tempo que, através da classe, altera o background color.
+//Foi utilizada a propriedade localName do DOM, localizada através de pesquisa no https://www.w3schools.com/xml/prop_element_localname.asp
 
-function selectItem (select) {
-    for (let index = 0; index < createItem.length; index += 1){
-        if (createItem[index] !== select.target) {
-            createItem[index].style.backgroundColor = 'white';
-        };
+const colorChange = document.getElementById('lista-tarefas');
+
+function changeBackgroundColor (event) {
+    const local = document.querySelector('.selected');
+    if (local!== null) {
+        local.classList.remove('selected');
+    }
+    if (event.target.localName === "li"){
+        event.target.classList.add('selected');
     };
-    select.target.style.backgroundColor = 'rgb(128,128,128)';
 }
-taskList.addEventListener('click', selectItem);
+
+colorChange.addEventListener('click', changeBackgroundColor)
