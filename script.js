a529f42684d0598553c3777d9de4b21546a787d0
duribeiro <eduardo.duduribeiro1@gmail.com>
2021-05-11 20:29:43 -0400

    Desafio 8 - Atribui a classe selected a apenas um item da lista

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const button = document.querySelector('#criar-tarefa');
 const listItems = document.querySelector('#lista-tarefas');
 const inputTask = document.querySelector('#texto-tarefa');
-// capturar a lista ordenada
 const ol = document.querySelector('#lista-tarefas');
 
 function addTaskList() {
@@ -16,10 +15,27 @@ function addTaskList() {
 }
 addTaskList();
 
+ol.addEventListener('click', (event) => {
+  // Capture o elemento que tenha a classe selected
+  const itemSelected = document.querySelector('.selected');
+  // se o elemento com a classe selected existir
+  if (itemSelected !== null) {
+    // remova a classe selected dele
+    itemSelected.classList.remove('selected');
+  }
+  // Se o elemento com a classe selected não existir, adione a classe selected ao elemento clicado.
+  event.target.classList.add('selected');
+});
+
 function paintTask() {
   ol.addEventListener('click', (event) => {
-    const clickedList = event.target.style;
-    clickedList.backgroundColor = 'rgb(128,128,128)';
+    console.log(event.target.className === 'selected');
+    if (event.target.className === 'selected') {
+      const clickedList = event.target;
+      clickedList.style.backgroundColor = 'rgb(128,128,128)';
+    }
   });
 }
 paintTask();
+
+// devo pintar só quem tem a classe selected
