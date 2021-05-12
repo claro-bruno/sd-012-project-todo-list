c6aafde7b29d140e510488fc0c849265e897d655
Dayvid Ruan <ruan12bbg@gmail.com>
2021-05-12 14:00:05 -0300

    Requisiot 7

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ let ol = document.querySelector('#lista-tarefas')
 function addTask() {
     const tarefa = document.createElement('li');
     tarefa.innerHTML = input.value;
+    tarefa.className = 'itemList'
     ol.appendChild(tarefa);
     input.value = '';
   }
   button.addEventListener('click', addTask);
+  
+ol.addEventListener('click', selectColor);
+  function selectColor(event) {
+  const seleciona = document.querySelector('.selected');
+   if (seleciona !== null) {
+    seleciona.classList.remove('selected'); 
+}
+    event.target.classList.add('selected');
+    
+}
