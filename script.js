c2271355baa0bbb38b4344d0df70b2a5b6e545fa
Camila Damásio <camiladamasioef@hotmail.com>
2021-05-12 13:38:26 -0300

    Adiciona exercicio 10

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+const ol = document.getElementById('lista-tarefas');
+const div = document.querySelector('div');
+
 window.onload = function () {
   const addTarefa = document.getElementById('criar-tarefa');
   addTarefa.addEventListener('click', (event) => {
     const input = document.getElementById('texto-tarefa');
-    console.log(input.value);
+    // console.log(input.value);
     const li = document.createElement('li');
     li.classList.add('itemList');
     li.className = 'tarefa';
-    const ol = document.getElementById('lista-tarefas');
     li.innerHTML = input.value;
     ol.appendChild(li);
     input.value = '';
@@ -26,16 +28,25 @@ document.addEventListener('click', (event) => {
 });
 
 function riskList() {
-  document.addEventListener('dblclick', (event) => {
+  ol.addEventListener('dblclick', (event) => {
     event.target.classList.toggle('completed');
   });
 }
 riskList();
 
 //10
-// document.addEventListener('click', (event) => {  
-//   const limparTarefas = document.getElementById('apaga-tudo');
-//     if(li !== '') {
-//       console.log(li === '')
-//     }
-// });
+// Adicione um botão com id="apaga-tudo" que quando clicado deve apagar todos os itens da lista
+
+// Capturar o botão;
+const clearButton = document.querySelector('#apaga-tudo');
+
+//Criar um escutador de eventos para o botão;
+clearButton.addEventListener('click', (event) => {
+
+  //Quando clicado, remover elementos filhos da ol;
+  //Capturar os elementos filhos da ol;
+  const olChildrens = document.querySelector('#lista-tarefas');
+  while (olChildrens.hasChildNodes()) {
+    olChildrens.removeChild(olChildrens.firstChild);
+  }
+});
