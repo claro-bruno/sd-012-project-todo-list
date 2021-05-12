bbd8d3ace4359044cf89d3a75a3b67340fded623
Martin <escorpmartin97@gmail.com>
2021-05-12 19:16:36 -0300

    adiciona li

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-let section = document.querySelector('#tarefas')
+let section = document.querySelector('#tarefas');
+let button = document.querySelector('#criar-tarefa');
+let inputT = document.querySelector('#texto-tarefa')
+let olTarefa = document.querySelector('#lista-tarefas');
+
 
 function lista() {
-    let list = document.createElement('ol');
-    list.id = 'lista-tarefas';
-    section.appendChild(list);
+  let list = document.createElement('ol');
+  list.id = 'lista-tarefas';
+  section.appendChild(list);
 }
 lista()
+
+function addTarefa(event) {
+  let li = document.createElement('li');
+  li.innerText = inputT.value;
+  inputT.value = '';
+  olTarefa.appendChild(li);
+}
+
+ addTarefa()
+function listButton() {
+    button.addEventListener('click', addTarefa);
+  }
+  
