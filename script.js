95efdf75cdf4b96135b0ee4e1205f06b8a004072
LuctReis <luctreis@gmail.com>
2021-05-12 15:41:58 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -14,3 +14,46 @@ function criarTarefa () {
       }
 }
 addTaskButton.addEventListener('click', criarTarefa);
+
+document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('task-item')) {
+      clickedTask(event);
+    };
+    if (event.target.id === 'apaga-tudo') {
+      deleteTasks();
+    }
+    if (event.target.id === 'remover-finalizados') {
+      removeCompletedTasks();
+    }
+    if (event.target.id === 'salvar-tarefas') {
+      saveTasksLS();
+    }
+    if (event.target.id === 'mover-cima') {
+      upper();
+    }
+    if (event.target.id === 'mover-baixo') {
+      downer();
+    }
+    if (event.target.id === 'remover-selecionado') {
+      removeTaskSelected();
+    }
+  })
+
+  document.addEventListener('dblclick', function (event) {
+    if (event.target.classList.contains('task-item')) {
+      completedTasks(event);
+    }
+  })
+
+  function clickedTask(event) {
+    const tasks = document.getElementsByClassName('task-item');
+    const greyTask = event.target;
+    for (let i = 0; i < tasks.length; i += 1) {
+      tasks[i].classList.remove('selected');    
+    }
+    if (greyTask.classList.contains('selected')) {
+      greyTask.classList.add('selected');
+    } else {
+      greyTask.classList.add('selected');
+    }
+  }
