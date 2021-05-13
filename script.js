e084805b61e030331ae138d1bceda4b4be08ad40
Martin <escorpmartin97@gmail.com>
2021-05-13 20:28:24 -0300

    add ex8

--- script.js
+++ script.js
@@ -14,13 +14,19 @@ btn.addEventListener('click', function () {
     let olTarefa = document.querySelector('#lista-tarefas')
     let inputT = document.querySelector('#texto-tarefa');
   let li = document.createElement('li');
+  li.className = 'li';
     olTarefa.appendChild(li);
   li.innerText = inputT.value;
     inputT.value = '';
-
-li.addEventListener('click', selecao)
+li.addEventListener('click', cor)
 });
 
-function selecao (event) {
-event.target.style.backgroundColor = 'rgb(128,128,128)'
+function cor (event) {
+    let selectClass = document.querySelectorAll('.li');
+    for (let index = 0; index < selectClass.length; index += 1) {
+        selectClass[index].style.backgroundColor = 'white';
+
+    }
+event.target.style.backgroundColor = 'rgb(128,128,128)';
 }
+
