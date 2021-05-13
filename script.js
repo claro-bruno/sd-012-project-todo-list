cae700ac7123f1d1733402576cb311d27c8c5a0b
Augusto <augustocm2002@yahoo.com.br>
2021-05-13 11:59:54 -0300

    Resolvendo requisito 8

--- script.js
+++ script.js
@@ -8,48 +8,67 @@ let buttonText = document.querySelector('#criar-tarefa');
 function addItem (event) {
     
  
-    if (createTask.value != "") {
+    // if (createTask.value != "") {
         let newLi = document.createElement('li');
         newLi.innerHTML = document.querySelector('#texto-tarefa').value;
         listOl.appendChild(newLi);
-        newLi.className = 'lista';
+        // newLi.className = 'lista';
+
+        createTask.value = "";
+    }
+        buttonText.addEventListener('click', addItem)
     
     
 function changeColor (event){
  
-    let grey = document.querySelector('.grey');
-   
-  
+    let newClass = document.getElementsByTagName('li');
+
+    for(index = 0; index<newClass.length; index += 1){
+
+        newClass[index].classList.remove('grey');
+
+    }
+
+    // let grey = document.querySelector('.grey');
         event.target.classList.add('grey');
+     
         // grey.classList.remove('grey');
 }
-    
-    let allList = document.querySelector('.lista');
-
-    allList.addEventListener('click', changeColor)
+    listOl.addEventListener('click', changeColor)
 
   
-}
-    //Apaga campo de texto
-    createTask.value = "";
-}
-    buttonText.addEventListener('click', addItem)
 
+    //Apaga campo de texto
+  
 
 //Tarefa 7
 
     let allList = document.querySelector('.lista');
     let greyColor = 'rgb(128, 128, 128)';
-    let resetColor = 'white'
+    // let resetColor = 'white'
     let grey = document.querySelector('.grey');
 
+//Tarefa 09
 
+//  function styleList (event) {
+
+// let list = [];
+// for (index = 0; index <list.length; index += 1){
+//     let saveList = list [index] ;
+//     let styleList = document.querySelector('#texto-tarefa')
+//     event.target.classList.add ('completed') 
+//     document.getElementsByClassName('completed').style.textDecoration = "line-through";
+    
+// }
+
+//  }
+//  allList.addEventListener('dblclick', styleList)
 
 //Tarefa 10
     let deleteButton = document.querySelector('#apaga-tudo');
 
 function deleteList () {
-    
+
     listOl.innerHTML = "";
 
     }
