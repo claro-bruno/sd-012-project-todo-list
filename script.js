93a8fc007e85acfa36670506d9be5126165185fd
Igor.Duarte <igorhdsouza@gmail.com>
2021-05-13 10:16:39 -0300

    Requisito 05

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 // Requisito 02 
-let funcionamento = document.createElement('p');
+const funcionamento = document.createElement('p');
 funcionamento.setAttribute('id', 'funcionamento');
 document.body.appendChild(funcionamento);
-funcionamento.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo'
+funcionamento.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
 
 //Requisito 03
-let textoTarefa = document.createElement('input');
+const textoTarefa = document.createElement('input');
 textoTarefa.setAttribute('id', 'texto-tarefa');
 document.body.appendChild(textoTarefa);
 
 //Requisito 04
-let listaTarefas = document.createElement('ol');
+const listaTarefas = document.createElement('ol');
 listaTarefas.setAttribute('id', 'lista-tarefas');
 document.body.appendChild(listaTarefas);
+
+//Requisito 05
+const criarTarefa = document.createElement('button');
+criarTarefa.setAttribute('id', 'criar-tarefa');
+document.body.appendChild(criarTarefa);
+criarTarefa.innerText = 'Criar';
+
+criarTarefa.addEventListener('click', function () {
+    let listItem = document.createElement('li');
+    listaTarefas.appendChild(listItem);
+    listItem.innerText = textoTarefa.value;
+    textoTarefa.value = '';
+    console.log(listaTarefas.innerText);
+})
+
