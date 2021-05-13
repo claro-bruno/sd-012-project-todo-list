87f42cf840c2dad69b4ddcf3b5595de61f8a1a9f
AndZoni <lorenzoniandre@outlook.com>
2021-05-13 13:16:59 +0100

    Adiciona Requisitos 9, 10 e 11

--- script.js
+++ script.js
@@ -42,3 +42,48 @@ function selectTarefa() {
 }
 
 selectTarefa();
+// https://www.w3schools.com/jsref/prop_style_textdecoration.asp
+function riscadoStyle() {
+  const tarefaRiscada = document.querySelectorAll('.tarefa');
+  for (let index = 0; index < tarefaRiscada.length; index += 1) {
+    if (tarefaRiscada[index].classList.contains('completed')) {
+      tarefaRiscada[index].style.textDecoration = 'line-through solid black';
+    } else {
+      tarefaRiscada[index].style.textDecoration = '';
+    }
+  }
+}
+
+riscadoStyle();
+
+function riscarTarefa() {
+  list.addEventListener('dblclick', (event) => {
+    event.target.classList.toggle('completed');
+    riscadoStyle();
+  });
+}
+
+riscarTarefa();
+
+const limparButton = document.querySelector('#apaga-tudo');
+
+function limparTarefa() {
+  limparButton.addEventListener('click', () => {
+    const tarefasNaLista = document.querySelector('#lista-tarefas');
+    while (tarefasNaLista.hasChildNodes()) {
+      tarefasNaLista.removeChild(tarefasNaLista.firstChild);
+    }
+  });
+}
+
+limparTarefa();
+
+const removerButton = document.querySelector('#remover-finalizados');
+
+removerButton.addEventListener('click', () => {
+  const listFinished = document.querySelectorAll('.completed');
+
+  for (let index = 0; index < listFinished.length; index += 1) {
+    listFinished[index].parentNode.removeChild(listFinished[index]);
+  }
+});
