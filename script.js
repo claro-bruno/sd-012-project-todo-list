63df2cd5b7cfe095f8d87fca9825b862648b895c
Alexander <alexanderteodorocastro95@hotmail.com>
2021-05-13 19:52:37 -0300

    teste5

--- script.js
+++ script.js
@@ -6,25 +6,23 @@
 const botao = document.getElementById('criar-tarefa');
 
 function aperte() {
-    const olist = document.getElementById('lista-tarefas');
-    let valorInput = document.getElementById('texto-tarefa');
-    let novoItem = document.createElement('li')
-    novoItem.className = 'item-list';
-    novoItem.innerText = valorInput.value
-    if (valorInput.value !== '') {
-        olist.appendChild(novoItem);
-        valorInput.value = '';
-    }
-
-        let itemDaLista = document.querySelectorAll('.item-list');
-        for (let index = 0; index < itemDaLista.length; index += 1) {
-            itemDaLista[index].addEventListener, function (event) {
-                event.target.classList.add('cinza');
-                if(event.target.ClassName === 'cinza'){
-                    
+  const olist = document.getElementById('lista-tarefas');
+  let valorInput = document.getElementById('texto-tarefa');
+  let novoItem = document.createElement('li');
+  novoItem.className = 'item-list';
+  novoItem.innerText = valorInput.value;
+  if (valorInput.value !== '') {
+    olist.appendChild(novoItem);
+    valorInput.value = '';
+  }
+}
+  botao.addEventListener('click', aperte);
+/*
+        let itemDaLista = document.querySelectorAll('#lista-tarefa');
+            itemDaLista[index].addEventListener('click', function (event) {
+                for (let index = 0; index < itemDaLista.length; index += 1) {
+                    if (event.target.ClassName !== 'cinza'){
+                    event.target.classList.add('cinza')
+                    }
                 }
-            }
-        };
-    }
-
-botao.addEventListener('click', aperte) 
+            }) */
