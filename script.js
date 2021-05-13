a05fb34c0438120829385dbd56af3ba546ef7aa9
Bruno <brunocostah@hotmail.com>
2021-05-13 01:30:05 -0300

    Requisito 3 a 9 completo

---
+++ script.js
@@ -0,0 +1,42 @@
+
+const listTask = document.querySelector('#lista-tarefas');
+const textTask = document.getElementById('texto-tarefa');
+const buttonTask = document.querySelector('#criar-tarefa');
+
+function createTask() {
+    buttonTask.addEventListener('click', () => {
+    if(textTask.value.length > 0) {
+    let addTask = document.createElement('li');
+    addTask.innerText = textTask.value;
+    addTask.addEventListener('click', createTask);
+    textTask.value = '';
+    listTask.appendChild(addTask);
+        }
+    });
+};
+
+createTask()
+
+function clickItemList(){
+    listTask.addEventListener('click', (event) => {
+       const selector = document.querySelector('.selector');
+       if (selector !== null) {
+           selector.classList.remove('selector');
+           selector.removeAttribute('style');
+        }
+        event.target.classList.add('selector');
+    });
+};
+clickItemList()
+
+function completeTask(){
+listTask.addEventListener('dblclick', (event) =>{
+    if(event.target.classList.contains('completed')) {
+        event.target.classList.remove('completed');
+    }
+    else {
+        event.target.classList.add('completed');
+    }
+});
+};
+completeTask()
