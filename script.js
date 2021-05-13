ac9301480171215beb1d21a2580e61b3f1c7b4d9
gscuri <gscuri6@hotmail.com>
2021-05-13 02:06:44 -0300

    resolucao exercicio 5 e 6

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-function addToDoList() {
-  let tarefa = document.getElementById('texto-tarefa');
+let task = document.querySelector('#texto-tarefa').value;
+let todolist = document.querySelector('#lista-tarefas');
+let buttonAdd = document.querySelector('#criar-tarefa');
+let resetButton = document.querySelector('#apaga-tudo');
+
+// funcao para botao adicionar lista
+function makeAList (){
+
+  let task = document.querySelector('#texto-tarefa').value;
+  let text = document.createTextNode(task);
+  let list = document.createElement('li');
+  list.appendChild(text);
+  todolist.appendChild(list);
+  document.querySelector('#texto-tarefa').value = ''
 }
+
+buttonAdd.addEventListener('click',makeAList)
+
+// function removeText () {
+// task.value = ''
+// }
+// buttonAdd.addEventListener('click',removeText)
+// function addToDoList() {
+//   let list = document.createElement('li');
+//   list.innerHTML = task.value;
+//   todolist.appendChild(li);
+//   task.value = '';
+// }
+
+// buttonAdd.addEventListener('click', addToDoList);
