c237c4e55556261c322009fd7a84191232a97034
Mateus <mateusbgonzaga@gmail.com>
2021-05-13 13:37:41 -0300

    fifth requirement done, added js function

---
+++ script.js
@@ -0,0 +1,8 @@
+function addTask(){
+    let newTask = document.createElement('li');
+    newTask.innerHTML = document.querySelector('#text-tarefa').value;
+    document.querySelector('#texto-tarefa').value = '';
+    newTask.addEventListener('click', changeColor);
+    newTask.addEventListener('dblclick', completedTask);
+    document.querySelector('#lista-tarefas').appendChild(newTask);
+}
