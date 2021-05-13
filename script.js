1934afc2b06633eff5551e2ebebd22f49c3f8a1c
igor <igorsife80@gmail.com>
2021-05-13 16:55:16 -0300

    adiciona os botoes limpar lista, mover acima, mover abaixo

--- script.js
+++ script.js
@@ -5,55 +5,110 @@ let aBaixo = document.getElementById('mover-baixo');
 let limpaComp = document.getElementById('remover-finalizados');
 let salvaLis = document.getElementById('salvar-tarefas');
 let removeSel = document.getElementById('remover-selecionado');
-//ate aqui caapturei todos os elementos html dos botões <3
+//ate aqui caapturei todos os elementos html dos botÃµes <3
 let caixaInput = document.getElementById('texto-tarefa');
-let listarray = document.getElementById('lista-tarefas');
+let ornedlist = document.getElementById('lista-tarefas');
 //aqu capturei os elementos html da caixa de input e do arrey que iremos criar com ela <3
 
 function addlist() {
   let cria = document.createElement('li')
   cria.innerHTML = caixaInput.value;
   cria.className = 'cria'
-  listarray.appendChild(cria) // elemento_pai.appendchild(elemento_filho)
-  return cria
-  //aqui criamos uma variavel 'cria' para criamos as tags <li> dentro das nossas ul
+  ornedlist.appendChild(cria)
+
+  cria.addEventListener('click', seleciona);
 };
+
+function seleciona (event) {
+    removeSelect()
+    event.target.classList.add("selected");
+}
+
+function removeSelect() {
+    const selecionados = document.getElementsByClassName('selected');
+    Object.keys(selecionados).forEach(key => {
+        selecionados[key].classList.remove('selected');
+    })
+}
+
+function moverCima() {
+    const selecionados = document.getElementsByClassName('cria');
+    let identifica = 0;
+
+    if (selecionados.length > 1) {
+        Object.keys(selecionados).forEach(element => {
+            if (selecionados[element].classList.contains('selected')) {
+                identifica = element
+            }
+        })
+        removeSelect()
+        if (identifica == 0) {
+            selecionados[selecionados.length - 1].classList.add("selected");
+        } else {
+            selecionados[--identifica].classList.add("selected");
+        }
+    }
+}
+aCima.addEventListener('click', moverConteudoCima);
+
+function moverConteudoCima() {
+    let selecionados = document.getElementsByClassName('cria');
+    let identifica = 0;
+    let paiLista = document.getElementById('lista-tarefas');
+    if (selecionados.length > 1) {
+        Object.keys(selecionados).forEach(element => {
+            if (selecionados[element].classList.contains('selected')) {
+                identifica = element
+            }
+        })
+
+        if (identifica > 0) {
+            paiLista.insertBefore(selecionados[identifica], selecionados[identifica - 1])
+        }
+    }
+}
+
+function moverConteudoBaixo() {
+    let selecionados = document.getElementsByClassName('cria');
+    let identifica = 0;
+    let paiLista = document.getElementById('lista-tarefas');
+    if (selecionados.length > 1) {
+        Object.keys(selecionados).forEach(element => {
+            if (selecionados[element].classList.contains('selected')) {
+                identifica = element
+            }
+        })
+
+        if (parseInt(identifica) < (selecionados.length - 1)) {
+            paiLista.insertBefore(selecionados[Number(identifica) + 1], selecionados[identifica])
+        }
+    }
+}
+
+aBaixo.addEventListener('click', moverConteudoBaixo);
 
 function limpacaixa() {
   caixaInput.value = '';
   return caixaInput
-  //nessa função atribuimos o valor '' para limpar a caixa apor clicar em adicionar 
 }
-Adicionar.addEventListener('click', addlist, );
+Adicionar.addEventListener('click', addlist);
 Adicionar.addEventListener('click', limpacaixa);
 //ate aqui cimprimos do requisito 1 ao 6 com exito <3
 
-function trocaofundico() {
-  let listItem = document.querySelectorAll('li'); //------------------------------------// aqui criamos uma variavel e atribuimos a ela a tag li com o query selector
-  for (let posicaoDoarray = 0; posicaoDoarray < listItem.length; posicaoDoarray += 1) {// enquanto o posição do array for menor que o tamanho  do meu arrey  index recebe o antigo valor dele +1
-    listItem[posicaoDoarray].addEventListener('click', (clicaste) => { //-------------// aqui a variavel (lis[posição do array])  recebe um evento cujo um click seguido de um parametro 
-      let xablau = (clicaste).target; //---------------------------------------------// aqui é onde nasce o xablau que entra como alvo do paramentro da nossa função
-      xablau.                   //----------------------// adicionamos um backgroud diferenciado para xablau 
-      xablau.style.borderradius = '10px' //----------------------------------------//aqui damos um trato na bordinha do xablau (viva ao xablau )
-    });
-  }
-};
-//----------------------------------------------------------------------------// oprigado por ler a fanfique do xablau ̣:roket:
 
 
-/*function removecolor() {
-  for (let index = 0; index < listItem.length; index++) {
-    if (listItem[index] == style.css.backgroundColor('rgb(128,128,128)')) {
-      listItem[index] = listItem.style.backgroundColor = 'white'
-    } 
-  }
-}
-listarray.addEventListener('click', trocaofundico, removecolor)*/
- 
-apagarGeralX.addEventListener('click',apagaTudo);{
-  function apagaTudo(){
-    while (listarray.firstChild) {
-      listarray.removeChild(listarray.firstChild)
+
+  function apagaTudo() {
+    while (ornedlist.firstChild) {
+      ornedlist.removeChild(ornedlist.firstChild)
     }
   }
-};
+  apagarGeralX.addEventListener('click', apagaTudo)
+  /*listarray.addEventListener('dblclick', () => {
+    let listItem = document.querySelectorAll('li');
+    for (let index = 0; index < listItem.length; index++) {
+      let font = listItem[index].addEventListener('dblclick');{
+         font.classList = "tarefacompleta"
+      }; 
+    };
+  });*/
