c958fb1caeabf532f7eb623bcc9d3213241aa236
TarcisioMoura <bigtharcisiomoura@gmail.com>
2021-05-13 14:15:03 -0300

    Resolve Requesito 8

--- script.js
+++ script.js
@@ -6,32 +6,37 @@ let listaTarefa = document.querySelector('#lista-tarefas');
 let listas = [];
 
 meuButao.addEventListener('click', criaTarefa);
+function criaTarefa(){
+   if ( registraTarefa() > 0){
+   let minhaListaTarefa = meuInput.value;
+   listas.push(minhaListaTarefa);
+   meuInput.value = null;
+   };
+   tarefa()
+};
+
+function registraTarefa(){
+   return meuInput.value.length;   
+};
+//resolve requesito 6
 function tarefa(){
-   listaTarefa.innerHTML = '';
+   listaTarefa.innerHTML = ''; //limpa os dados, e salva somente dados novos na minha let listas;
    for (lista of listas) {
       let criaLista = document.createElement('li');
       let adicionaLista = document.createTextNode(lista);
-      criaLista.addEventListener('click', mudacor)
-      function mudacor(){
-         criaLista.className = 'trocaCor';
-         trocaCorfundo();
-         function trocaCorfundo(){
-            let pTarefa = document.querySelectorAll('.trocaCor');
-            
-            console.log(pTarefa[0])
-         }
-      }
+      criaLista.className = 'listas'
       criaLista.appendChild(adicionaLista);
       listaTarefa.appendChild(criaLista);
    };
-   
+   //resolve requesito 8, auxiliado pela Mentorias técnicas (Gisele Santin) e o colega Euclides Alves(Mostrando esse problema no seu próprio código, onde conseguir tirar como exemplo pro meu)
+   let li = document.querySelectorAll('.listas');
+   for (index = 0; index < li.length; index +=1){
+      li[index].addEventListener('click', mudarCor)
+      function mudarCor(){
+         for ( index2 = 0; index2 < li.length; index2 += 1){
+           li[index2].style.background = 'red';
+         }
+         event.target.style.background = 'rgb(128, 128, 128)';
+      };
+   };
 };
-
-//resolve requesito 6
-function criaTarefa(){
-   let minhaListaTarefa = meuInput.value;
-   listas.push(minhaListaTarefa);
-   meuInput.value = '';
-   tarefa()
-}
-
