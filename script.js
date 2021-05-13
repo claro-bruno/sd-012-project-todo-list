9d9d2c753e3d5324e7062e886b2ab8ec4292f72c
Sérgio A. Barbosa <sergio.alvesbarbosa@gmail.com>
2021-05-13 18:59:05 -0300

    Resolve requisito 13

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function apagaItem(event) {
     event.preventDefault();
     listaTarefas.innerHTML = '';
     //localStorage.clear();
-    console.log('oi');
+   // console.log('oi');
    
 }
  botaoApagar.addEventListener('click', apagaItem);
@@ -82,3 +82,62 @@ window.onload = () => {
     listaTarefas.innerHTML = tarefasSalvas;//o conteúdo interno da lista de tarefas será atualizado com o conteúdo de tarefas-salvas.
     
 }
+
+//Requisito 13
+//Move para cima
+
+const botaoAcima = document.getElementById('mover-cima');
+function moverParaCima(){
+    let li = document.getElementsByTagName('li');
+        //verendo a lista de li's
+    for(let index = 0; index < li.length; index +=1){
+        //se a li for selecionada
+        if (li[index].classList.contains('selected') && li[index].previousSibling !== null) {
+            //tenha esse comportamento
+            li[index].parentNode.insertBefore(li[index], li[index].previousElementSibling);
+        
+    }
+
+}
+}
+botaoAcima.addEventListener('click', moverParaCima);
+
+//const botaoAbaixo = document.getElementById('mover-cima');
+
+
+//Move p/ cima02
+// const botaoAcima = document.getElementById('mover-cima');
+// function moverParaAcima(){
+//     let paiElement = document.getElementsByTagName('ol');
+//     let selAcima = document.querySelector('.selected');
+//     paiElement[0].insertBefore(selAcima, selAcima.previousSibling); 
+//     if(selAcima.lastIndexOf(searchElement)){
+        
+//     }
+// }
+// botaoAcima.addEventListener('click', moverParaAcima);
+
+//Move p/ baixo
+const botaoAbaixo = document.getElementById('mover-baixo');
+function moverParaBaixo(){
+    //atribui a variável li a tag li
+    let paiElement = document.getElementsByTagName('ol');
+    let selAbaixo = document.querySelector('.selected');
+    if(selAbaixo && selAbaixo.nextSibling){
+    paiElement[0].insertBefore(selAbaixo, selAbaixo.nextSibling.nextSibling);
+}
+}
+botaoAbaixo.addEventListener('click', moverParaBaixo);
+
+//Requisito 14 Remover selecionado
+
+// const botaoApagarSelecionado = document.getElementsById('remover-selecionado');
+// function deleteSelected(){
+//     let itemSelecionado = document.querySelectorAll('.selected');
+//     for(let index = 0; index < itemSelecionado.length; index += 1)
+//         if(itemSelecionado[index].classList.contains('completed') === true){
+//             itemSelecionado[index].remove();
+//     }
+   
+// }
+// botaoApagarSelecionado.addEventListener('click', deleteSelected);
