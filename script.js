2c750ec14eeccebac83a60c114e360a536d20ab7
martinsph <ph.mmartins@gmail.com>
2021-05-14 13:15:33 -0300

    adiciona req 10

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const taskList = document.getElementById("lista-tarefas");
 const criarTarefa = document.getElementById("criar-tarefa");
 const conteudoInput = document.getElementById("texto-tarefa");
 const li = document.querySelector(".listItem");
-const listElmnt = 'lista-tarefas';
 
 //criando item da lista
 function createListItem() {
@@ -28,25 +27,23 @@ function selectItem() {
 };
 selectItem();
 
-// function completeItem() {
-//     taskList.addEventListener("dblclick", function(event) {
-//         const completeItem = document.querySelector(".completed");
-//         if (completeItem != null) {
-//             completeItem.classList.remove("completed");
-//         }
-//         event.target.classList.add("completed");
-//     });
-// };
-// completeItem();
 function completedStatus() {
     taskList.addEventListener("dblclick", function(event) {
         if (event.target.id !== taskList) {
             if (event.target.classList.contains('completed')) {
-              event.target.classList.remove('completed');
+                event.target.classList.remove('completed');
             } else { 
                 event.target.classList.add('completed'); 
-        }
-          };
+            };
+        };
     });
 };
 completedStatus();
+
+function btnClearAll() {
+    const clearButton = document.getElementById("apaga-tudo");
+    clearButton.addEventListener("click", function() {
+        taskList.remove(li);
+    });
+};
+btnClearAll();
