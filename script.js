a2acdf2422519917abb9d692ec23bc075cdd0b43
Emmanuel <acquabriall2020@gmail.com>
2021-05-14 12:17:48 -0300

    resolve o 7

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 let getItemList = document.getElementById('texto-tarefa');
 let getList = document.getElementById('lista-tarefas');
-let getButton = document.getElementById('criar-tarefa');
+
 
 function saveInput(){
     let createLi = document.createElement('li');
     createLi.innerText = getItemList.value; 
+    createLi.className = 'list-item';
     getList.appendChild(createLi);
-    getItemList.value = "";
-}    
+    getItemList.value = '';
+    
+    let getListItems = document.querySelectorAll('.list-item');
+    for (let indexListItem = 0; indexListItem < getListItems.length; indexListItem += 1){
+        getListItems[indexListItem].addEventListener('click', function(){
+            getListItems[indexListItem].style.backgroundColor = 'rgb(128,128,128)';
+        })
+    }
+}
+
+
+
+
