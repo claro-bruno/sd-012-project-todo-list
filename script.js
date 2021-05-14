7d6e2ca3a2cf738b34236c2cc52bdb2690b7eb0f
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2021-05-14 12:28:45 -0300

    requisito 9 e 11

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 
 const botao = document.querySelector('#criar-tarefa');
 botao.addEventListener('click', addTarefa);
-        
+
 function addTarefa() {
     const input = document.querySelector('#texto-tarefa');
     const lista = document.querySelector('#lista-tarefas');
     let item = document.createElement('li');
+    item.addEventListener('dblclick', function(event) {
+        event.target.classList.toggle('completed');
+    });
+
     item.className = 'tarefa';  
     lista.appendChild(item);
     item.innerText = input.value;
-    input.value = "";
-            
+    input.value = "";     
     const itens = document.querySelectorAll('.tarefa');
     for (let i = 0; i < itens.length; i += 1) {
         itens[i].addEventListener('click', addCinza);
@@ -22,19 +25,25 @@ function addTarefa() {
             }
             itens[i].className += ' select';
         }
-
+        
     }
-    for (let i = 0; i < itens.length; i += 1) {
-        itens[i].addEventListener('dblclick', riscaTarefa);
-        function riscaTarefa() {
-            itens[i].classList.toggle('completed');
-        }
-    }
-    const limpar = document.querySelector('#apaga-tudo');
-    limpar.addEventListener('click', apagaTudo); 
-    function apagaTudo() {
-        const lista = document.querySelector('#lista-tarefas'); 
-        lista.innerHTML = '';
-        }
 }
+const limpar = document.querySelector('#apaga-tudo');
+limpar.addEventListener('click', apagaTudo); 
+function apagaTudo() {
+    const lista = document.querySelector('#lista-tarefas'); 
+    lista.innerHTML = '';
+}     
+
+const remover = document.querySelector('#remover-finalizados');
+remover.addEventListener('click', removerFinalizadas) 
+function removerFinalizadas() {
+    const feito = document.querySelectorAll('.completed');
+    for (let i = 0; i < feito.length; i += 1) {
+        const lista = document.querySelector('#lista-tarefas');
+        lista.removeChild(feito[i]);
+    } 
+}    
+
+
 
