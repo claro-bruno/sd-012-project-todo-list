c0ea1edfc55aba56ef5847642906a73bee4220fd
Gutemberg <gttj2010@hotmail.com>
2021-05-14 11:33:23 -0300

    Requisitos 7, 8 e 9.

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ selectButton.addEventListener('click', function () {
   if (selectInput.value != 0) {
     const createTask = document.createElement('li');
     createTask.innerText = selectInput.value;
+    createTask.className = 'notSelected';
     selectInput.value = '';
     addOl.appendChild(createTask);
   } else {
@@ -17,11 +18,42 @@ selectButton.addEventListener('click', function () {
 });
 
 clearList.addEventListener('click', function () {
-    let ol = document.getElementsByTagName('ol')[0];
-    ol.innerText = "";
+  let ol = document.getElementsByTagName('ol')[0];
+  ol.innerText = '';
+});
+/*
+const paleta = document.getElementById('lista-tarefas');
+  for (let index = 0; index < paleta.length; index += 1) {
+    paleta[index].addEventListener('click', function(event) {      
+    let removeSelection = document.querySelector('.selected');
+    removeSelection.classList.remove('selected');
+    event.target.classList.add('selected');
+  });
+  
+}
+
+const li = document.querySelector('li');
+li.addEventListener('dblclick', () => {
+  const allLIs = document.getElementsByTagName(li);
+  for (let li of allLIs) {
+    li.classList.toggle('completed');
+  }
+});
+*/
+addOl.addEventListener('dblclick', function (event) {
+  if(!event.target.classList.contains("completed")){
+    event.target.classList.add("completed");
+  } else{
+    event.target.classList.remove("completed");
+  }
 });
 
-removeEnd.addEventListener('click', function () {
-    let ol = document.getElementsByTagName('ol')[0];
-    ol.innerText = "";
+addOl.addEventListener('click', function (event) {
+  let classe = document.getElementsByClassName("selected")[0];
+  if(classe){
+    classe.classList.remove("selected");
+    classe.style.backgroundColor = "";
+  }
+  event.target.classList.add("selected");   
+  event.target.style.backgroundColor = "rgb(128,128,128)"; 
 });
