f359e271cdc2335efc502fb93d5f52ef76ae6265
Joao Vieira <jonnyvieirausa@hotmail.com>
2021-05-14 05:08:26 -0300

    cria complete do item 9

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ let botao = document.getElementById('criar-tarefa');
 let apaga = document.getElementById('apaga-tudo');
 let texto = document.getElementById('texto-tarefa');
 
+function cinza(event){
+    let classe = document.getElementsByClassName("selecionado")[0];
+    if(classe){
+        classe.classList.remove("selecionado");
+        classe.style.backgroundColor = "";
+    }
+    event.target.classList.add("selecionado");
+    event.target.style.backgroundColor = "rgb(128,128,128)";
+
+}
