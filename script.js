e9aedf1d03b20d61411f0cc9609d2cf2509e1385
Joao Vieira <jonnyvieirausa@hotmail.com>
2021-05-14 04:58:59 -0300

    adiciona variaveis

---
+++ script.js
@@ -0,0 +1,5 @@
+let lista = document.getElementById('lista-terefa');
+let botao = document.getElementById('criar-tarefa');
+let apaga = document.getElementById('apaga-tudo');
+let texto = document.getElementById('texto-tarefa');
+
