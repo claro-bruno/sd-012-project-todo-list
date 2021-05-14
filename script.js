1aab7b7c0f487f9f88da62905004fe8f26f6aacf
Joao Vieira <jonnyvieirausa@hotmail.com>
2021-05-14 05:54:33 -0300

    resolve 6 7 8 e 9

--- script.js
+++ script.js
@@ -13,3 +13,41 @@ function cinza(event){
     event.target.style.backgroundColor = "rgb(128,128,128)";
 
 }
+
+function riscada(event){
+    if(!event.target.classList.contains("completed")){
+        event.target.classList.add("completed");
+    } else{
+        event.target.classList.remove("completed");
+    }
+}
+
+function criarLi(){
+    let tarefa = document.createElement("li");
+    tarefa.classList.add("item-lista");
+    tarefa.innerHTML = textoAdiciona.value;
+    document.getElementById("lista-tarefas").appendChild(tarefa);
+    tarefa.addEventListener("click", cinza);
+    tarefa.addEventListener("dblclick", riscada);
+};
+
+function limparInput(){
+    texto.value = "";
+};
+
+function apertaBotao(){
+    criarLi();
+    limparInput();    
+}
+
+botao.addEventListener("click", apertaBotao);
+
+function apagarLista(){
+    let listaPai = document.getElementById("lista-tarefas");
+    while(listaPai.firstChild){
+        listaPai.removeChild(listaPai.firstChild);
+    }
+}
+
+apaga.addEventListener("click",apagarLista);
+
