5553255c7bd4ad32893cf5e8b40ecb3d91c69770
Victor Veloso <victorveloso13@gmail.com>
2021-05-14 11:06:24 -0300

    Adiciona botoes

--- script.js
+++ script.js
@@ -7,9 +7,60 @@ function list () {
         let textTask = document.querySelector('#texto-tarefa').value;
         let listItem = document.createElement('li');
         listItem.innerHTML = textTask;
+        listItem.className = 'topics'
+        listItem.addEventListener('click', changeColor)
+        listItem,addEventListener('dblclick', listComplete)
         listTask.appendChild(listItem);
         textTask = document.querySelector('#texto-tarefa').value = '';
 
     })
 }
 list ();
+
+function changeColor (event){
+    let listItem = document.querySelectorAll('topics')
+    for(let index = 0; index < listItem.length; index += 1) {
+        if (listItem[index].id === 'selected')
+        listItem[index].id = ''
+        else
+        event.target.id = 'selected'
+    }
+}
+
+changeColor();
+
+
+function listComplete(event){
+
+
+    if(event.target.className.includes('completed'))
+      event.target.className = 'item-lista'
+    else
+    event.target.className  = ' completed'
+  }
+
+//Botão apagar
+  const clear = document.querySelector('#apaga-tudo');
+  function clearAll() {
+    clear.addEventListener('click', () => {
+      listTask.innerHTML = '';
+    });
+  }
+  clearAll();
+  
+
+  //Botão Remover finalizados
+  const clearEnd = document.querySelector('#remover-finalizados');
+  const completed = document.getElementsByClassName('completed');
+  function deleteEnd() {
+    clearEnd.addEventListener('click', () => {
+      console.log('complete');
+      for (let index = completed.length - 1; index >= 0; index -= 1) {
+        completed[index].remove();
+      }
+    })
+  }
+  deleteEnd();
+ 
+
+
