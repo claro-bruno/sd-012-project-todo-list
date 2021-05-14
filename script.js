064d8f0418debb34652bdb0cf58679f73b94f599
Alexander <alexanderteodorocastro95@hotmail.com>
2021-05-14 11:40:16 -0300

    requisito 6/7/8/9

--- script.js
+++ script.js
@@ -11,31 +11,30 @@ function aperte() {
   let novoItem = document.createElement('li');
   novoItem.className = 'item-list';
   novoItem.innerText = valorInput.value;
+  novoItem.addEventListener('click', corCinza)
+  novoItem.addEventListener('dblclick', doubleClick)
   if (valorInput.value !== '') {
     olist.appendChild(novoItem);
     valorInput.value = '';
-  
+    
   }
 }
 
-/*   }
-    let listaDoida = document.getElementsByClassName('item-list')
-      listaDoida.sort(a - b);
-      for (let index = 0; index < ListaDoida.length; index += 1) {
-        listaDoida[index]
-      }
-
-} */
-
 botao.addEventListener('click', aperte);
 
-/* function corcinza() {
-let itemDaLista = document.querySelectorAll('#lista-tarefa');
-for (let index = 0; index < itemDaLista.length; index += 1) {
-itemDaLista[index].addEventListener('click', function (event) {
-event.target.style.backgroundColor = 'rgb(128,128,128)';
+function corCinza(event) {
+    let valor = document.querySelectorAll('.item-list')
+    for (let index = 0; index < valor.length; index +=1){
+        valor[index].classList.remove('cinza')
+    }
+    event.target.classList.add('cinza');
+  }
 
-})
+function doubleClick (event){
+  if (event.target.classList.contains('completed')){
+      event.target.classList.remove('completed')
+  }else {
+    event.target.classList.add('completed')    
+  }
+
 }
-}
-itemDaLista.addEventListener('click', corcinza); */
