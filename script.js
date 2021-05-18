3123f249478db1ed6cfe632e6729a77b44ac255a
Emmanuel <acquabriall2020@gmail.com>
2021-05-18 09:22:48 -0300

    resolve o req 8

--- script.js
+++ script.js
@@ -8,15 +8,29 @@ function saveInput(){
     createLi.className = 'list-item';
     getList.appendChild(createLi);
     getItemList.value = '';
+    createLi.addEventListener('click', function(event){
+        let pegaElemento = document.querySelector('.selected');
+
+        if (pegaElemento === null) {
+            event.target.classList.add('selected');
+        } else {
+            pegaElemento.classList.remove('selected');
+            event.target.classList.add('selected');
+        }
+    })
+
+    // Capturar elemento com  a classe selected
+    // Check elemento existe (if)
+    // remove classe selected do item selecionado
+    // adiciona classe selected no item clicado 
     
-    let getListItems = document.querySelectorAll('.list-item');
-    for (let indexListItem = 0; indexListItem < getListItems.length; indexListItem += 1){
-        getListItems[indexListItem].addEventListener('click', function(){
-            getListItems[indexListItem].style.backgroundColor = 'rgb(128,128,128)';
+    /*let getListItems = document.getElementsByClassName('list-item');
+    for (let getIndexList = 0; getIndexList < getListItems.length; getIndexList += 1) {
+        getListItems[getIndexList].addEventListener('click', function(){
+            getListItems[getIndexList].classList.add('selected');
         })
-    }
+    }*/
+
+
+
 }
-
-
-
-
