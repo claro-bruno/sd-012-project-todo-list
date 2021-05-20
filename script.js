b15d425ac0fea23398843c6e169071ad29bd0507
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-20 13:01:09 -0300

    requisito 5- button

--- script.js
+++ script.js
@@ -6,21 +6,20 @@ let list = document.getElementById('lista-tarefas');
 
 
 function bottonAdd () {
-  addButton.addEventListener('click', function() {
-  let newTask = document.createElement('li');
+addButton.addEventListener('click', function() {
+let newTask = document.createElement('li');
   
-  newTask.innerHTML = textoTarefa.value;
-  list.appendChild(newTask);
-    
-  textoTarefa.value  = " ";
+newTask.innerHTML = textoTarefa.value;
+list.appendChild(newTask);
+textoTarefa.value  = " ";
       
 });
 }
 bottonAdd ();
 
 
-  list.addEventListener('click', function(event) {
-    let target = event.target;
+list.addEventListener('click', function(event) {
+  let target = event.target;
     target.classList.toggle('selected');
  });
 
