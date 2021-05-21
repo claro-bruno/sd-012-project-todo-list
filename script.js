2ed22c68b3d44d777757f4512ec805f753c84486
Emerson Ovidio <emersonovidiosantos@gmail.com>
2021-05-20 23:24:46 -0300

    revisa o 5

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-const addButton = document.getElementById('criar-tarefa');
+const button = document.getElementById('criar-tarefa');
 const textoTarefa = document.getElementById('texto-tarefa');
 const list = document.getElementById('lista-tarefas');
 
 
 
-//requisito 5
-function bottonAdd () {
-addButton.addEventListener('click', function() {
-const newTask = document.createElement('li'); 
+function buttonAdd () {
+button.addEventListener('click', () => {         //inicia addEventlistener + click    
+const newTask = document.createElement('li');  // declara novaTarefa filho de li
   
-newTask.innerHTML = textoTarefa.value;  
-list.appendChild(newTask);    
-textoTarefa.value  = " ";     
-      
+newTask.innerHTML = textoTarefa.value;  // insere o valor de textoTarefa em newTask
+list.appendChild(newTask);     // cria lista como filho 
+textoTarefa.value  = " ";     // o input do textoTarefa é "zerado"
+     
 });
 }
-bottonAdd ();
+buttonAdd();                       // a funcao é invocada - "invocada mesmo"
+                                  // destaco o apoio e contribuições da colega Alice Goncalves.
 
 
-list.addEventListener('click', function(event) {
-  const target = event.target;
-    target.classList.toggle('selected');
 
+list.addEventListener('click', function(event) {           //funcao seleciona elemento 
+  const target = event.target;                  // const target recebe event.target
+    target.classList.toggle('selected'); 
+          
  });
 
 
