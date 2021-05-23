2b3529497d9ed3f1059cdb32d1a41cc568e43817
Pedro Pinheiro <ph.santos.pinheiro@hotmail.com>
2021-05-23 12:58:53 -0300

    requisito 7

--- script.js
+++ script.js
@@ -2,11 +2,21 @@ let criarTarefa = document.getElementById('criar-tarefa');
 let listaTarefas = document.getElementById('lista-tarefas'); 
 let textoTarefa = document.getElementById('texto-tarefa');
 
-criarTarefa.addEventListener('click', () => {;
+
+criarTarefa.addEventListener('click', criandoLi) 
+
+function criandoLi (){
 let listaLi = document.createElement('li')
 listaLi.innerHTML = textoTarefa.value
 listaTarefas.appendChild(listaLi)
 textoTarefa.value = ''
-})    
+
+listaLi.addEventListener('click', colocandoCor)
+function colocandoCor (){
+    listaLi.style.backgroundColor = 'rgb(128, 128, 128)'
+}
+}
+
+
 
 
