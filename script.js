8c13da38d375dee397981a8dec9c316974bb5946
Cassio Mascarenhas <csmlvx@gmail.com>
2021-05-23 13:43:16 -0300

    corrigindo requisito 5

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
 function addTarefa(){
-   
+
     const text = document.getElementById('texto-tarefa');
     const listaTarefas = document.getElementById('lista-tarefas');
     const tagLi = document.createElement('li')
 
+// Item inspirado na página https://www.fabiobmed.com.br/site/verificar-se-um-campo-do-formulario-esta-vazio-via-javascript/
     if (text.value === '') {
         alert('Por favor, preencha o campo nome');
         text.focus();
@@ -13,4 +14,5 @@ function addTarefa(){
    tagLi.innerHTML = text.value
 
    listaTarefas.appendChild(tagLi);
+   text.value = '';
 }
