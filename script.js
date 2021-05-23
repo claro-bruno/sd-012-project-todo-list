bcc713080760e7be2621274d2b63905a9fd6af90
Gabriela-GUerra <gabrielaguerra.arq@gmail.com>
2021-05-23 18:31:49 -0300

    adiciona requisitos 5 e 6

---
+++ script.js
@@ -0,0 +1,21 @@
+const elementoLista = document.querySelector('#lista-tarefas');
+const elementoInput = document.querySelector('#texto-tarefa');
+const elementoBotao = document.querySelector('#criar-tarefa');
+const tarefas = [];
+function mostraTarefas (){
+  elementoLista.innerHTML = '';
+  for(tarefa of tarefas){
+    const elementoTarefa = document.createElement('li');
+    const textoTarefa = document.createTextNode(tarefa);
+    elementoTarefa.appendChild(textoTarefa);
+    elementoLista.appendChild(elementoTarefa)
+  }
+}
+mostraTarefas()
+function addTarefa(){
+  const textoTarefa = elementoInput.value;
+  tarefas.push(textoTarefa);
+  elementoInput.value = '';
+  mostraTarefas();
+}
+elementoBotao.setAttribute('onclick', 'addTarefa()');
