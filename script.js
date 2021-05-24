6f851adf4b35afd3425f50563ace67171ec6ec73
Gabriela-GUerra <gabrielaguerra.arq@gmail.com>
2021-05-24 18:40:07 -0300

    adiciona requisitos 8

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
 const elementoLista = document.querySelector('#lista-tarefas');
 const elementoInput = document.querySelector('#texto-tarefa');
 const elementoBotao = document.querySelector('#criar-tarefa');
+
 elementoBotao.addEventListener('click', function(){
   const criaLi = document.createElement('li');
+  criaLista.className = 'completed';
   criaLi.innerText = elementoInput.value;
   elementoLista.appendChild(criaLi);
   elementoInput.value = '';
-  criaLi.addEventListener('click', function(){
-    criaLi.style.backgroundColor = 'rgb(128, 128, 128)';
+
+criaLi.addEventListener('click', function(event){
+  const lista = document.querySelectorAll('li');
+  for (let index = 0; index < lista.length; index +=1) {
+    if (lista[index].style.backgroundColor === 'rgb(128, 128, 128)');
+    lista[index].style.backgroundColor = '';
+  };
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
   })
 });
 
 
 
+// // requisito 10
+// let todosItens = document.getElementsByClassName('tarefas');
 
-
-
-// requisito 9
-function duploClick() {
-
-  let clique = document.getElementById("click-risca");
-  let riscarItem = document.getElementsByClassName("completed");
-    
-  clique.onclick = function(event) {
-  event.preventDefault();
-  style.getElementsByClassName('completed'); 
- };
- 
-;}
-
+// function apagarTodoItem() {
+//     let apagarTarefas = document.getElementById('apagar-tudo');
+//     apagarTarefas.addEventListener('click', function (event) {
+//       let listaTarefas = document.getElementById('lista-tarefas');
+//       let todosItens = document.getElementsByClassName('tarefas');
+//         for (let indice = 0; indice < todosItens.length; indice += 1) {
+//         tarefas.removeChild(todosItens[indice]);
+//       }
+//     });
+// }
