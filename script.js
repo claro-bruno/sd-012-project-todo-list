79da49a41fa1750c35ca88c81f9b6b5b318798e4
Wadson <wadson.dev92@gmail.com>
2021-05-24 12:29:12 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ const elementoLista = document.querySelector('#lista-tarefas');
 const elementoInput = document.querySelector('#texto-tarefa');
 const elementoBotao = document.querySelector('#criar-tarefa');
 
-elementoBotao.addEventListener('click', function(){
+elementoBotao.addEventListener('click', function () {
   const criaLi = document.createElement('li');
   criaLi.innerText = elementoInput.value;
   elementoLista.appendChild(criaLi);
   elementoInput.value = '';
-  criaLi.addEventListener('click', function(){
-    criaLi.style.backgroundColor = 'rgb(128, 128, 128)';
+
+  criaLi.addEventListener('click', function (event) {
+    
+    const listaItens = document.querySelectorAll('li');
+    for(let index = 0; index < listaItens.length; index += 1){
+      if(listaItens[index].style.backgroundColor === 'rgb(128, 128, 128)'){
+        listaItens[index].style.backgroundColor = '';
+        console.log('oi');
+      }
+    }
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    console.log('ooooii');
   })
 });
