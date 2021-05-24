424ffecbe5225d973124cfe7c57838b83bd625d8
Wadson <wadson.dev92@gmail.com>
2021-05-24 20:49:11 -0300

    requisito 9

--- script.js
+++ script.js
@@ -9,15 +9,28 @@ elementoBotao.addEventListener('click', function () {
   elementoInput.value = '';
 
   criaLi.addEventListener('click', function (event) {
-    
     const listaItens = document.querySelectorAll('li');
     for(let index = 0; index < listaItens.length; index += 1){
       if(listaItens[index].style.backgroundColor === 'rgb(128, 128, 128)'){
         listaItens[index].style.backgroundColor = '';
-        console.log('oi');
+        
       }
     }
     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-    console.log('ooooii');
+    
+  })
+
+  criaLi.addEventListener('dblclick', function (event) {
+    const listaItens = document.querySelectorAll('li');
+    for(let index = 0; index < listaItens.length; index += 1){
+      if(listaItens[index].className === 'completed'){
+        listaItens[index].className = '';
+        console.log('oi');
+      }
+    }
+    event.target.className = 'completed';
+    console.log('Duplo Clique')
   })
 });
+
+
