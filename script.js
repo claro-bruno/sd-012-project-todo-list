bd581cdb7bfc2c10949dfe68e3fcf13d82ed9861
Aldria Melo <aldriamelo.academico@gmail.com>
2021-05-23 21:48:47 -0300

    adiciona requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 function adicionaLista() {
-let criaTarefa = document.getElementById('texto-tarefa').value;
-let lista = document.getElementById('lista-tarefas').innerHTML;
+	let criaTarefa = document.getElementById('texto-tarefa').value;
+	let lista = document.getElementById('lista-tarefas').innerHTML;
 
-lista += "<li>" +criaTarefa+ "</li>"
-document.getElementById('lista-tarefas').innerHTML = lista;
-document.getElementById('texto-tarefa').value = null;
+	lista += '<li>' + criaTarefa + '</li>';
+	document.getElementById('lista-tarefas').innerHTML = lista;
+	document.getElementById('texto-tarefa').value = null;
 }
-// https://www.youtube.com/watch?v=pRwxgtqImZQ
+// feito com base https://www.youtube.com/watch?v=pRwxgtqImZQ
+
+let alteraCor = document.getElementById('lista-tarefas');
+alteraCor.addEventListener('click', mudaCor);
+function mudaCor(event){
+	let itemSelecionado = document.querySelectorAll('li')
+	for (index = 0; index < itemSelecionado.length; index +=1){
+		itemSelecionado[index].style.backgroundColor=""
+	}
+ event.target.style.backgroundColor="rgb(128, 128, 128)";
+}
