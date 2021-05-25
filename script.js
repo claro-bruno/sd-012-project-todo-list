84c7c020e6f2446eadd8be917de5b612d5677a33
Gabriela-GUerra <gabrielaguerra.arq@gmail.com>
2021-05-25 00:31:57 -0300

    adiciona requisitos 9 e 10

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const elementoLista = document.querySelector('#lista-tarefas');
 const elementoInput = document.querySelector('#texto-tarefa');
 const elementoBotao = document.querySelector('#criar-tarefa');
 
+
 elementoBotao.addEventListener('click', function() {
     const criaLi = document.createElement('li');
     criaLi.innerText = elementoInput.value;
@@ -17,16 +18,14 @@ elementoBotao.addEventListener('click', function() {
         event.target.style.backgroundColor = 'rgb(128, 128, 128)';
       };
     });
-  });
+ });
 
-criaLi.addEventListener('dblclick', function(event) {
-    const listaItens = document.querySelectorAll('li');
-    for (let index = 0; index < listaItens.length; index +=1) {
-      if (listaItens[index].className === 'completed') {
-      listaItens[index].className = '';
+ criaLi.addEventListener('dblclick', function(event) {
+    if (event.target.className === 'completed') {
+      event.target.className = '';
+    } else {
+        event.target.className = 'completed';
     };
-    }
-    event.target.className = 'completed';
 });
 
 const button = document.getElementById('apaga-tudo');
@@ -37,3 +36,4 @@ button.addEventListener('click', function () {
   console.log(lista[index])
   };
 });
+
