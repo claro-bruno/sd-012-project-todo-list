fcbc900b9366edcb4192a6e49f78235d7ba081f5
Gabriela-GUerra <gabrielaguerra.arq@gmail.com>
2021-05-24 23:50:43 -0300

    adiciona requisitos 9 e 10

--- script.js
+++ script.js
@@ -2,34 +2,38 @@ const elementoLista = document.querySelector('#lista-tarefas');
 const elementoInput = document.querySelector('#texto-tarefa');
 const elementoBotao = document.querySelector('#criar-tarefa');
 
-elementoBotao.addEventListener('click', function(){
-  const criaLi = document.createElement('li');
-  criaLi.innerText = elementoInput.value;
-  elementoLista.appendChild(criaLi);
-  elementoInput.value = '';
+elementoBotao.addEventListener('click', function() {
+    const criaLi = document.createElement('li');
+    criaLi.innerText = elementoInput.value;
+    elementoLista.appendChild(criaLi);
+    elementoInput.value = '';
+  
+    criaLi.addEventListener('click', function(event){
+      const lista = document.querySelectorAll('li');
+      for (let index = 0; index < lista.length; index +=1) {
+        if (lista[index].style.backgroundColor === 'rgb(128, 128, 128)') {
+          lista[index].style.backgroundColor = '';
+        };
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+      };
+    });
+  });
 
-criaLi.addEventListener('click', function(event){
-  const lista = document.querySelectorAll('li');
-  for (let index = 0; index < lista.length; index +=1) {
-    if (lista[index].style.backgroundColor === 'rgb(128, 128, 128)');
-    lista[index].style.backgroundColor = '';
-  };
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-  })
+criaLi.addEventListener('dblclick', function(event) {
+    const listaItens = document.querySelectorAll('li');
+    for (let index = 0; index < listaItens.length; index +=1) {
+      if (listaItens[index].className === 'completed') {
+      listaItens[index].className = '';
+    };
+    }
+    event.target.className = 'completed';
 });
 
-
-
-// // requisito 10
-// let todosItens = document.getElementsByClassName('tarefas');
-
-// function apagarTodoItem() {
-//     let apagarTarefas = document.getElementById('apagar-tudo');
-//     apagarTarefas.addEventListener('click', function (event) {
-//       let listaTarefas = document.getElementById('lista-tarefas');
-//       let todosItens = document.getElementsByClassName('tarefas');
-//         for (let indice = 0; indice < todosItens.length; indice += 1) {
-//         tarefas.removeChild(todosItens[indice]);
-//       }
-//     });
-// }
+const button = document.getElementById('apaga-tudo');
+button.addEventListener('click', function () {
+  const lista = document.querySelectorAll('li');
+    for (let index = 0; index < lista.length; index += 1) {
+    lista[index].remove();
+  console.log(lista[index])
+  };
+});
